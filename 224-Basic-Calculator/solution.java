aa8acaf4bb1513c3980e33372d0660ddb95ebe40
krunning2 <zhidongke@yahoo.com>
2016-05-04 09:28:19 -0700

    [224. Basic Calculator][Runtime Error]committed by Krunning

--- 224-Basic-Calculator/solution.java
+++ 224-Basic-Calculator/solution.java
@@ -23,7 +23,7 @@ public class Solution {
             }else{
                 int tmp = 0;
                 while(p < s.length() && Character.isDigit(s.charAt(p))){
-                    tmp = tmp * 10 + (s.chatAt(p) - '0');
+                    tmp = tmp * 10 + (s.charAt(p) - '0');
                     p++;
                 }
                 ints.push(tmp);
