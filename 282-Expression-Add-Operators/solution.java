c7b0ba737a7cdfa0647f8dd6ebed1666efe4e4b3
krunning2 <zhidongke@yahoo.com>
2016-05-04 23:14:33 -0700

    [282. Expression Add Operators][Accepted]committed by Krunning

--- 282-Expression-Add-Operators/solution.java
+++ 282-Expression-Add-Operators/solution.java
@@ -3,7 +3,7 @@ public class Solution {
         List<String> res = new ArrayList<String>();
         if(num == null || num.length() == 0) return res;
         helper(num, target, res, "", 0, 0, 0);
-        return rst;
+        return res;
     }
     private void helper(String num, int target, List<String> res, String path, int pos, long eval, long multi){
         if(pos == num.length()){
@@ -13,6 +13,7 @@ public class Solution {
             return;
         }
         for(int i = pos; i < num.length(); i++){
+            if(i != pos && num.charAt(pos) == '0') break;
             long cur = Long.parseLong(num.substring(pos, i + 1));
             if(pos == 0){
                 helper(num, target, res, path + cur, i + 1, cur, cur);
