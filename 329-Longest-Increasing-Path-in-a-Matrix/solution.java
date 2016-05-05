1aa909d582298519733518c0e22676c85f228f15
krunning2 <zhidongke@yahoo.com>
2016-05-05 09:36:34 -0700

    [329. Longest Increasing Path in a Matrix][Compile Error]committed by Krunning

---
+++ 329-Longest-Increasing-Path-in-a-Matrix/solution.java
@@ -0,0 +1,40 @@
+public class Solution {
+    public int longestIncreasingPath(int[][] matrix) {
+        if(matrix == null || matrix.length ==0) return 0;
+        int[][] mem = new int[matrix.length][matrix[0].length];
+        boolean[][] visited = new boolean[matrix.length][matrix[0].length];
+        int[] res = {0};
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                DFShelper(matrix, j, i, mem, visited, Integer.MIN_VALUE, 1, res);
+            }
+        }
+        return res[0];
+        
+    }
+    private void DFShelper(int[][] matrix, int x, int y, int[][] mem, boolean[][] visited,int cur, int count, int[] res){
+        if(matrix, x, y, cur, visited){
+            if(mem[y][x] > count){
+                return;
+            }
+            if(count > res[0]){
+                res[0] = count;
+            }
+            int[]dx = {1, -1, 0, 0};
+            int[]dy = {0, 0 , 1, -1};
+            for(int i = 0; i < 4; i++){
+                visited[y][x] = true;
+                DFShelper(matrix, x + dx, y + dy, mem, visited, matrix[y][x], count + 1, res);
+                mem[y][x] = Math.max(mem[y][x], res[0]);
+                visited[y][x] = false;
+            }
+        }
+        
+    }
+    private boolean isValid(int[][]matrix, int x, int y, int cur, boolean[][] visited){
+        if(x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length && matrix[y][x] > cur){
+            return true;
+        }
+        return false;
+    }
+}
