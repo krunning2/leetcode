3d89462680fc3e4659900e5c99fb29ee4e480346
krunning2 <zhidongke@yahoo.com>
2016-05-05 21:43:30 -0700

    [329. Longest Increasing Path in a Matrix][Compile Error]committed by Krunning

--- 329-Longest-Increasing-Path-in-a-Matrix/solution.java
+++ 329-Longest-Increasing-Path-in-a-Matrix/solution.java
@@ -3,34 +3,31 @@ public class Solution {
         if(matrix == null || matrix.length ==0) return 0;
         int[][] mem = new int[matrix.length][matrix[0].length];
         boolean[][] visited = new boolean[matrix.length][matrix[0].length];
-        int[] res = {0};
+        int max = 1;
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[0].length; j++){
-                DFShelper(matrix, j, i, mem, visited, Integer.MIN_VALUE, 1, res);
-                //mem = new int[matrix.length][matrix[0].length];
+                int tmp = DFShelper(matrix, j, i, mem, visited);
+                max = Math.max(max, tmp);
             }
         }
-        return res[0];
+        return max;
         
     }
-    private void DFShelper(int[][] matrix, int x, int y, int[][] mem, boolean[][] visited,int cur, int count, int[] res){
+    private int DFShelper(int[][] matrix, int x, int y, int[][] mem, boolean[][] visited){
+        if(mem[y][x] > 0) return mem[y][x];
+        int max = 1;
         if(isValid(matrix, x, y, cur, visited)){
-            if(mem[y][x] > count){
-                return;
-            }
-            if(count > res[0]){
-                res[0] = count;
-            }
-            mem[y][x] = Math.max(mem[y][x], count);
             int[]dx = {1, -1, 0, 0};
             int[]dy = {0, 0 , 1, -1};
             for(int i = 0; i < 4; i++){
                 visited[y][x] = true;
-                DFShelper(matrix, x + dx[i], y + dy[i], mem, visited, matrix[y][x], count + 1, res);
+                int cur = DFShelper(matrix, x + dx[i], y + dy[i], mem, visited, matrix[y][x]) + 1;
+                max = Math.max(cur, max);
                 visited[y][x] = false;
             }
         }
-        
+        mem[y][x] = max;
+        return max;
     }
     private boolean isValid(int[][]matrix, int x, int y, int cur, boolean[][] visited){
         if(x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length && matrix[y][x] > cur && !visited[y][x]){
