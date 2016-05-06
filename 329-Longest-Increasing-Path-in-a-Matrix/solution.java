a57ea23c57929cf521a49f8fb0e52be760fc5ffb
krunning2 <zhidongke@yahoo.com>
2016-05-05 21:26:51 -0700

    [329. Longest Increasing Path in a Matrix][Compile Error]committed by Krunning

--- 329-Longest-Increasing-Path-in-a-Matrix/solution.java
+++ 329-Longest-Increasing-Path-in-a-Matrix/solution.java
@@ -7,6 +7,7 @@ public class Solution {
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[0].length; j++){
                 DFShelper(matrix, j, i, mem, visited, Integer.MIN_VALUE, 1, res);
+                int[][] mem = new int[matrix.length][matrix[0].length];
             }
         }
         return res[0];
@@ -20,19 +21,19 @@ public class Solution {
             if(count > res[0]){
                 res[0] = count;
             }
+            mem[y][x] = Math.max(mem[y][x], count);
             int[]dx = {1, -1, 0, 0};
             int[]dy = {0, 0 , 1, -1};
             for(int i = 0; i < 4; i++){
                 visited[y][x] = true;
                 DFShelper(matrix, x + dx[i], y + dy[i], mem, visited, matrix[y][x], count + 1, res);
-                mem[y][x] = Math.max(mem[y][x], res[0]);
                 visited[y][x] = false;
             }
         }
         
     }
     private boolean isValid(int[][]matrix, int x, int y, int cur, boolean[][] visited){
-        if(x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length && matrix[y][x] > cur && !visited[x][y]){
+        if(x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length && matrix[y][x] > cur && !visited[y][x]){
             return true;
         }
         return false;
