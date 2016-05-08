b38d34243e0f8ea7d4c54bae0df9474672582ec4
krunning2 <zhidongke@yahoo.com>
2016-05-08 16:42:43 -0700

    [315. Count of Smaller Numbers After Self][Runtime Error]committed by Krunning

---
+++ 315-Count-of-Smaller-Numbers-After-Self/solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public List<Integer> countSmaller(int[] nums) {
+        List<Integer> res = new ArrayList<Integer>();
+        List<Integer> T = new ArrayList<Integer>();
+        for(int i = nums.length - 1; i >= 0; i--){
+            int l = 0, r = nums.length - 1;
+            while(l + 1 < r){
+                int mid = l - (l - r) / 2;
+                if(nums[i] <= T.get(mid)){
+                    r = mid;
+                }else{
+                    l = mid;
+                }
+                if(nums[i] >= T.get(l)){
+                    res.add(l);
+                    T.add(i, nums[i]);
+                }else{
+                    res.add(r);
+                    T.add(i, nums[i]);
+                }
+            }
+        }
+        Collections.reverse(res);
+        return res;
+    }
+}
