98d3a074722f33348b0d655ee185513114229556
krunning2 <zhidongke@yahoo.com>
2016-05-08 19:22:17 -0700

    [315. Count of Smaller Numbers After Self][Accepted]committed by Krunning

--- 315-Count-of-Smaller-Numbers-After-Self/solution.java
+++ 315-Count-of-Smaller-Numbers-After-Self/solution.java
@@ -1,32 +1,52 @@
 public class Solution {
     public List<Integer> countSmaller(int[] nums) {
         List<Integer> res = new ArrayList<Integer>();
-        List<Integer> list = new ArrayList<Integer>();
-        for(int i = nums.length - 1; i >= 0; i--){
-            int index = find(nums[i], list);
-            res.add(index);
-            list.add(index, nums[i]);
+        if(nums == null || nums.length == 0){
+            return res;
+        }
+        res.add(0);
+        if(nums.length == 1) return res;
+        Node root = new Node(nums[nums.length - 1]);
+        for(int i = nums.length - 2; i >= 0; i--){
+            int count = insert(root, nums[i]);
+            res.add(count);
         }
         Collections.reverse(res);
         return res;
     }
-    private int find(int target, List<Integer> list){
-        if(list.size() == 0) return 0;
-        int start = 0;
-        int end = list.size() - 1;
-        if(list.get(end) < target) return end + 1;
-        while(start + 1 < end){
-            int mid = start - (start - end) / 2;
-            if(list.get(mid) >= target){
-                end = mid;
+    private int insert(Node root, int val){
+        int cur = 0;
+        while(true){
+            if(val <= root.val){
+                root.count ++;
+                if(root.left == null){
+                    root.left = new Node(val);
+                    break;
+                }else{
+                    root = root.left;
+                }
             }else{
-                start = mid;
+                cur += root.count;
+                if(root.right == null){
+                    root.right = new Node(val);
+                    break;
+                }else{
+                    root = root.right;
+                }
             }
         }
-        if(list.get(start) >= target){
-            return start;
-        }else{
-            return end;
-        }
+        return cur;
+    }
+}
+class Node{
+    Node left;
+    Node right;
+    int val;
+    int count;
+    Node(int val){
+        left = null;
+        right = null;
+        this.val = val;
+        count = 1;
     }
 }
