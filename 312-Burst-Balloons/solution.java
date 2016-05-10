af2138e68141cabd62ffddbd15e3db05e2cb305b
krunning2 <zhidongke@yahoo.com>
2016-05-09 22:37:24 -0700

    [312. Burst Balloons][Accepted]committed by Krunning

--- 312-Burst-Balloons/solution.java
+++ 312-Burst-Balloons/solution.java
@@ -5,8 +5,8 @@ public class Solution {
         int[] new_nums = new int[len + 2];
         for(int i = 1; i <= len; i++) new_nums[i] = nums[i - 1];
         new_nums[0] = new_nums[len + 1] = 1;
-        for(int k = 0; k < len; k ++){
-            for(int i = 1; i < n - k; i ++){
+        for(int k = 0; k <= len; k ++){
+            for(int i = 1; i <= len - k; i ++){
                 int j = i + k;
                 for(int x = i; x <= j; x ++){
                     dp[i][j] = Math.max(dp[i][j], dp[i][x - 1] + new_nums[i - 1] * new_nums[x] * new_nums[j + 1] + dp[x + 1][j]);
