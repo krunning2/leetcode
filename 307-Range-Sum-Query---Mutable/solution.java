6c75acc8d46ebafc58430391856a146ad6eb0a1d
krunning2 <zhidongke@yahoo.com>
2016-06-04 15:34:07 -0700

    [307. Range Sum Query - Mutable][Compile Error]committed by Krunning

---
+++ 307-Range-Sum-Query---Mutable/solution.java
@@ -0,0 +1,47 @@
+public class NumArray {
+    int[] BIT;
+    int n;
+    int[] nums;
+    public NumArray(int[] nums) {
+        n = nums.length;
+        BIT = new int[n + 1];
+        for(int i = 0; i < n; i++){
+            updateBIT(i, nums[i]);
+        }
+        this.nums = nums;
+    }
+    private void updateBIT(int i, int val){
+        i++;
+        while(i <= n){
+            BIT[i] += val;
+            i += (i & -i)
+        }
+    }
+    private int getSum(int i){
+        i++;
+        int sum = 0;
+        while(i > 0){
+            sum += BIT[i];
+            i -= (i & -i);
+        }
+        return sum;
+    }
+
+    void update(int i, int val) {
+        int diff = val - nums[i];
+        nums[i] = val;
+        updateBIT(i, diff);
+    }
+
+    public int sumRange(int i, int j) {
+        return getSum(j) - getSum(j - 1);
+    }
+    
+}
+
+
+// Your NumArray object will be instantiated and called as such:
+// NumArray numArray = new NumArray(nums);
+// numArray.sumRange(0, 1);
+// numArray.update(1, 10);
+// numArray.sumRange(1, 2);
