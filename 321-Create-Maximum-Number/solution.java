5b9efbd829afe38686c68cc778a4bc6d124a3cc2
krunning2 <zhidongke@yahoo.com>
2016-06-05 14:05:32 -0700

    [321. Create Maximum Number][Runtime Error]committed by Krunning

--- 321-Create-Maximum-Number/solution.java
+++ 321-Create-Maximum-Number/solution.java
@@ -7,27 +7,25 @@ public class Solution {
             int[] tmp3 = new int[k];
             int pos1 = 0, pos2 = 0, pos3 = 0;
             while(pos1 < tmp1.length || pos2 < tmp2.length){
-                if(compare(nums1, pos1, nums2, pos2) > 0){
+                if(compare(nums1, pos1, nums2, pos2)){
                     tmp3[pos3 ++] = tmp1[pos1 ++];
                 }else{
                     tmp3[pos3 ++] = tmp2[pos2 ++];
                 }
             }
-            if(compare(tmp3, 0, res, 0) < 0){
+            if(!compare(tmp3, 0, res, 0)){
                 res = tmp3;
             }
         }
         return res;
     }
     // In case of 04 0 >> 004, should be 040
-    private int compare(int[] nums1, int s1, int[] nums2, int s2){
-        while(s1 < nums1.length && s2 < nums2.length && nums1[s1] == nums2[s2]){
-            s1++;
-            s2++;
+    public boolean compare(int[] nums1, int start1, int[] nums2, int start2) {
+        for (; start1 < nums1.length && start2 < nums2.length; start1++, start2++) {
+            if (nums1[start1] > nums2[start2]) return true;
+            if (nums1[start1] < nums2[start2]) return false;
         }
-        if(s1 == nums1.length) return -1;
-        if(s2 == nums2.length) return 1;
-        return nums1[s1]-nums2[s2];
+        return start1 != nums1.length;
     }
     
     private int[] getMaxSub(int[] num, int k){
