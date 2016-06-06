80580eac8bde74c1832bd5da5b8d002d420a3819
krunning2 <zhidongke@yahoo.com>
2016-06-06 09:27:22 -0700

    [269. Alien Dictionary][Compile Error]committed by Krunning

--- 269-Alien-Dictionary/solution.java
+++ 269-Alien-Dictionary/solution.java
@@ -1,48 +1,55 @@
 public class Solution {
-        public String alienOrder(String[] words) {
-        Map<Character, Set<Character>> map=new HashMap<Character, Set<Character>>();
-        Map<Character, Integer> degree=new HashMap<Character, Integer>();
-        String result="";
-        if(words==null || words.length==0) return result;
-        for(String s: words){
-            for(char c: s.toCharArray()){
-                degree.put(c,0);
+    public String alienOrder(String[] words) {
+        HashMap<Character, Integer> degree = new HashMap<Character, Integer>();
+        HashMap<Character, Set<Character>> map = new HashMap<Character, Set<Character>>();
+        for(String s : words){
+            for(char c : s.toCharArray()){
+                degree.put(c, 0);
             }
         }
-        for(int i=0; i<words.length-1; i++){
-            String cur=words[i];
-            String next=words[i+1];
-            int length=Math.min(cur.length(), next.length());
-            for(int j=0; j<length; j++){
-                char c1=cur.charAt(j);
-                char c2=next.charAt(j);
-                if(c1!=c2){
+        for(int i = 0; i < words.length - 1; i ++){
+            String cur = words[i];
+            String next = words[i + 1];
+            int len = Math.min(cur.length(), next.length());
+            for(int j = 0; j < len; j++){
+                if(cur.charAt(j) != next.charAt(j)){
                     Set<Character> set=new HashSet<Character>();
-                    if(map.containsKey(c1)) set=map.get(c1);
-                    if(!set.contains(c2)){
-                        set.add(c2);
-                        map.put(c1, set);
-                        degree.put(c2, degree.get(c2)+1);
+                    if(map.containsKey(cur.charAt(j))){
+                        set = map.get(cur.charAt(j));
+                    }else{
+                        map.put(cur.charAt(j), set);
                     }
-                    break;
+                    if(!set.contains(next.charAt(j))){
+                        degree.put(cur.charAt(j), degree.get(cur.charAt(j)+1);
+                        set.add(next.charAt(j));
+                    }
                 }
+                // because we are not able to compare rest after this
+                break;
             }
         }
-        Queue<Character> q=new LinkedList<Character>();
-        for(char c: degree.keySet()){
-            if(degree.get(c)==0) q.add(c);
+        Queue<Character> queue = new LinkedList<Character>();
+        for(char c : degree.keySet()){
+            if(degree.get(c) == 0){
+                queue.offer(c);
+            }
         }
-        while(!q.isEmpty()){
-            char c=q.remove();
-            result+=c;
+        StringBuilder res = new StringBuilder();
+        while(!queue.isEmpty()){
+            char c = queue.poll();
+            res.append(c);
             if(map.containsKey(c)){
-                for(char c2: map.get(c)){
-                    degree.put(c2,degree.get(c2)-1);
-                    if(degree.get(c2)==0) q.add(c2);
+                for(char cur : map.get(c)){
+                    degree.put(cur, map.get(cur) - 1);
+                    if(map.get(cur) == 0){
+                        queue.offer(map.get(cur));
+                    }
                 }
             }
         }
-        if(result.length()!=degree.size()) return "";
-        return result;
+        if(degree.size != res.length()){
+            return "";
+        }
+        return res.toString();
     }
 }
