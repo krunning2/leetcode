c09f2e5c4bd2b41847e68f4a53f9f3dc7ea85272
krunning2 <zhidongke@yahoo.com>
2016-06-07 00:00:57 -0700

    [157. Read N Characters Given Read4][Compile Error]committed by Krunning

---
+++ 157-Read-N-Characters-Given-Read4/solution.java
@@ -0,0 +1,31 @@
+/* The read4 API is defined in the parent class Reader4.
+      int read4(char[] buf); */
+
+public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    private int p_buff = 0;
+    private int buffCnt = 0;
+    private char[] buff = new char[4];
+    public int read(char[] buf, int n) {
+        int p = 0;
+        while(p < n){
+            if(buffCnt == 0){
+                buffCnt = read(buff);
+            }
+            while(p < n && p_buff < buffCnt){
+                buf[p++] = buff[p_buff++];
+            }
+            if(p_buff == buffCnt){
+                buffCnt = 0;
+            }
+            if(buffCnt < 4){
+                break;
+            }
+        }
+        return p;
+    }
+}
