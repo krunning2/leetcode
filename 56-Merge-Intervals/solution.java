6344f472c484638803fe3cbc71532c8e4532c9a1
krunning2 <zhidongke@yahoo.com>
2016-06-06 23:03:48 -0700

    [56. Merge Intervals][Compile Error]committed by Krunning

--- 56-Merge-Intervals/solution.java
+++ 56-Merge-Intervals/solution.java
@@ -16,7 +16,7 @@ public class Solution {
             public int compare(Interval i1, Interval i2){
                 return i1.start - i2.start;
             }
-        }
+        };
         Collections.sort(intervals, c);
         List<Interval> res = new ArrayList<Interval>();
         Interval pre = intervals.get(0);
