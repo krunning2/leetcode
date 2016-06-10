d8573b4acbab92afba614038e28b884a98f546e8
krunning2 <zhidongke@yahoo.com>
2016-06-09 20:45:18 -0700

    [270. Closest Binary Search Tree Value][Compile Error]committed by Krunning

---
+++ 270-Closest-Binary-Search-Tree-Value/solution.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int closestValue(TreeNode root, double target) {
+        if(root == null){
+            throw new Exception("Root can't be null");
+        }
+        int[] res = {root.val};
+        helper(root, target, res);
+        return res[0];
+    }
+    private void helper(TreeNode root, double target, int[] res){
+        if(root == null){
+            return;
+        }
+        if(Math.abs(root.val - target) < Math.abs(res[0] - target)){
+            res[0] = root.val;
+        }
+        if(target > root.val){
+            helper(root.right, target, res);
+        }else{
+            helper(root.left, target, res);
+        }
+    }
+}
