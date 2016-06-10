fad255e87962de9d8511a7ba8e365e014271608d
krunning2 <zhidongke@yahoo.com>
2016-06-09 21:37:41 -0700

    [272. Closest Binary Search Tree Value II][Compile Error]committed by Krunning

---
+++ 272-Closest-Binary-Search-Tree-Value-II/solution.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        List<Integer> res = new ArrayList<Integer> ();
+        inorder(root, target, k, res);
+        return res;
+    }
+    private void inorder(TreeNode root, double target, int k, List<Integer> res){
+        if(root == null){
+            return;
+        }
+        inorder(root.left, target, int k, res);
+        if(res.size() == k){
+            if(Math.abs(root.val - target) < Math.abs(target - res.get(0))){
+                res.remove();
+            }else{
+                return;
+            }
+        }
+        res.add(root.val);
+        inorder(root.right, target, k, res);
+    }
+}
