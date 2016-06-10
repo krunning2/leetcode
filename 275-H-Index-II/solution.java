d320a057cf44f8a7579e73dc551ef9bbd3c9d110
krunning2 <zhidongke@yahoo.com>
2016-06-10 00:17:14 -0700

    [275. H-Index II][Time Limit Exceeded]committed by Krunning

--- 275-H-Index-II/solution.java
+++ 275-H-Index-II/solution.java
@@ -3,19 +3,20 @@ public class Solution {
         if(citations == null || citations.length == 0){
             return 0;
         }
+        Arrays.sort(citations);
         int start = 0, end = citations.length - 1, len = citations.length;
         while(start + 1 < end){
-            int mid = start - (start - end) / 2;
-            if(citations[mid] > len - mid){
-                mid = start;
+            int mid = start + (end - start) / 2;
+            if(citations[mid] < len - mid){
+                start = mid;
             }else{
-                mid = end;
+                end = mid;
             }
         }
-        if(citations[end] >= end){
-            return end;
-        }else if(citations[start] >= start){
-            return start;
+        if(citations[end] <= len - end){
+            return citations[end];
+        }else if(citations[start] <= len - start){
+            return citations[start];
         }
         return 0;
     }
