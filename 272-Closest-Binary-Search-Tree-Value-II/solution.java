a0421730cd96e8e4df8abd08ab260d822684e427
krunning2 <zhidongke@yahoo.com>
2016-06-09 22:31:52 -0700

    [272. Closest Binary Search Tree Value II][Compile Error]committed by Krunning

--- 272-Closest-Binary-Search-Tree-Value-II/solution.java
+++ 272-Closest-Binary-Search-Tree-Value-II/solution.java
@@ -9,23 +9,52 @@
  */
 public class Solution {
     public List<Integer> closestKValues(TreeNode root, double target, int k) {
-        List<Integer> res = new ArrayList<Integer> ();
-        inorder(root, target, k, res);
-        return res;
-    }
-    private void inorder(TreeNode root, double target, int k, List<Integer> res){
-        if(root == null){
-            return;
-        }
-        inorder(root.left, target, k, res);
-        if(res.size() == k){
-            if(Math.abs(root.val - target) < Math.abs(target - res.get(0))){
-                res.remove(0);
+        Stack<TreeNode> predecessor = new Stack<TreeNode>();
+        Stack<TreeNode> successor = new Stack<TreeNode>();
+        List<Integer> res = new ArrayList<Integer>();
+        while(root != null){
+            if(root.val <= target){
+                predecessor.push(root);
+                root = root.right;
             }else{
-                return;
+                successor.push(root);
+                root = root.left;
             }
         }
-        res.add(root.val);
-        inorder(root.right, target, k, res);
+        while(k > 0){
+            if(!predecessor.isEmpty() && !successor.isEmpty()){
+                break;
+            }
+            if(predecessor.isEmpty()){
+                res.add(getSuccessor(successor));
+            } else if(successor.isEmpty()){
+                res.add(getPredecessor(predecessor));
+            } else if(Math.abs(successor.peek() - target) < Math.abs(predecessor.peek() - target)){
+                res.add(getSuccessor(successor));
+            }else{
+                res.add(getPredecessor(predecessor));
+            }
+            k--;
+        }
+        return res;
+    }
+    private int getPredecessor(Stack<TreeNode> predecessor){
+        TreeNode top = predecessor.pop();
+        TreeNode cur = top.left;
+        while(cur != null){
+            predecessor.push(cur);
+            cur = cur.right;
+        }
+        return top.val;
+    }
+    
+    private int getSuccessor(Stack<TreeNode> successor){
+        TreeNode top = successor.pop();
+        TreeNode cur = top.right;
+        while(cur != null){
+            successor.push(cur);
+            cur = cur.left;
+        }
+        return top.val;
     }
 }
