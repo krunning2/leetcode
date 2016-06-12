1052dc6518f8de4a63f8555787d8a8aa3226a9b3
krunning2 <zhidongke@yahoo.com>
2016-06-12 14:45:13 -0700

    [330. Patching Array][Time Limit Exceeded]committed by Krunning

--- 330-Patching-Array/solution.java
+++ 330-Patching-Array/solution.java
@@ -1,16 +1,14 @@
 public class Solution {
     public int minPatches(int[] nums, int n) {
-         long max = 0;
-    int cnt = 0;
-    for (int i = 0; max < n;) {
-        if (i >= nums.length || max < nums[i] - 1) {
-            max += max + 1;
-            cnt++;
-        } else {
-            max += nums[i];
-            i++;
+        int miss = 1, added = 0, i = 0;
+        while (miss <= n) {
+            if (i < nums.length && nums[i] <= miss) {
+                miss += nums[i++];
+            } else {
+                miss += miss;
+                added++;
+            }
         }
-    }
-    return cnt;
+        return added;
     }
 }
