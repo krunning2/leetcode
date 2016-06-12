e03891a29ce4b122bb3e8813158eff75ef173316
krunning2 <zhidongke@yahoo.com>
2016-06-12 14:11:12 -0700

    [330. Patching Array][Compile Error]committed by Krunning

--- 330-Patching-Array/solution.java
+++ 330-Patching-Array/solution.java
@@ -2,7 +2,7 @@ public class Solution {
     public int minPatches(int[] nums, int n) {
         long miss = 1, added = 0, i = 0;
         while (miss <= n) {
-            if (i < nums.size() && nums[i] <= miss) {
+            if (i < nums.length && nums[i] <= miss) {
                 miss += nums[i++];
             } else {
                 miss += miss;
