eb86d60af57e203d49a905b7d5c45d739cb9086e
krunning2 <zhidongke@yahoo.com>
2016-06-12 14:12:06 -0700

    [330. Patching Array][Accepted]committed by Krunning

---
+++ 330-Patching-Array/solution.cpp
@@ -0,0 +1,14 @@
+public class Solution {
+    public int minPatches(int[] nums, int n) {
+        int miss = 1, added = 0, i = 0;
+        while (miss <= n) {
+            if (i < nums.length && nums[i] <= miss) {
+                miss += nums[i++];
+            } else {
+                miss += miss;
+                added++;
+            }
+        }
+        return added;
+    }
+}
