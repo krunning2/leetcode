df9d1df85be6065f12b1b66df58a9578b4cb323c
krunning2 <zhidongke@yahoo.com>
2016-06-11 20:51:50 -0700

    [318. Maximum Product of Word Lengths][Compile Error]committed by Krunning

---
+++ 318-Maximum-Product-of-Word-Lengths/solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int maxProduct(String[] words) {
+        if(words == null || words.length <= 1){
+            return 0;
+        }
+        int[] values = new int[words.length];
+        for(int i = 0; i< s.length(); i++){
+            String s = words[i];
+            for(char c : s.toCharArray()){
+                values[i] |= 1 << (c - 'a');
+            }
+        }
+        int max = 0;
+        for(int i = 0; i < words.length; i++){
+            for(int j = i + 1; j < words.length; j++){
+                if((values[j] & values[i]) == 0 && words[i].length() * words[j].length() > max){
+                    max = words[i].length() * words[j].length();
+                }
+            }
+        }
+        return max;
+    }
+}
