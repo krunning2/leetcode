bc39fcc6f60abf92f6f93593a53d9c005d65eb19
krunning2 <zhidongke@yahoo.com>
2016-06-12 14:51:03 -0700

    [330. Patching Array][Accepted]committed by Krunning

--- 330-Patching-Array/solution.java
+++ 330-Patching-Array/solution.java
@@ -1,14 +1,14 @@
 public class Solution {
     public int minPatches(int[] nums, int n) {
-        int miss = 1, added = 0, i = 0;
-        while (miss <= n) {
-            if (i < nums.length && nums[i] <= miss) {
-                miss += nums[i++];
-            } else {
-                miss += miss;
-                added++;
+        int cnt = 0,i=0;
+        for(long known_sum = 1;known_sum <= n;){
+            if(i < nums.length && known_sum >= nums[i]){
+                known_sum += nums[i++];
+            }else{
+                known_sum <<= 1;
+                cnt++;
             }
         }
-        return added;
+        return cnt;
     }
 }
