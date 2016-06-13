0f2b4767fee92f14cd1e83a9b755cf9c16d2ce36
krunning2 <zhidongke@yahoo.com>
2016-06-12 21:16:06 -0700

    [356. Line Reflection][Compile Error]committed by Krunning

---
+++ 356-Line-Reflection/solution.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public boolean isReflected(int[][] points) {
+        if(points == null || points.length == 0){
+            return false;
+        }
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        HashMap<Integer, Set<Integer>> map = new HashMap<Integer, Set<Integer>>();
+        for(int i = 0; i < points.length; i++){
+            max = Math.max(max, [i][0]);
+            min = Math.min(min, [i][0]);
+            map.putIfAbsent(points[i][0], new HashSet<Integer>());
+            map.get(points[i][0].add(points[i][1]))
+        }
+        int mid =(max + min) >> 1;
+        for(int i = 0; i < points.length; i++){
+            int curX = points[i][0];
+            int curY = points[i][1];
+            int targetX = 2 * mid - curX;
+            if(map.containsKey(targetX) && map.get(targetX).contains(curY)){
+                continue;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
