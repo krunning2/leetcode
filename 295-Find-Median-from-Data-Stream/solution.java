f5cb38f100d70a67a97ffc70bf3a646471cfa5eb
krunning2 <zhidongke@yahoo.com>
2016-06-13 21:54:41 -0700

    [295. Find Median from Data Stream][Accepted]committed by Krunning

--- 295-Find-Median-from-Data-Stream/solution.java
+++ 295-Find-Median-from-Data-Stream/solution.java
@@ -1,42 +1,21 @@
 public class MedianFinder {
-    Comparator<Integer> c = new Comparactor<Integer>(){
-        public int compare(Integer a, Integer b){
-            return b - a;
-        }
-    };
-    PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(10, c);
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(Collections.reverseOrder());
     PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
-    Integer median = null;
     // Adds a number into the data structure.
     public void addNum(int num) {
-        if(median == null){
-            median = num;
-            maxHeap.offer(num);
-        }else{
-            if(num < median){
-                maxHeap.offer(num);
-            }else{
-                minHeap.offer(num);
-            }
-            if(minHeap.size() + 1 > maxHeap.size()){
-                maxHeap.offer(median);
-                median = minHeap.poll();
-            }
-            if(maxHeap.size() + 1 > minHeap.size()){
-                minHeap.offer(median);
-                median = maxHeap.poll();
-            }
+        maxHeap.offer(num);
+        minHeap.offer(maxHeap.poll());
+        if(maxHeap.size() < minHeap.size()){
+            maxHeap.offer(minHeap.poll());
         }
     }
 
     // Returns the median of current data stream
     public double findMedian() {
-        if(maxHeap.size() == minHeap.size()){
-            return median * 1.0;
-        }else if(maxHeap.size() > minHeap.size()){
-            return (median + maxHeap.peek()) / 2.0;
+        if(minHeap.size() == maxHeap.size()){
+            return (minHeap.peek() + maxHeap.peek()) / 2.0;
         }else{
-            return (median + minHeap.peek()) / 2.0;
+            return maxHeap.peek() * 1.0;
         }
     }
 };
