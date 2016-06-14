9b04bd389fad9e4047c13f0fb503d6c355981236
krunning2 <zhidongke@yahoo.com>
2016-06-13 20:48:31 -0700

    [4. Median of Two Sorted Arrays][Compile Error]committed by Krunning

--- 4-Median-of-Two-Sorted-Arrays/solution.java
+++ 4-Median-of-Two-Sorted-Arrays/solution.java
@@ -1,6 +1,12 @@
 public class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        
+        int m = nums1.length;
+        int n = nums2.length;
+        if((n + m) % 2 == 0){
+            return (find(nums1, nums2, 0, 0, (n + m) / 2 + 1) + find(nums1, nums2, 0, 0, (n + m) / 2) )/ 2.0;
+        }else{
+            return find(A, B, 0, 0 , (n + m) / 2 + 1);
+        }
     }
     private int find(int[] A, int[] B, int startA, int startB, int k){
         if(startA >= A.length){
