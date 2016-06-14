cfe5f86677c9a8280e08a75e25c8fc4403deebb1
krunning2 <zhidongke@yahoo.com>
2016-06-13 23:39:08 -0700

    [212. Word Search II][Accepted]committed by Krunning

--- 212-Word-Search-II/solution.java
+++ 212-Word-Search-II/solution.java
@@ -38,57 +38,53 @@ public class Solution {
         visited[x][y] = false;  
     }  
     
-    class TrieNode {
-        Map<Character, TrieNode> map;
-        boolean is_end;
-        public TrieNode() {
-            map = new HashMap<Character, TrieNode>();
-            is_end = false;
-        }
-    }
-
-    public class Trie {
-        private TrieNode root;
-    
-        public Trie() {
-            root = new TrieNode();
-        }
-    
-        // Inserts a word into the trie.
-        public void insert(String word) {
-            TrieNode node = root;
-            for(char c : word.toCharArray()){
-                node.map.putIfAbsent(c, new TrieNode());
-                node = node.map.get(c);
-            }
-            node.is_end = true;
-        }
-    
-        // Returns if the word is in the trie.
-        public boolean search(String word) {
-            TrieNode node = root;
-            for(char c : word.toCharArray()){
-                if(node.map.containsKey(c)){
-                    node = node.map.get(c);
-                }else{
-                    return false;
-                }
-            }
-            return node.is_end;
-        }
-    
-        // Returns if there is any word in the trie
-        // that starts with the given prefix.
-        public boolean startsWith(String prefix) {
-            TrieNode node = root;
-            for(char c : prefix.toCharArray()){
-                if(node.map.containsKey(c)){
-                    node = node.map.get(c);
-                }else{
-                    return false;
-                }
-            }
-            return true;
-        }
-    }
+    class TrieNode {  
+        public TrieNode[] children = new TrieNode[26];  
+        public String item = "";  
+          
+        // Initialize your data structure here.  
+        public TrieNode() {  
+        }  
+    }  
+  
+    class Trie {  
+        private TrieNode root;  
+      
+        public Trie() {  
+            root = new TrieNode();  
+        }  
+      
+        // Inserts a word into the trie.  
+        public void insert(String word) {  
+            TrieNode node = root;  
+            for (char c : word.toCharArray()) {  
+                if (node.children[c - 'a'] == null) {  
+                    node.children[c - 'a'] = new TrieNode();  
+                }  
+                node = node.children[c - 'a'];  
+            }  
+            node.item = word;  
+        }  
+      
+        // Returns if the word is in the trie.  
+        public boolean search(String word) {  
+            TrieNode node = root;  
+            for (char c : word.toCharArray()) {  
+                if (node.children[c - 'a'] == null) return false;  
+                node = node.children[c - 'a'];  
+            }  
+            return node.item.equals(word);  
+        }  
+      
+        // Returns if there is any word in the trie  
+        // that starts with the given prefix.  
+        public boolean startsWith(String prefix) {  
+            TrieNode node = root;  
+            for (char c : prefix.toCharArray()) {  
+                if (node.children[c - 'a'] == null) return false;  
+                node = node.children[c - 'a'];  
+            }  
+            return true;  
+        }  
+    }  
 }
