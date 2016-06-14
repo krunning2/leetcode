64c4bcce58ce8684a8944c8a0e919eb74f70fb26
krunning2 <zhidongke@yahoo.com>
2016-06-13 23:37:29 -0700

    [212. Word Search II][Time Limit Exceeded]committed by Krunning

--- 212-Word-Search-II/solution.java
+++ 212-Word-Search-II/solution.java
@@ -1,42 +1,42 @@
 public class Solution {
-    public List<String> findWords(char[][] board, String[] words) {
-        Set<String> res = new HashSet<String>();
-        if(board == null || board.length == 0 || words == null || words.length == 0){
-            return new ArrayList<String>();
-        }
-        Trie t = new Trie();
-        for(String s : words){
-            t.insert(s);
-        }
-        int m = board.length;
-        int n = board[0].length;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                DFS(board, i, j, "", new boolean[m][n], t, res);
-            }
-        }
-        return new ArrayList<String>(res);
-    }
-    
-    private void DFS(char[][] board, int i, int j, String s, boolean[][]visited, Trie t, Set<String> res){
-        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j]){
-            return;
-        }
-        s += board[i][j];
-        if(!t.startsWith(s)){
-            return;
-        }
-        if(t.search(s)){
-            res.add(s);
-        }
-        visited[i][j] = true;
-        int[] dx = {0, 0, 1, -1};
-        int[] dy = {1, -1, 0, 0};
-        for(int k = 0; k < 4; k++){
-            DFS(board, i + dx[k], j + dy[k], s, visited, t, res);
-        }
-        visited[i][j] = false;
-    }
+    Set<String> res = new HashSet<String>();  
+      
+    public List<String> findWords(char[][] board, String[] words) {  
+        Trie trie = new Trie();  
+        for (String word : words) {  
+            trie.insert(word);  
+        }  
+          
+        int m = board.length;  
+        int n = board[0].length;  
+        boolean[][] visited = new boolean[m][n];  
+        for (int i = 0; i < m; i++) {  
+            for (int j = 0; j < n; j++) {  
+                dfs(board, visited, "", i, j, trie);  
+            }  
+        }  
+          
+        return new ArrayList<String>(res);  
+    }  
+      
+    public void dfs(char[][] board, boolean[][] visited, String str, int x, int y, Trie trie) {  
+        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length) return;  
+        if (visited[x][y]) return;  
+          
+        str += board[x][y];  
+        if (!trie.startsWith(str)) return;  
+          
+        if (trie.search(str)) {  
+            res.add(str);  
+        }  
+          
+        visited[x][y] = true;  
+        dfs(board, visited, str, x - 1, y, trie);  
+        dfs(board, visited, str, x + 1, y, trie);  
+        dfs(board, visited, str, x, y - 1, trie);  
+        dfs(board, visited, str, x, y + 1, trie);  
+        visited[x][y] = false;  
+    }  
     
     class TrieNode {
         Map<Character, TrieNode> map;
