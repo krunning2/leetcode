8bfc436c53dbaaadc0685b91411f81465e2cbb4e
krunning2 <zhidongke@yahoo.com>
2016-06-15 09:17:25 -0700

    [57. Insert Interval][Accepted]committed by Krunning

---
+++ 57-Insert-Interval/solution.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+        List<Interval> res = new ArrayList<Interval> ();
+        if(intervals == null || newInterval == null) return intervals;
+        for(Interval i : intervals){
+            if(newInterval.end < i.start){
+                res.add(newInterval);
+                newInterval = i;
+            }else if(newInterval.start <= i.end && newInterval.end >= i.start){
+                newInterval.start = Math.min(newInterval.start, i.start);
+                newInterval.end = Math.max(newInterval.end, i.end);
+            }else{
+                res.add(i);
+            }
+        }
+        res.add(newInterval);
+        return res;
+    }
+}
