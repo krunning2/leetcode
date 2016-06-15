334d0dbf3c06e352671bc663b3b7b88002430048
krunning2 <zhidongke@yahoo.com>
2016-06-14 20:23:33 -0700

    [128. Longest Consecutive Sequence][Accepted]committed by Krunning

--- 128-Longest-Consecutive-Sequence/solution.java
+++ 128-Longest-Consecutive-Sequence/solution.java
@@ -6,7 +6,7 @@ public class Solution {
         }
         int res = 0;
         for(int i = 0; i < nums.length; i++){
-            if(map.get(i)){
+            if(map.get(nums[i])){
                 continue;
             }
             int cur = nums[i];
