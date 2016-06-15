943fac5c3887b357414d7ce80373ecdad4932b67
krunning2 <zhidongke@yahoo.com>
2016-06-14 21:29:41 -0700

    [239. Sliding Window Maximum][Compile Error]committed by Krunning

---
+++ 239-Sliding-Window-Maximum/solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if(nums == null || k <= 0){
+            return null;
+        }
+        int n = nums.length;
+        int[] res = new int[n - k + 1];
+        Deque<Integer> deque = new LinkedList<Integer>();
+        for(int i = 0; i < n; i++){
+            while(!deque.isEmpty() && deque.peekLast() < nums[i]){
+                deque.pollLast();
+            }
+            deque.offerLast(nums[i]);
+            if(i - k >= 0 && nums[i - k] = deque.peekFirst()){
+                deque.pollFirst();
+            }
+            if(i + 1 == k){
+                res[i - k + 1] = deque.peekFirst();
+            }
+        }
+        return res;
+    }
+}
