304dca2db921b72047dfb42e8e6a0c6870200160
krunning2 <zhidongke@yahoo.com>
2016-06-14 20:22:33 -0700

    [128. Longest Consecutive Sequence][Compile Error]committed by Krunning

---
+++ 128-Longest-Consecutive-Sequence/solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int longestConsecutive(int[] nums) {
+        Map<Integer, Boolean> map = new HashMap<Integer, Boolean>();
+        for(int n : nums){
+            map.put(n, false);
+        }
+        int res = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(map.get(i)){
+                continue;
+            }
+            int cur = nums[i];
+            int count = 1;
+            while(map.containsKey(cur + 1)){
+                count++;
+                map.put(cur + 1, true);
+                cur++;
+            }
+            int cur = nums[i];
+            while(map.containsKey(cur - 1)){
+                count++;
+                map.put(cur - 1, true);
+                cur--;
+            }
+            res = Math.max(res, count);
+        }
+        return res;
+    }
+}
