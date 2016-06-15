3c1fe23e5566ed5d65805e84fa5bfe849326e955
krunning2 <zhidongke@yahoo.com>
2016-06-14 20:09:48 -0700

    [23. Merge k Sorted Lists][Compile Error]committed by Krunning

---
+++ 23-Merge-k-Sorted-Lists/solution.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(lists.size(), new Comparator<ListNode>(){
+            public int compare(ListNode l1, ListNode l2){
+                return l1.val - l2.val;
+            }
+        });
+        ListNode head = new ListNode();
+        ListNode cur = head;
+        for(ListNode node : lists){
+            if(node != null){
+                queue.offer(node);
+            }
+        }
+        while(!queue.isEmpty()){
+            cur.next = queue.poll();
+            if(cur.next.next != null){
+                queue.offer(cur.next.next);
+            }
+            cur = cur.next;
+        }
+        return head.next;
+    }
+}
