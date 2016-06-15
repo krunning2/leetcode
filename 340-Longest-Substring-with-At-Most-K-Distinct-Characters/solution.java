af5598c32b1e4646180607301995503ac0183b55
krunning2 <zhidongke@yahoo.com>
2016-06-14 20:54:46 -0700

    [340. Longest Substring with At Most K Distinct Characters][Accepted]committed by Krunning

--- 340-Longest-Substring-with-At-Most-K-Distinct-Characters/solution.java
+++ 340-Longest-Substring-with-At-Most-K-Distinct-Characters/solution.java
@@ -1,30 +1,32 @@
 public class Solution {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
-        if(s == null || s.length() == 0 || k <= 0){
+        if(s == null || s.length() == 0){
             return 0;
         }
-        int p1 = 0, p2 = 0;
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
         int res = 0;
-        while(p2 < s.length()){
-            char c2 = s.charAt(p2);
-            if(map.containsKey(c2)){
-                map.put(c2, map.get(c2) + 1);
-            }else{
-                map.put(c2, 1);
-            }
-            
-            while(p1 <= p2 && map.size() > k){
-                char c1 = s.charAt(p1);
-                if(map.get(c1) > 1){
-                    map.put(c1, map.get(c1) - 1);
+        int j = 0;
+        for(int i = 0; i < s.length(); i++){
+            while(j < s.length()){
+                char cur = s.charAt(j);
+                if(map.size() < k || map.containsKey(cur)){
+                    if(map.containsKey(cur)){
+                        map.put(cur, map.get(cur) + 1);
+                    }else{
+                        map.put(cur, 1);
+                    }
+                    j++;
                 }else{
-                    map.remove(c1);
+                    break;
                 }
-                p1++;
             }
-            p2++;
-            res = Math.max(res, p2 - p1);
+            res = Math.max(res, j - i);
+            char tmp = s.charAt(i);
+            if(map.containsKey(tmp) && map.get(tmp) > 1){
+                map.put(tmp, map.get(tmp) - 1);
+            }else if(map.containsKey(tmp) && map.get(tmp) <= 1){
+                map.remove(tmp);
+            }
         }
         return res;
     }
