f88b484d84c3f998d009e71f1c23a20d1e39bd54
krunning2 <zhidongke@yahoo.com>
2016-06-14 20:41:22 -0700

    [340. Longest Substring with At Most K Distinct Characters][Compile Error]committed by Krunning

---
+++ 340-Longest-Substring-with-At-Most-K-Distinct-Characters/solution.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        if(s == null || s.length() == 0 || k <= 0){
+            return 0;
+        }
+        int p1 = 0, p2 = 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int res = 0;
+        while(p2 < s.length()){
+            char c2 = s.charAt(p2);
+            if(map.containsKey(c2))){
+                map.put(c2, map.get(c2) + 1);
+            }else{
+                map.put(c2, 1);
+            }
+            
+            while(p1 <= p2 && map.size() > k){
+                char c1 = s.charAt(p1);
+                if(map.get(c1) > 1){
+                    map.put(c1, map.get(c1) - 1);
+                }else{
+                    map.remove(c1);
+                }
+                p1++;
+            }
+            p2++;
+            res = Math.max(p2 - p1);
+        }
+        return res;
+    }
+}
