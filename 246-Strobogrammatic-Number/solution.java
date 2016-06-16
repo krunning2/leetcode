c34cdec8e26a5f61cd6156e0b51e9a4c4705a6e4
krunning2 <zhidongke@yahoo.com>
2016-06-15 23:36:11 -0700

    [246. Strobogrammatic Number][Accepted]committed by Krunning

--- 246-Strobogrammatic-Number/solution.java
+++ 246-Strobogrammatic-Number/solution.java
@@ -11,9 +11,10 @@ public class Solution {
             start++;
             end--;
         }
-        if(start == end) return (num.charAt(start) == '6' || num.charAt(start) == '9' || num.charAt(start) == '1');
+        if(start == end) return (num.charAt(start) == '8' || num.charAt(start) == '1' || num.charAt(start) == '0');
+        return true;
     }
     private boolean check(char c1, char c2){
-        return (c1 == '6' && c2 == '9' || c1 == '9' && c2 == '6' || c1 == '1' && c2 == '1');
+        return (c1 == '6' && c2 == '9' || c1 == '9' && c2 == '6' || c1 == '1' && c2 == '1' || c1 == '8' && c2 == '8' || c1 == '0' && c2 == '0');
     }
 }
