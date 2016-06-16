0995b0b1952a093acdab95586e5560c6ec298ef2
krunning2 <zhidongke@yahoo.com>
2016-06-15 23:33:13 -0700

    [246. Strobogrammatic Number][Compile Error]committed by Krunning

---
+++ 246-Strobogrammatic-Number/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean isStrobogrammatic(String num) {
+        if(num == null || num.length() == 0){
+            return true;
+        }
+        int start = 0, end = num.length() - 1;
+        while(start < end){
+            if(!check(num.charAt(start), num.charAt(end))){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        if(start == end) return (num.charAt(start) == '6' || num.charAt(start) == '9' || num.charAt(start) == '1');
+    }
+    private boolean check(char c1, char c2){
+        return (c1 == '6' && c2 == '9' || c1 == '9' && c2 == '6' || c1 == '1' && c2 == '1'){
+    }
+}
