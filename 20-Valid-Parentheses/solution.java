4fa8e8505016c6b9880954da065c16bde15cf432
krunning2 <zhidongke@yahoo.com>
2016-06-16 22:01:00 -0700

    [20. Valid Parentheses][Runtime Error]committed by Krunning

---
+++ 20-Valid-Parentheses/solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }else{
+                char cur = get(c);
+                if(stack.peek() != cur) return false;
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+    private char get(char c){
+        switch(c) {
+            case ')': return '(';
+            case '}': return '{';
+            case ']': return '[';
+            default : return '0';
+        }
+    }
+}
