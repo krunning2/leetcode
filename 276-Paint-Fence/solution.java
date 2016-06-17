3eaba7d84cd63701e452b1577d1ac10aa92d69c8
krunning2 <zhidongke@yahoo.com>
2016-06-16 23:56:13 -0700

    [276. Paint Fence][Accepted]committed by Krunning

--- 276-Paint-Fence/solution.java
+++ 276-Paint-Fence/solution.java
@@ -10,7 +10,7 @@ public class Solution {
         dp[1] = k;
         dp[2] = k * k;
         for(int i = 3; i <=n; i++){
-            dp[i] = (k - 1)(dp[i - 1] + dp[i - 2]);
+            dp[i] = (k - 1) * (dp[i - 1] + dp[i - 2]);
         }
         return dp[n];
     }
