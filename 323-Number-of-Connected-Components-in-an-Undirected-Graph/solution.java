62e84b5ba321accf892a05f2be5958171d75910e
krunning2 <zhidongke@yahoo.com>
2016-06-18 14:21:10 -0700

    [323. Number of Connected Components in an Undirected Graph][Compile Error]committed by Krunning

---
+++ 323-Number-of-Connected-Components-in-an-Undirected-Graph/solution.java
@@ -0,0 +1,45 @@
+public class Solution {
+    public int countComponents(int n, int[][] edges) {
+        boolean visited[] = new boolean[n];
+        int res = n;
+        if(edges == null || edges.length == 0){
+            return 0;
+        }
+        UnionFind uf = new UnionFind(n);
+        for(int i = 0; i < n; i++){
+            int n0 = edges[i][0];
+            int n1 = edges[i][1];
+            if(uf.find(n0) != uf.find(n1)){
+                res --;
+                uf.union(n0, n1);
+            }
+        }
+        return res;
+    }
+    class UnionFind{
+        Map<Integer, Integer> father;
+        UnionFind(int n){
+            father.put(n, n);
+        }
+        public int find(int key){
+            int f = father.get(key);
+            while(f != father.get(f)){
+                f = father.get(f);
+            }
+            int p = father.get(key);
+            int tmp = -1;
+            while(p != father.get(p)){
+                tmp = father.get(p);
+                father.put(p, f);
+                p = tmp;
+            }
+        }
+        public void union(int key1, int key2){
+            int f1 = find(key1);
+            int f2 = find(key2);
+            if(f1 != f2){
+                father.put(f1, f2);
+            }
+        }
+    }
+}
