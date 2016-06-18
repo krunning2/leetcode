d9102dc33e0c60eaaedef755014bb14566382c15
krunning2 <zhidongke@yahoo.com>
2016-06-18 12:12:45 -0700

    [146. LRU Cache][Compile Error]committed by Krunning

---
+++ 146-LRU-Cache/solution.java
@@ -0,0 +1,57 @@
+public class LRUCache {
+    
+    private int capacity;
+    private Map<Integer, Node> map;
+    Node head;
+    Node tail;
+    
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<Integer, Node>();
+        head.next = tail;
+        tail.pre = head;
+    }
+    
+    public int get(int key) {
+        if(map.containsKey(key)){
+            Node cur = map.get(key);
+            cur.pre.next = cur.next;
+            cur.next.pre = cur.pre;
+            moveToTail(cur);
+            return cur.val;
+        }
+        return -1;
+    }
+    
+    public void set(int key, int value) {
+        if(map.size() >= capacity){
+            head.next = head.next.next;
+            head.next.next.pre = head;
+        }
+        if(map.containsKey(key)){
+            get(key).val = value;
+        }else{
+            Node n = new Node(key, value);
+            map.put(key, n);
+            moveToTail(n);
+        }
+    }
+    
+    private moveToTail(Node cur){
+        cur.pre = tail.pre;
+        tail.pre.next = cur;
+        cur.next = tail;
+        tail.pre = cur;
+    }
+    
+    class Node{
+        Node pre;
+        Node next;
+        int key;
+        int val;
+        Node(int key, int val){
+            this.key = key;
+            this.val = val;
+        }
+    }
+}
