ca607b344854cd0f7053316f428bb8c38c0662e3
krunning2 <zhidongke@yahoo.com>
2016-06-18 20:49:46 -0700

    [207. Course Schedule][Compile Error]committed by Krunning

---
+++ 207-Course-Schedule/solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
+        int indegree[] = new int[numCourses];
+        for(int i = 0; i < numCourses; i++){
+            map.put(i, new ArrayList<Integer>());
+        }
+        for(int i = 0; i < prerequisites.length; i++){
+            map.get(prerequisites[i][1]).add(prerequisites[i][0]);
+            indegree[prerequisites[i][0]]++;
+        }
+        Queue<Integer> q = new LinkedList<Integer>();
+        for(int in : indegree){
+            if(in == 0){
+                q.offer(in);
+            }
+        }
+        int count = numCourses;
+        while(!q.isEmpty()){
+            int cur = queue.poll();
+            for(int i : map.get(cur)){
+                if(--indegree[i] == 0){
+                    q.offer(i);
+                }
+            }
+            count--;
+        }
+        return count == 0;
+    }
+}
