a88e5c4f248f9aa851acc2ee51b21814d686112c
krunning2 <zhidongke@yahoo.com>
2016-06-19 12:11:27 -0700

    [341. Flatten Nested List Iterator][Compile Error]committed by Krunning

---
+++ 341-Flatten-Nested-List-Iterator/solution.java
@@ -0,0 +1,53 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+public class NestedIterator implements Iterator<Integer> {
+    Iterator<NestedInteger> iterator;
+    Stack<NestedInteger> stack;
+    public NestedIterator(List<NestedInteger> nestedList) {
+        iterator = nestedList.terator();
+        stack = new Stack<NestedInteger>();
+        stack.push(iterator);
+    }
+
+    @Override
+    public Integer next() {
+        while(iterator.hasNext() || !stack.isEmpty()){
+            if(!iterator.hasNext()){
+               iterator = stack.pop(); 
+            }
+            NestedInteger cur = iterator.next();
+            if(!cur.isInteger){
+                iterator = cur.getList().iterator();
+                stack.push(iterator);
+            }else{
+                return cur.getInteger();
+            }
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        iterator.hasNext() || !stack.isEmpty();
+    }
+}
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i = new NestedIterator(nestedList);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
