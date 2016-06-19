1832bc518009eadefe374d31db612e80f493dc71
krunning2 <zhidongke@yahoo.com>
2016-06-18 22:01:42 -0700

    [50. Pow(x, n)][Accepted]committed by Krunning

---
+++ 50-Pow(x,-n)/solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public double myPow(double x, int m) {
+        long n = m;
+        if(n < 0){
+            n = -n;
+            x = 1 / x;
+        }
+        double res = 1;
+        for(double f = x; n > 0; n = n >> 1){
+            if(n % 2 == 1){
+                res *= f;
+            }
+            f *= f;
+        }
+        return res;
+    }
+}
