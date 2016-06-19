434b3e65d3608ef1248b3d73af2823bf6003bf27
krunning2 <zhidongke@yahoo.com>
2016-06-18 23:09:15 -0700

    [357. Count Numbers with Unique Digits][Accepted]committed by Krunning

--- 357-Count-Numbers-with-Unique-Digits/solution.java
+++ 357-Count-Numbers-with-Unique-Digits/solution.java
@@ -1,8 +1,8 @@
 public class Solution {
     public int countNumbersWithUniqueDigits(int n) {
         n = Math.min(10, n);
-        dp[0] = 1;
         int[] dp = new int[n + 1];
+        dp[0] = 1;
         for(int i = 1; i <= n; i++){
             dp[i] = 9;
             for(int x = 9; x >= 9 - i + 2; x --){
