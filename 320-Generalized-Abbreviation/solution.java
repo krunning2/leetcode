1fbcd4940c173f6e608d893a6633cc422884ae7f
krunning2 <zhidongke@yahoo.com>
2016-06-18 22:32:56 -0700

    [320. Generalized Abbreviation][Compile Error]committed by Krunning

---
+++ 320-Generalized-Abbreviation/solution.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public List<String> generateAbbreviations(String word) {
+        List<String> res = new ArrayList<String>();
+        if(word == null || word.length() == 0){
+            return res;
+        }
+        helper(word, 0, 0, new StringBuilder(), res);
+        return res;
+    }
+    private void helper(String word, int pos, int count, StringBuilder sb, List<String> res){
+        int len = sb.length();
+        if(pos == word.length()){
+            if(count == 0){
+                res.add(sb.toString());
+            }else{
+                res.add(sb.append(count).toString());
+            }
+            return;
+        }else{
+            helper(word, pos + 1, count + 1, sb, res);
+            if(count != 0) {
+                sb.append(count)
+            }
+            helper(word, pos + 1, 0, sb, res);
+        }
+        sb.setLength(len);
+    }
+}
