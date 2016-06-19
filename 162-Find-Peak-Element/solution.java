b0d504fa0624a87d77e47d80290300359c976377
krunning2 <zhidongke@yahoo.com>
2016-06-19 14:18:13 -0700

    [162. Find Peak Element][Compile Error]committed by Krunning

---
+++ 162-Find-Peak-Element/solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start + 1 < end){
+            int mid = start - (start - end) / 2;
+            if(nums[mid] > nums[mid - 1]){
+                start = mid;
+            }else{
+                end = mid;
+            }
+        }
+        return A[start] > A[end] ? start : end;
+    }
+}
