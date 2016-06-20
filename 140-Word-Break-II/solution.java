889985dc490ca546979b83ba422a661debb1ec0b
krunning2 <zhidongke@yahoo.com>
2016-06-20 00:31:49 -0700

    [140. Word Break II][Time Limit Exceeded]committed by Krunning

--- 140-Word-Break-II/solution.java
+++ 140-Word-Break-II/solution.java
@@ -10,7 +10,8 @@ public class Solution {
         }
         for(int i = pos; i < s.length(); i++){
             if(dict.contains(s.substring(pos, i + 1))){
-                helper(s, dict, i+1, (cur + " "+ s.substring(pos, i + 1)));
+                String tmp = pos == 0 ? s.substring(pos, i + 1) : cur + " "+ s.substring(pos, i + 1);
+                helper(s, dict, i+1, res, tmp);
             }
         }
     }
