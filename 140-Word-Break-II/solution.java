f5a1b852d48beb0717f8270827421615f90c542f
krunning2 <zhidongke@yahoo.com>
2016-06-20 00:37:16 -0700

    [140. Word Break II][Compile Error]committed by Krunning

--- 140-Word-Break-II/solution.java
+++ 140-Word-Break-II/solution.java
@@ -1,17 +1,19 @@
 public class Solution {
     public List<String> wordBreak(String s, Set<String> wordDict) {
         List<String> res = new ArrayList<String>();
-        helper(s, wordDict, 0, res, "");
+        helper(s, wordDict, 0, res, "", new boolean[s.length()]);
         return res;
     }
-    private void helper(String s, Set<String> dict, int pos, List<String> res, String cur){
+    private void helper(String s, Set<String> dict, int pos, List<String> res, String cur, boolean[] visited){
         if(pos == s.length()) {
             res.add(cur);
         }
         for(int i = pos; i < s.length(); i++){
-            if(dict.contains(s.substring(pos, i + 1))){
+            if(visited[i] && dict.contains(s.substring(pos, i + 1))){
                 String tmp = pos == 0 ? s.substring(pos, i + 1) : cur + " "+ s.substring(pos, i + 1);
+                int size = res.size();
                 helper(s, dict, i+1, res, tmp);
+                if(size == res.size()) visited[i] = false;
             }
         }
     }
