c30eb61b861f5daddb749e361f2868b54d6e75ec
krunning2 <zhidongke@yahoo.com>
2016-06-19 23:24:00 -0700

    [314. Binary Tree Vertical Order Traversal][Accepted]committed by Krunning

--- 314-Binary-Tree-Vertical-Order-Traversal/solution.java
+++ 314-Binary-Tree-Vertical-Order-Traversal/solution.java
@@ -10,32 +10,26 @@
 public class Solution {
     public List<List<Integer>> verticalOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
-        Map<Integer, List<Pair>> map = new HashMap<Integer, List<Pair>>();
+        if(root == null) return res;
+        Map<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
         Queue<Pair> q = new LinkedList<Pair>();
         Pair p = new Pair(0, root);
         q.offer(p);
-        map.put(0, new ArrayList<Pair>(){
-            {
-                add(p);
-            }
-        });
         while(!q.isEmpty()){
             int size = q.size();
             for(int i = 0; i < size; i++){
                 Pair cur = q.poll();
+                map.putIfAbsent(cur.col, new ArrayList<Integer>());
+                map.get(cur.col).add(cur.node.val);
                 if(cur.node.left != null){
                     Pair tmp = new Pair(cur.col - 1, cur.node.left);
                     q.offer(tmp);
-                    map.putIfAbsent(cur.col - 1, new ArrayList<Pair>());
-                    map.get(cur.col - 1).add(tmp);
                 }
                 if(cur.node.right != null){
                     Pair tmp = new Pair(cur.col + 1, cur.node.right);
                     q.offer(tmp);
-                    map.putIfAbsent(cur.col + 1, new ArrayList<Pair>());
-                    map.get(cur.col + 1).add(tmp);
                 }
                 max = Math.max(max, cur.col);
                 min = Math.min(min, cur.col);
