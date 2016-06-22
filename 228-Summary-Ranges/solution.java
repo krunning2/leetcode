9ba7b9d379e4ee56b7e0911c05bea03baba9232d
krunning2 <zhidongke@yahoo.com>
2016-06-21 21:44:02 -0700

    [228. Summary Ranges][Compile Error]committed by Krunning

---
+++ 228-Summary-Ranges/solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> res = new ArrayList<String>();
+        if(nums == null || nums.length == 0) return res;
+        int next = nums[0];
+        int pre = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            if(next == nums[i]){
+                next++;
+                continue;
+            }
+            if(next < nums[i]){
+                res.add(print(pre, next - 1));
+                pre = nums[i];
+                next = nums[i];
+            }
+        }
+        res.add(print(pre, next - 1));
+    }
+    private String print(int a, int b){
+        return a >= b ? String.valueOf(a) : a + "->" + b;
+    }
+}
