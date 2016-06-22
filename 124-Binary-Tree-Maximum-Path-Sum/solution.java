413bee2996fb90751ee107050c8af816d1f74e21
krunning2 <zhidongke@yahoo.com>
2016-06-21 23:11:25 -0700

    [124. Binary Tree Maximum Path Sum][Accepted]committed by Krunning

--- 124-Binary-Tree-Maximum-Path-Sum/solution.java
+++ 124-Binary-Tree-Maximum-Path-Sum/solution.java
@@ -11,16 +11,16 @@ public class Solution {
     public int maxPathSum(TreeNode root) {
         if(root == null) return Integer.MIN_VALUE;
         int[] max = {Integer.MIN_VALUE};
-        find(node, root.val, max);
+        find(root, max);
         return max[0];
     }
-    private int find(TreeNode node, int cur, int[] max){
+    private int find(TreeNode node, int[] max){
         if(node == null) return 0;
-        int left = find(node.left, cur + node.val, max);
-        int right = find(node.right, cur + node.val, max);
-        int branchMax = Math.max(left, right);
-        int curMax = Math.max(branchMax, cur);
-        max[0] = Math.max(max[0], curMax);
-        return curMax;
+        int left = find(node.left, max);
+        int right = find(node.right, max);
+        int branchMax = Math.max(Math.max(left, right) + node.val, node.val);
+        int crossMax = Math.max(branchMax, left + right + node.val);
+        max[0] = Math.max(max[0], crossMax);
+        return branchMax;
     }
 }
