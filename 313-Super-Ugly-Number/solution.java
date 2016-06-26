89554d6025e0510dc87bfa0ffdbc0a06a0a73e52
krunning2 <zhidongke@yahoo.com>
2016-06-26 15:49:28 -0700

    [313. Super Ugly Number][Compile Error]committed by Krunning

---
+++ 313-Super-Ugly-Number/solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    //2 4 7 8 13 14 16  19 2*7 
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        List<Integer> list = new ArrayList<Integer>();
+        int[] index = new int[primes.length];
+        list.add(1);
+        while(list.size < n){
+            int min = Integer.MAX_VALUE;
+            for(int i : index){
+                min = Math.min(min, list.get(index[i]) * primes[i]);
+            }
+            list.add(min);
+            for(int i : index){
+                if(min == list.get(index[i]) * primes[i]){
+                    index[i]++;
+                }
+            }
+        }
+        return list.get(n - 1);
+    }
+}
