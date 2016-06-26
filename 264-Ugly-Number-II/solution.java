dc2dd4bd8550a88fe5f53a3741dbf3a9083cf9b3
krunning2 <zhidongke@yahoo.com>
2016-06-26 15:05:06 -0700

    [264. Ugly Number II][Compile Error]committed by Krunning

---
+++ 264-Ugly-Number-II/solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int nthUglyNumber(int n) {
+        if(n <= 0) return -1;
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        int i2, i3, i5 = 0;
+        int m2, m3, m5 = 0;
+        while(list.size() < n){
+            m2 = list.get(i2) * 2;
+            m3 = list.get(i3) * 3;
+            m5 = list.get(i5) * 3;
+            int min = Math.min(m2, Math.min(m3, m5));
+            if(min == m2) i2 ++;
+            if(min == m3) i3 ++;
+            if(min == m5) i5 ++;
+            list.add(min);
+        }
+        return list.get(n - 1);
+    }
+}
