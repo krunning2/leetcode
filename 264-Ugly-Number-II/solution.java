ffe323ae89b9d08dfb3f718943906a524e1ba342
krunning2 <zhidongke@yahoo.com>
2016-06-26 15:06:27 -0700

    [264. Ugly Number II][Accepted]committed by Krunning

--- 264-Ugly-Number-II/solution.java
+++ 264-Ugly-Number-II/solution.java
@@ -3,12 +3,12 @@ public class Solution {
         if(n <= 0) return -1;
         List<Integer> list = new ArrayList<Integer>();
         list.add(1);
-        int i2, i3, i5 = 0;
-        int m2, m3, m5 = 0;
+        int i2 = 0, i3 = 0, i5 = 0;
+        int m2 = 0, m3 = 0, m5 = 0;
         while(list.size() < n){
             m2 = list.get(i2) * 2;
             m3 = list.get(i3) * 3;
-            m5 = list.get(i5) * 3;
+            m5 = list.get(i5) * 5;
             int min = Math.min(m2, Math.min(m3, m5));
             if(min == m2) i2 ++;
             if(min == m3) i3 ++;
