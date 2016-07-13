38f57ad47ae8dea6f3727f49c06f1f488351acd2
krunning2 <zhidongke@yahoo.com>
2016-07-12 20:34:42 -0700

    [276. Paint Fence][Accepted]committed by Krunning

--- 276-Paint-Fence/solution.java
+++ 276-Paint-Fence/solution.java
@@ -2,12 +2,12 @@ public class Solution {
     public int numWays(int n, int k) {
         if(n <= 0 || k <= 0) return 0;
         if(n == 1) return k;
-        int[] dp = new int[n + 1];
+        int[] dp = new int[4];
         dp[1] = k;
         dp[2] = k * k;
         for(int i = 3; i <= n; i++){
-            dp[i] = (dp[i - 1] + dp[i - 2]) * (k - 1);
+            dp[i % 3] = (dp[(i - 1) % 3] + dp[(i - 2) % 3]) * (k - 1);
         }
-        return dp[n];
+        return dp[n % 3];
     }
 }
