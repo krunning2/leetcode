8378ee20e6da4ef07774f9d41b7b1897d155ede0
krunning2 <zhidongke@yahoo.com>
2016-07-12 20:33:31 -0700

    [276. Paint Fence][Compile Error]committed by Krunning

--- 276-Paint-Fence/solution.java
+++ 276-Paint-Fence/solution.java
@@ -6,7 +6,7 @@ public class Solution {
         dp[1] = k;
         dp[2] = k * k;
         for(int i = 3; i <= n; i++){
-            dp[i] =() dp[i - 1] + dp[i - 2]) * (k - 1);
+            dp[i] = dp[i - 1] + dp[i - 2]) * (k - 1);
         }
         return dp[n];
     }
