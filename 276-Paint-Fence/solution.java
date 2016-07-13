fe92d3c410ed4fbf2921ed52e5265fc5d2dc523c
krunning2 <zhidongke@yahoo.com>
2016-07-12 20:33:20 -0700

    [276. Paint Fence][Compile Error]committed by Krunning

--- 276-Paint-Fence/solution.java
+++ 276-Paint-Fence/solution.java
@@ -1,17 +1,13 @@
 public class Solution {
     public int numWays(int n, int k) {
-        if(n <= 0 || k <=0){
-            return 0;
-        }
-        if(n == 1){
-            return k;
-        }
-        int[] dp = new int[4];
+        if(n <= 0 || k <= 0) return 0;
+        if(n == 1) return k;
+        int[] dp = new int[n + 1];
         dp[1] = k;
         dp[2] = k * k;
-        for(int i = 3; i <=n; i++){
-            dp[i % 3] = (k - 1) * (dp[(i - 1) % 3] + dp[(i - 2) % 3]);
+        for(int i = 3; i <= n; i++){
+            dp[i] =() dp[i - 1] + dp[i - 2]) * (k - 1);
         }
-        return dp[n % 3];
+        return dp[n];
     }
 }
