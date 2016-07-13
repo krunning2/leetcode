964a1c62aa077a3adab9594e5bb00f911c032b06
krunning2 <zhidongke@yahoo.com>
2016-07-12 23:01:39 -0700

    [311. Sparse Matrix Multiplication][Runtime Error]committed by Krunning

---
+++ 311-Sparse-Matrix-Multiplication/solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[][] multiply(int[][] A, int[][] B) {
+        int[][] res = new int[A.length][A[0].length];
+        int m = A.length;
+        int n = A[0].length;
+        for(int i = 0; i < m; i++){
+            for(int k = 0; k < n; k++){
+                if(A[i][k] != 0){
+                    for(int j = 0; j < n; j++){
+                        res[i][j] += A[i][j] * B[j][i];
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
