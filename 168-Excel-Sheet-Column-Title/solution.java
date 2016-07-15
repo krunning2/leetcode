35a8f364063d44d99e21ce4208d9f2791738e14a
krunning2 <zhidongke@yahoo.com>
2016-07-15 08:51:43 -0700

    [168. Excel Sheet Column Title][Accepted]committed by Krunning

--- 168-Excel-Sheet-Column-Title/solution.java
+++ 168-Excel-Sheet-Column-Title/solution.java
@@ -2,7 +2,7 @@ public class Solution {
     public String convertToTitle(int n) {
         String res="";
         while(n>0){
-    		res=char('A'+(n-1)%26)+res;
+    		res=(char)('A'+(n-1)%26)+res;
     		n=(n-1)/26;
     	}
     	return res;
