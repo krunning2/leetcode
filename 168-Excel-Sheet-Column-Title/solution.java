a83c32de1e60f33df5d0e23ee19463137ee9ac03
krunning2 <zhidongke@yahoo.com>
2016-07-15 08:41:06 -0700

    [168. Excel Sheet Column Title][Compile Error]committed by Krunning

--- 168-Excel-Sheet-Column-Title/solution.java
+++ 168-Excel-Sheet-Column-Title/solution.java
@@ -2,7 +2,7 @@ public class Solution {
     public String convertToTitle(int n) {
         StringBuilder sb = new StringBuilder();
         while(n > 0){
-            sb.append(letters[(n - 1) % 26 + 'A']);
+            sb.append(Character.valueOf((n - 1) % 26 + 'A'));
             n = (n - 1) / 26;
         }
         return sb.toString();
