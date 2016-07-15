0e9fd17c638d0726b2c892fa74dc84ca1eeee65e
krunning2 <zhidongke@yahoo.com>
2016-07-15 09:33:30 -0700

    [221. Maximal Square][Runtime Error]committed by Krunning

--- 221-Maximal-Square/solution.java
+++ 221-Maximal-Square/solution.java
@@ -4,19 +4,21 @@ public class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
         int[][] dp = new int[2][n];
+        int max = 0;
         for(int i = 0; i < m; i++){
             dp[i % 2][0] = matrix[i][0];
+            max = Math.max(max, dp[0][i]);
         }
         for(int i = 0; i < n; i++){
-            dp[0][i % 2] = matrix[0][i];
+            dp[0][i] = matrix[0][i];
+            max = Math.max(max, dp[0][i]);
         }
-        int max = 0;
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){
                 if(matrix[i][j] == 0){
                     dp[i % 2][j] = 0;
                 }else{
-                    dp[i % 2][j] = Math.min(dp[(i - 1 % 2)][j - 1], Math.min(dp[i % 2][j], dp[(i - 1) % 2][j])) + 1;
+                    dp[i % 2][j] = Math.min(dp[(i - 1) % 2][j - 1], Math.min(dp[i % 2][j], dp[(i - 1) % 2][j])) + 1;
                 }
                 max = Math.max(max, dp[i % 2][j]);
             }
