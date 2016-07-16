cb8e8987ebbbbc44ead608103559b22918065dd5
krunning2 <zhidongke@yahoo.com>
2016-07-16 14:32:54 -0700

    [221. Maximal Square][Accepted]committed by Krunning

--- 221-Maximal-Square/solution.java
+++ 221-Maximal-Square/solution.java
@@ -1,26 +1,24 @@
 public class Solution {
     public int maximalSquare(char[][] matrix) {
-        if(matrix == null || matrix.length == 0) return 0;
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int[][] dp = new int[2][n];
+        if(matrix.length == 0) return 0;
+        int m = matrix.length, n = matrix[0].length;
         int max = 0;
+        int[][] dp = new int[m][n];
+        // 第一列赋值
         for(int i = 0; i < m; i++){
-            dp[i % 2][0] = matrix[i][0];
-            max = Math.max(max, dp[0][i]);
+            dp[i][0] = matrix[i][0] - '0';
+            max = Math.max(max, dp[i][0]);
         }
+        // 第一行赋值
         for(int i = 0; i < n; i++){
-            dp[0][i] = matrix[0][i];
+            dp[0][i] = matrix[0][i] - '0';
             max = Math.max(max, dp[0][i]);
         }
+        // 递推
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){
-                if(matrix[i][j] == 0){
-                    dp[i % 2][j] = 0;
-                }else{
-                    dp[i % 2][j] = Math.min(dp[(i - 1) % 2][j - 1], Math.min(dp[i % 2][j], dp[(i - 1) % 2][j])) + 1;
-                }
-                max = Math.max(max, dp[i % 2][j]);
+                dp[i][j] = matrix[i][j] == '1' ? Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1])) + 1 : 0;
+                max = Math.max(max, dp[i][j]);
             }
         }
         return max * max;
