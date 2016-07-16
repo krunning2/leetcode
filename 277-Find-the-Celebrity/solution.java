c845b3328e4119e4ed8acbd8935a0c5f6e80ae67
krunning2 <zhidongke@yahoo.com>
2016-07-16 16:06:22 -0700

    [277. Find the Celebrity][Runtime Error]committed by Krunning

---
+++ 277-Find-the-Celebrity/solution.java
@@ -0,0 +1,21 @@
+/* The knows API is defined in the parent class Relation.
+      boolean knows(int a, int b); */
+
+public class Solution extends Relation {
+    public int findCelebrity(int n) {
+        int res = -1;
+        for(int i = 0; i < n; i++){
+            if(!knows(i, res)){
+                res = i;
+            }
+        }
+        for(int i = 0; i < n; i++){
+            if(res != i){
+                if(knows(res, i) || !knows(i, res)){
+                    return -1;
+                }
+            }
+        }
+        return res;
+    }
+}
