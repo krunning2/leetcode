bc1ad4f907e071f9b724561c3c9f73a9a3e3c037
krunning2 <zhidongke@yahoo.com>
2016-07-19 22:20:35 -0700

    [209. Minimum Size Subarray Sum][Compile Error]committed by Krunning

---
+++ 209-Minimum-Size-Subarray-Sum/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int minSubArrayLen(int s, int[] nums) {
+        int sum = 0;
+        int left = 0, right = 0, int len = nums.length, res = nums.length + 1;
+        while(right < len){
+            while(right < len && sum < s){
+                sum += nums[right ++];
+            }
+            while(sum >= s){
+                res = Math.min(res, right - left);
+                sum -= nums[left --];
+            }
+        }
+        return res == nums.length + 1 ? 0 : res;
+    }
+}
