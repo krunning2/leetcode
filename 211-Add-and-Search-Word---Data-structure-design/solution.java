38cb82b40d890b25ee4d7f3adae3ce6bcee76613
krunning2 <zhidongke@yahoo.com>
2016-07-19 23:09:15 -0700

    [211. Add and Search Word - Data structure design][Compile Error]committed by Krunning

--- 211-Add-and-Search-Word---Data-structure-design/solution.java
+++ 211-Add-and-Search-Word---Data-structure-design/solution.java
@@ -40,7 +40,7 @@ public class WordDictionary {
             }
             char c = s.charAt(pos);
             if(c == '.'){
-                for(Node child : c.map.values{
+                for(Node child : c.map.values){
                     if(searchHelper(word, pos + 1, child)) return true;
                 }
                 return false;
