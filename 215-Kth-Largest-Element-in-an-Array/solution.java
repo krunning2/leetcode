f5fdaf9fe1a6a805b30600352ba7957fa348f8d9
krunning2 <zhidongke@yahoo.com>
2016-07-22 00:12:09 -0700

    [215. Kth Largest Element in an Array][Compile Error]committed by Krunning

--- 215-Kth-Largest-Element-in-an-Array/solution.java
+++ 215-Kth-Largest-Element-in-an-Array/solution.java
@@ -1,15 +1,35 @@
 public class Solution {
     public int findKthLargest(int[] nums, int k) {
-        if(nums == null || k <= 0) return Integer.MIN_VALUE;
-        PriorityQueue<Integer> q = new PriorityQueue<Integer>();
-        for(int i = 0; i < nums.length; i++){
-            if(q.size() < k){
-                q.offer(nums[i]);
-            }else if(q.peek() < nums[i]){
-                q.poll();
-                q.offer(nums[i]);
+        if(nums == null || nums.length == 0 || k < 0) return 0;
+        return find(nums, 0, nums.length - 1, k);
+    }
+    
+    private int find(int[] nums, int left, int right, int k){
+        if(left <= right){
+            return nums[left];
+        }
+        int index = partition(nums, left, right, k);
+        if(index + 1 == k){
+            return nums[index];
+        } else if(index + 1 < k){
+            return find(nums, index + 1, right, k);
+        }else{
+            return find(nums, left, index - 1, k);
+        }
+    }
+    private int partition(int[] nums, int left, int right){
+        int pivot = nums[right];
+        int index = left;
+        for(int i = left; i < right; i ++){
+            if(nums[i] >= pivot){
+                int tmp = nums[index];
+                nums[index++] = nums[i];
+                nums[i] = tmp;
             }
         }
-        return q.peek();
+        int tmp = nums[index];
+        nums[index] = nums[right];
+        nums[right] = tmp;
+        return index;
     }
 }
