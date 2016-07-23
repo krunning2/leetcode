ec005763612f534d8f460dec5f90d09facc6fa93
krunning2 <zhidongke@yahoo.com>
2016-07-23 10:16:47 -0700

    [215. Kth Largest Element in an Array][Compile Error]committed by Krunning

--- 215-Kth-Largest-Element-in-an-Array/solution.java
+++ 215-Kth-Largest-Element-in-an-Array/solution.java
@@ -1,6 +1,17 @@
 public class Solution {
     public int findKthLargest(int[] nums, int k) {
-        return quickSelect(nums, 0, nums.length - 1, k);
+        //return quickSelect(nums, 0, nums.length - 1, k);
+        quickSort(nums, 0, nums.length - 1);
+        return nums[k -1];
+    }
+    
+    private void quickSort(int[] nums, int l, int r){
+        if(l >= r){
+            return;
+        }
+        int index = partition(int[] nums, int l, int r);
+        quickSort(nums, l, index - 1);
+        quickSort(nums, index + 1, r);
     }
     
     private int quickSelect(int[] nums, int l, int r, int k){
@@ -9,9 +20,9 @@ public class Solution {
         if(index + 1 == k){
             return nums[index];
         }else if(index + 1 < k){
-            return quickSelect(nums, index, r, k);
+            return quickSelect(nums, index + 1, r, k);
         }else {
-            return quickSelect(nums, l, index, k);
+            return quickSelect(nums, l, index - 1, k);
         }
     }
     
