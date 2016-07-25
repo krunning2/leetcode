0c13586346044f9ef6468e1d0608c9614177b63c
krunning2 <zhidongke@yahoo.com>
2016-07-25 09:19:45 -0700

    [170. Two Sum III - Data structure design][Time Limit Exceeded]committed by Krunning

--- 170-Two-Sum-III---Data-structure-design/solution.java
+++ 170-Two-Sum-III---Data-structure-design/solution.java
@@ -1,25 +1,28 @@
 public class TwoSum {
-    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-    // Add the number to an internal data structure.
-	public void add(int num) {
-	    if(map.containsKey(num)){
-	        map.put(num, 2);
-	    }else{
-	        map.put(num, 1);
-	    }
-	}
-
-    // Find if there exists any pair of numbers which sum is equal to the value.
-	public boolean find(int value) {
-	    for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-	        int num1 = entry.getKey();
-	        int num2 = value - num1;
-	        if(map.containsKey(num2) && (entry.getValue() > 1 || num1 != num2)){
-	              return true;
-	        }
-	    }
-	    return false;
-	}
+    Set<Integer> sum;
+        Set<Integer> num;
+        
+        TwoSum(){
+            sum = new HashSet<Integer>();
+            num = new HashSet<Integer>();
+        }
+        // Add the number to an internal data structure.
+    	public void add(int number) {
+    	    if(num.contains(number)){
+    	        sum.add(number * 2);
+    	    }else{
+    	        Iterator<Integer> iter = num.iterator();
+    	        while(iter.hasNext()){
+    	            sum.add(iter.next() + number);
+    	        }
+    	        num.add(number);
+    	    }
+    	}
+    
+        // Find if there exists any pair of numbers which sum is equal to the value.
+    	public boolean find(int value) {
+    	    return sum.contains(value);
+    	}
 }
 
 
