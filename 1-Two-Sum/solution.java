c9ab53ae03ff810b5fbea7b2e330d8d62a72728f
krunning2 <zhidongke@yahoo.com>
2016-07-25 08:43:44 -0700

    [1. Two Sum][Runtime Error]committed by Krunning

---
+++ 1-Two-Sum/solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i]) && i != map.get(nums[i])){
+                return new int[]{map.get(nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {0,0};
+    }
+}
