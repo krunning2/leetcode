c0c753dc6443e9d90960d8a92b95c33149ab8450
krunning2 <zhidongke@yahoo.com>
2016-07-25 08:47:21 -0700

    [167. Two Sum II - Input array is sorted][Accepted]committed by Krunning

--- 167-Two-Sum-II---Input-array-is-sorted/solution.java
+++ 167-Two-Sum-II---Input-array-is-sorted/solution.java
@@ -1,5 +1,5 @@
 public class Solution {
-    public int[] twoSum(int[] numbers, int target) {
+    public int[] twoSum(int[] nums, int target) {
         int  p1 = 0;
         int p2 = nums.length - 1;
         while(p1 < p2){
