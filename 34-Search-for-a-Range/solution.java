623f61bcb6c27c412aa442fff1d7e852ed63ba17
krunning2 <zhidongke@yahoo.com>
2016-07-25 22:53:24 -0700

    [34. Search for a Range][Compile Error]committed by Krunning

---
+++ 34-Search-for-a-Range/solution.java
@@ -0,0 +1,40 @@
+public class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] res = {-1, -1};
+        if(nums == null || nums.length <= 0) return res;
+        int s = 0;
+        int e = nums.length - 1;
+        while(s + 1 < e){
+            int mid = s - (s - end) / 2;
+            if(nums[mid] >= target){
+                e = mid;
+            }else{
+                s = mid;
+            }
+        }
+        if(nums[s] == target){
+            res[0] = s;
+        }else if(nums[s] == target){
+            res[0] = e;
+        }else{
+            return res;
+        }
+        
+        s = res[0];
+        int e = nums.length - 1;
+        while(s + 1 < e){
+            int mid = s - (s - end) / 2;
+            if(nums[mid] > target){
+                e = mid;
+            }else{
+                s = mid;
+            }
+        }
+        if(nums[e] == target){
+            res[1] = e;
+        }else if(nums[s] == target){
+            res[1] = s;
+        }
+        return res;
+    }
+}
