a5dcd5f9c4460ff332c1bbe6c796bdc99901436d
krunning2 <zhidongke@yahoo.com>
2016-07-26 21:03:15 -0700

    [103. Binary Tree Zigzag Level Order Traversal][Compile Error]committed by Krunning

---
+++ 103-Binary-Tree-Zigzag-Level-Order-Traversal/solution.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        if(root == null) return res;
+        queue.offer(root);
+        boolean flag = ture;
+        while(! queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> tmp = new ArrayList<Integer>();
+            for(int i = 0; i < size; i++){
+                TreeNode node = queue.poll();
+                tmp.add(node.val);
+                if(node.left != null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
+            }
+            if(flag){
+                Arrays.reverse(tmp);
+                flag = !flag;
+            }
+            res.add(tmp);
+        }
+        return res;
+    }
+}
