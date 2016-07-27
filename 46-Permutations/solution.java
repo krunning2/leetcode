4f84e5de4fcbad0e0f9df1f72200923a9c004917
krunning2 <zhidongke@yahoo.com>
2016-07-26 21:22:42 -0700

    [46. Permutations][Compile Error]committed by Krunning

---
+++ 46-Permutations/solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        helper(nums, new boolean[nums.length], res, new ArrayList());
+        return res;
+    }
+    
+    private void helper(int[] nums, boolean[] visited, List<List<Integer>> res, List<Integer> cur){
+        if(cur.size == nums.length){
+            res.add(cur);
+            return;
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(!visited[i]){
+                cur.add(nums[i]);
+                visited[i] = true;
+                helper(nums, visited, res, cur);
+                visited[i] = false;
+                cur.remove(cur.size() - 1);
+            }
+        }
+    }
+}
