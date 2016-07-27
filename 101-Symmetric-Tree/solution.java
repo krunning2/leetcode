6a61674cc3e2e5b661c3940f68f9dcac4ccc8951
krunning2 <zhidongke@yahoo.com>
2016-07-26 20:46:45 -0700

    [101. Symmetric Tree][Runtime Error]committed by Krunning

---
+++ 101-Symmetric-Tree/solution.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        return helper(root.left, root.right);
+    }
+    private boolean helper(TreeNode left, TreeNode right){
+        if(left == null || right == null) return left == null && right == null;
+        if(left.val != right.val) return false;
+        if(left.left == null && right.right != null || right.right == null && left.left != null) return false;
+        if(left.left != null && right.right != null && (left.left.val != right.right.val || left.right.val != right.left.val)) return false;
+        return true;
+    }
+}
