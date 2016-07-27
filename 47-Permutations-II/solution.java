04f25c8ee32ba3e7a7355b9fe0e8484a7a310e5d
krunning2 <zhidongke@yahoo.com>
2016-07-26 21:26:25 -0700

    [47. Permutations II][Runtime Error]committed by Krunning

---
+++ 47-Permutations-II/solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        helper(nums, new boolean[nums.length], res, new ArrayList());
+        return res;
+    }
+     private void helper(int[] nums, boolean[] visited, List<List<Integer>> res, List<Integer> cur){
+        if(cur.size() == nums.length){
+            res.add(new ArrayList(cur));
+            return;
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(!visited[i] && (i == 1 || i != 1 && nums[i] != nums[i - 1])){
+                cur.add(nums[i]);
+                visited[i] = true;
+                helper(nums, visited, res, cur);
+                visited[i] = false;
+                cur.remove(cur.size() - 1);
+            }
+        }
+    }
+}
