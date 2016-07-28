432f9045588961662a96462ef36ca026c36c7461
krunning2 <zhidongke@yahoo.com>
2016-07-27 21:49:45 -0700

    [337. House Robber III][Compile Error]committed by Krunning

--- 337-House-Robber-III/solution.java
+++ 337-House-Robber-III/solution.java
@@ -9,34 +9,22 @@
  */
 public class Solution {
     public int rob(TreeNode root) {
-        if(root == null) return 0;
-        int[] dp = new int[3];
-        dp[0] = root.val;
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
-        int level = 0;
-        queue.offer(root);
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            int sum = 0;
-            for(int i = 0; i < size; i++){
-                TreeNode cur = queue.poll();
-                sum += cur.val;
-                if(cur.left != null){
-                    queue.offer(cur.left);
-                }
-                if(cur.right != null){
-                    queue.offer(cur.right);
-                }
-            }
-            if(level == 0){
-                dp[0] = sum;
-            }else if(level == 1){
-                dp[1] = Math.max(dp[0], sum);
-            }else{
-                dp[level % 3] = Math.max(dp[(level - 1) % 3], dp[(level - 2) % 3] + sum);
-            }
-            level++;
+        return helper(root, new HashMap());
+    }
+    private int helper(TreeNode node, HashMap<TreeNode, Integer> map){
+        if(node == null){
+            return 0;
         }
-        return dp[(level - 1) % 3]
+        if(map.containsKey(node)) return map.get(node);
+        int val = 0;
+        if(node.left != null){
+            val += helper(node.left.left) + helper(node.left.right);
+        }
+        if(node.right != null){
+            val += helper(node.right.left) + helper(node.right.right);
+        }
+        val = Math.max(val + node.val, helper(node.left, map) + helper(node.right, map));
+        map.put(node, val);
+        return val;
     }
 }
