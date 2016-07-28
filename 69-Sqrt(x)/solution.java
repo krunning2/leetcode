784cf1bd87a30151f839884f23ed313ec27b2a96
krunning2 <zhidongke@yahoo.com>
2016-07-28 00:16:11 -0700

    [69. Sqrt(x)][Compile Error]committed by Krunning

---
+++ 69-Sqrt(x)/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int mySqrt(int num) {
+        if (num < 1) return false;
+        long left = 1, right = num / 2 + 1;// long type to avoid 2147483647 case
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long t = mid * mid;
+            if (t > num) {
+              right = mid - 1;
+            } else if (t < num) {
+              left = mid + 1;
+            } else {
+              return mid;
+            }
+        }
+        return 0;
+    }
+}
