6e48509114677681a26ada959c05db9da197f31d
krunning2 <zhidongke@yahoo.com>
2016-07-27 23:41:46 -0700

    [127. Word Ladder][Compile Error]committed by Krunning

---
+++ 127-Word-Ladder/solution.java
@@ -0,0 +1,43 @@
+public class Solution {
+    public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+        wordList.add(beginWord);
+        wordList.add(endWord);
+        int length = 1;
+        Queue<String> queue = new LinkedList<String>();
+        queue.offer(beginWord);
+        Set<String> set = new HashSet();
+        set.add(start);
+        while(!queue.isEmpty()){
+            int size = 0;
+            level ++;
+            for(int i = 0; i < size; i++){
+                String cur = queue.poll();
+                for(int j = 0; j < cur.length(); j++){
+                    for(String tmp : getString(cur, wordList, j)){
+                        if(tmp.equals(endWord)) return level;
+                        if(!set.contains(tmp)){
+                            queue.offer(tmp);
+                            set.add(tmp);
+                        }
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+    private List<String> getString(String s, Set<String> set, int pos){
+        List<String> res = new ArrayList();
+        for(char c = 'a'; c <= 'z'; c++){
+            String ss = swap(s, pos, c);
+            if(c != s.charAt(pos) && set.contains(ss)){
+                res.add(ss);
+            }
+        }
+        return res;
+    }
+    private String swap(String s, int p1, char c){
+        char[] tmp = s.toCharArray();
+        tmp[p1] = c;
+        return new String(tmp);
+    }
+}
