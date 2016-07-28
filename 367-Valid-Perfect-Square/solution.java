3f166a8d34fd089662909762cf6dd781894056f6
krunning2 <zhidongke@yahoo.com>
2016-07-28 00:14:35 -0700

    [367. Valid Perfect Square][Accepted]committed by Krunning

--- 367-Valid-Perfect-Square/solution.java
+++ 367-Valid-Perfect-Square/solution.java
@@ -1,20 +1,19 @@
 public class Solution {
     public boolean isPerfectSquare(int num) {
         if (num < 1) return false;
-      long left = 1, right = num;// long type to avoid 2147483647 case
-    
-      while (left <= right) {
-        long mid = left + (right - left) / 2;
-        long t = mid * mid;
-        if (t > num) {
-          right = mid - 1;
-        } else if (t < num) {
-          left = mid + 1;
-        } else {
-          return true;
+        long left = 1, right = num / 2 + 1;// long type to avoid 2147483647 case
+        
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long t = mid * mid;
+            if (t > num) {
+              right = mid - 1;
+            } else if (t < num) {
+              left = mid + 1;
+            } else {
+              return true;
+            }
         }
-      }
-    
-      return false;
+            return false;
     }
 }
