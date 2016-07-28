1075050b5c4046762c7f2fd54376341301ddd034
krunning2 <zhidongke@yahoo.com>
2016-07-28 00:10:54 -0700

    [367. Valid Perfect Square][Compile Error]committed by Krunning

--- 367-Valid-Perfect-Square/solution.java
+++ 367-Valid-Perfect-Square/solution.java
@@ -1,10 +1,18 @@
 public class Solution {
     public boolean isPerfectSquare(int num) {
-        int i = 1;
-        while(num > 0){
-            num -= i;
-            i += 2;
+        int s = 1;
+        int e = num / 2;
+        while(s <= e){
+            int mid = s - (s - e) / 2;
+            int t = mid * mid;
+            if(t == num){
+                return true;
+            }else if(t < num){
+                s = mid + 1;
+            }else{
+                end = mid - 1;
+            }
         }
-        return num == 0;
+        return false;
     }
 }
