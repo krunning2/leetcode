48cac77b0b73120e547060cbe65fc402466b4cf9
krunning2 <zhidongke@yahoo.com>
2016-07-27 23:47:44 -0700

    [127. Word Ladder][Accepted]committed by Krunning

--- 127-Word-Ladder/solution.java
+++ 127-Word-Ladder/solution.java
@@ -1,25 +1,23 @@
 public class Solution {
-    public int ladderLength(String start, String end, Set<String> dict) {
-        // write your code here
-        Set<String> set = new HashSet<String>();
-        Queue<String> queue = new LinkedList<String>();
-        dict.add(start);
-        dict.add(end);
-        queue.offer(start);
-        set.add(start);
+    public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+        wordList.add(beginWord);
+        wordList.add(endWord);
         int level = 1;
-        while(! queue.isEmpty()){
-            level ++;
+        Queue<String> queue = new LinkedList<String>();
+        queue.offer(beginWord);
+        Set<String> set = new HashSet();
+        set.add(beginWord);
+        while(!queue.isEmpty()){
             int size = queue.size();
+            level ++;
             for(int i = 0; i < size; i++){
                 String cur = queue.poll();
                 for(int j = 0; j < cur.length(); j++){
-                    for(String s : getStrings(cur, j, dict)){
-                        if(end.equals(s)){
-                            return level;
-                        }else if(!set.contains(s)){
-                            set.add(s);
-                            queue.offer(s);
+                    for(String tmp : getString(cur, wordList, j)){
+                        if(tmp.equals(endWord)) return level;
+                        if(!set.contains(tmp)){
+                            queue.offer(tmp);
+                            set.add(tmp);
                         }
                     }
                 }
@@ -27,19 +25,19 @@ public class Solution {
         }
         return 0;
     }
-    
-    private List<String> getStrings(String s, int pos, Set<String> dict){
-        List<String> res = new ArrayList<String>();
+    private List<String> getString(String s, Set<String> set, int pos){
+        List<String> res = new ArrayList();
         for(char c = 'a'; c <= 'z'; c++){
-            if(s.charAt(pos) != c && dict.contains(replace(s, pos, c))){
-                res.add(replace(s, pos, c));
+            String ss = swap(s, pos, c);
+            if(c != s.charAt(pos) && set.contains(ss)){
+                res.add(ss);
             }
         }
         return res;
     }
-    private String replace(String s, int pos, char c){
+    private String swap(String s, int p1, char c){
         char[] tmp = s.toCharArray();
-        tmp[pos] = c;
+        tmp[p1] = c;
         return new String(tmp);
     }
 }
