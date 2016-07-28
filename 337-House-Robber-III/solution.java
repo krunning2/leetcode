fcd387a86989d5c9ca8ed6e1833a13810649a9f7
krunning2 <zhidongke@yahoo.com>
2016-07-27 22:07:25 -0700

    [337. House Robber III][Accepted]committed by Krunning

--- 337-House-Robber-III/solution.java
+++ 337-House-Robber-III/solution.java
@@ -9,22 +9,18 @@
  */
 public class Solution {
     public int rob(TreeNode root) {
-        return helper(root, new HashMap());
+        int[] res = helper(root);
+        return Math.max(res[0], res[1]);
     }
-    private int helper(TreeNode node, HashMap<TreeNode, Integer> map){
-        if(node == null){
-            return 0;
+    private int[] helper(TreeNode root){
+        if(root == null){
+            return new int[2];
         }
-        if(map.containsKey(node)) return map.get(node);
-        int val = 0;
-        if(node.left != null){
-            val += helper(node.left.left, map) + helper(node.left.right, map);
-        }
-        if(node.right != null){
-            val += helper(node.right.left, map) + helper(node.right.right, map);
-        }
-        val = Math.max(val + node.val, helper(node.left, map) + helper(node.right, map));
-        map.put(node, val);
-        return val;
+        int[] left = helper(root.left);
+        int[] right = helper(root.right);
+        int[] cur = new int[2];
+        cur[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        cur[1] = left[0] + right[0] + root.val;
+        return cur;
     }
 }
