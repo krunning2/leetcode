edcf855a4cc832bfb4d86d31196fd9bbd5d164ac
krunning2 <zhidongke@yahoo.com>
2016-07-28 21:51:10 -0700

    [187. Repeated DNA Sequences][Runtime Error]committed by Krunning

---
+++ 187-Repeated-DNA-Sequences/solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        int[] map = new int[26];
+        map['C' - 'A'] = 1;
+        map['G' - 'A'] = 2;
+        map['T' - 'A'] = 3;
+        HashMap<Integer, Integer> counter = new HashMap();
+        List<String> res = new ArrayList<String>();
+        for(int i = 0; i < s.length(); i++){
+            int v = 0;
+            for(int j = i; j < i + 10; j++){
+                v = v << 2;
+                v |= map[s.charAt(j) - 'A'];
+            }
+            if(counter.containsKey(v) && counter.get(v) == 1){
+                res.add(s.substring(i, i + 10));
+                counter.put(v, 2);
+            }else{
+                counter.put(v, 1);
+            }
+        }
+        return res;
+    }
+}
