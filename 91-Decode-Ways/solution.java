ee7abd3eb9cdcd8bf181c28fea2e2470becfd931
krunning2 <zhidongke@yahoo.com>
2016-07-29 00:05:11 -0700

    [91. Decode Ways][Accepted]committed by Krunning

--- 91-Decode-Ways/solution.java
+++ 91-Decode-Ways/solution.java
@@ -5,10 +5,20 @@ public class Solution {
         if(n == 1) return check1(s.charAt(0));
         int[] dp = new int[n];
         dp[0] = check1(s.charAt(0));
-        dp[1] = check1(s.charAt(0)) * check1(s.charAt(1)) + check2(s.charAt(0),s.charAt(1));
-        for(int i = 2; i < n; i++){
-            if(check1(s.charAt(i)) == 1) dp[i] = dp[i - 1];
-            if(check2(s.charAt(i - 1), s.charAt(i)) == 1) dp[i] += dp[i - 2];
+        
+        for(int i = 1; i < n; i++){
+            int first = Integer.parseInt(s.substring(i, i + 1));
+            int second = Integer.parseInt(s.substring(i - 1, i + 1));
+            if(first >= 1 && first <= 9){
+                dp[i] = dp[i - 1];
+            }
+            if(second >= 10 && second <= 26){
+                if(i > 1){
+                    dp[i] += dp[i - 2];
+                }else{
+                    dp[i] += 1; 
+                }
+            }
         }
         return dp[n - 1];
     }
