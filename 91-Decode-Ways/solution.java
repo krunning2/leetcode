906f4ad734dce746b839168bddff6406c856b3bd
krunning2 <zhidongke@yahoo.com>
2016-07-28 23:56:01 -0700

    [91. Decode Ways][Accepted]committed by Krunning

--- 91-Decode-Ways/solution.java
+++ 91-Decode-Ways/solution.java
@@ -5,15 +5,15 @@ public class Solution {
         if(n == 1) return check1(s.charAt(0));
         int[] dp = new int[n];
         dp[0] = check1(s.charAt(0));
-        dp[1] = dp[0] + check2(s.charAt(0), s.charAt(1));
+        dp[1] = check1(s.charAt(0)) * check1(s.charAt(1)) + check2(s.charAt(0),s.charAt(1));
         for(int i = 2; i < n; i++){
             if(check1(s.charAt(i)) == 1) dp[i] = dp[i - 1];
-            if(check2(s.charAt(i-1),s.charAt(i))==1) dp[i] += dp[i - 2];
+            if(check2(s.charAt(i - 1), s.charAt(i)) == 1) dp[i] += dp[i - 2];
         }
         return dp[n - 1];
     }
     private int check1(char c){
-        return c == '0' : 0 : 1;
+        return c == '0' ? 0 : 1;
     }
     private int check2(char c1, char c2){
         return c1 == '1' || c1 == '2' && c2 <= '6' ? 1 : 0;
