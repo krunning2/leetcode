55a9011f7dfe4ad2cd2f919e1227a42942b0b6b0
krunning2 <zhidongke@yahoo.com>
2016-07-29 22:58:38 -0700

    [125. Valid Palindrome][Compile Error]committed by Krunning

---
+++ 125-Valid-Palindrome/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public boolean isPalindrome(String s) {
+        String tirmed = s.trim().toLowerCase();
+        int start = 0;
+        int end = s.length() - 1;
+        while(start <= end){
+            if(check(s.charAt(start)) && check(s.charAt(end)) && s.charAt(start) != s.chatAt(end)){
+                return false;
+            }
+            if(check(s.charAt(start))){
+                start ++;
+            }
+            if(check(s.charAt(end))){
+                end --;
+            }
+        }
+        return true;
+    }
+    private boolean check(char c){
+        return c <= 'z' && c >= 'a';
+    }
+}
