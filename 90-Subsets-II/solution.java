8a7fe49c5f6cb4bc1a495b5475ee52de3642c9fe
krunning2 <zhidongke@yahoo.com>
2016-07-30 15:10:20 -0700

    [90. Subsets II][Compile Error]committed by Krunning

--- 90-Subsets-II/solution.java
+++ 90-Subsets-II/solution.java
@@ -1,20 +1,19 @@
 public class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
-        helper(res, nums, 0, new ArrayList(), new boolean[nums.length]);
+        Arrays.sort(nums);
+        helper(res, nums, 0, new ArrayList())
         return res;
     }
-    private void helper(List<List<Integer>> res, int[] nums, int pos, List<Integer> cur, boolean[] visited){
+    private void helper(List<List<Integer>> res, int[] nums, int pos, List<Integer> cur){
         res.add(new ArrayList<Integer>(cur));
         for(int i = pos; i < nums.length; i++){
-            if(i > 0 && nums[i] == nums[i - 1] && !visited[i]){
+            if(i != pos && nums[i] == nums[i - 1]){
                 continue;
             }
-            visited[i] = true;
             cur.add(nums[i]);
             helper(res, nums, i + 1, cur);
             cur.remove(cur.size() - 1);
-            visited[i] = false;
         }
     }
 }
