837ed76632df5243805adb923b6edd948bc7762b
krunning2 <zhidongke@yahoo.com>
2016-07-29 23:44:48 -0700

    [121. Best Time to Buy and Sell Stock][Accepted]committed by Krunning

---
+++ 121-Best-Time-to-Buy-and-Sell-Stock/solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int res = 0;
+        for(int i = 0; i < prices.length; i++){
+            res = Math.max(res, prices[i] - min);
+            min = Math.min(min, prices[i]);
+        }
+        return res;
+    }
+}
