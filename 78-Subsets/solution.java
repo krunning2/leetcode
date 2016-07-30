09f538e5fb59705390aefc601b945e5f6fa80c84
krunning2 <zhidongke@yahoo.com>
2016-07-29 23:41:15 -0700

    [78. Subsets][Compile Error]committed by Krunning

---
+++ 78-Subsets/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        Arrays.sort(nums);
+        res.add(new ArrayList<Integer>());
+        helper(nums, 0, res, new ArrayList());
+        return res;
+    }
+    private void helper(int[] nums, int pos, List<List<Integer>> res, List<Integer> cur){
+        if(cur.size() > 0){
+            res.add(new ArrayList(cur));
+        }
+        for(int i = pos; i < nums.length; i++){
+            cur.add(nums[i]);
+            helper(nums, i + 1, res cur);
+            cur.remove(cur.size() - 1);
+        }
+    }
+}
