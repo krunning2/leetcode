4f750604e1ce473b26b6650fc564ae56de357f6d
krunning2 <zhidongke@yahoo.com>
2016-07-30 12:07:32 -0700

    [28. Implement strStr()][Accepted]committed by Krunning

--- 28-Implement-strStr()/solution.java
+++ 28-Implement-strStr()/solution.java
@@ -1,12 +1,12 @@
 public class Solution {
     public int strStr(String h, String n) {
-        if(n == null){
+        if(n == null || n.length() == 0){
             return 0;
         }
         if(h == null){
             return -1;
         }
-        for(int i = 0; i < h.length(); i++){
+        for(int i = 0; i <= h.length() - n.length(); i++){
             for(int j = 0; j < n.length(); j++){
                 if(h.charAt(i + j) != n.charAt(j)){
                     break;
