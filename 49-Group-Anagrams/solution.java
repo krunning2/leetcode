c09227b3d452d02f976036862ddf9c73708abb22
krunning2 <zhidongke@yahoo.com>
2016-07-30 13:00:24 -0700

    [49. Group Anagrams][Compile Error]committed by Krunning

--- 49-Group-Anagrams/solution.java
+++ 49-Group-Anagrams/solution.java
@@ -4,7 +4,7 @@ public class Solution {
         if(strs == null || strs.length == 0) return res;
         Map<String, List<String>> map = new HashMap();
         for(String s : strs){
-            String key = new String(Arrays.sort(s.toCharArray()));
+            String key = String.valueOf(Arrays.sort(s.toCharArray()));
             map.putIfAbsent(key, new ArrayList());
             map.get(key).add(s);
         }
