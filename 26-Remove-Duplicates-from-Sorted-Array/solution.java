23cdbb7a13e37a89403a7bd96006add0c894e53f
krunning2 <zhidongke@yahoo.com>
2016-07-30 15:56:55 -0700

    [26. Remove Duplicates from Sorted Array][Compile Error]committed by Krunning

---
+++ 26-Remove-Duplicates-from-Sorted-Array/solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        int p = 0;
+        int start = 0;
+        while(p < nums.length){
+            if(p + 1 < nums.length && cur == nums[p + 1]){
+                int cur = nums[p];
+                while(p < nums.length){
+                    nums[p] == cur;
+                    p++;
+                }
+                nums[start++] = cur;
+            }else{
+                nums[start ++] = nums[p];
+                p++;
+            }
+        }
+        return start;
+    }
+}
