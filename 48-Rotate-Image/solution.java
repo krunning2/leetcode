5aec0f539f08518342f8b533e8ee1170a4b1bfff
krunning2 <zhidongke@yahoo.com>
2016-07-30 22:41:41 -0700

    [48. Rotate Image][Compile Error]committed by Krunning

--- 48-Rotate-Image/solution.java
+++ 48-Rotate-Image/solution.java
@@ -3,6 +3,14 @@ public class Solution {
         if(matrix == null || matrix.length == 0){
             return;
         }
+        if(matrix.length < 3){
+            rotate1(matrix)
+        }else{
+            rotate2(matrix);
+        }
+    }
+    
+    private void rotate1(int[][] matrix){
         int n = matrix.length;
         for(int i = 0; i < n / 2; i++){
             for(int j = 0; j < (n + 1) / 2; j++){
@@ -11,6 +19,20 @@ public class Solution {
                 matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
                 matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
                 matrix[j][n - i - 1] = tmp;
+            }
+        }
+    }
+    
+    // anti-clockwise
+    private void rotate2(int[][] matrix){
+        int n = matrix.length;
+        for(int i = 0; i < n / 2; i++){
+            for(int j = 0; j < (n + 1) / 2; j++){
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = tmp;
             }
         }
     }
