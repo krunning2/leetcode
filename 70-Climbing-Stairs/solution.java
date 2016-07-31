ec69613f176c418bf417af0c073c95bc2ce4ef96
krunning2 <zhidongke@yahoo.com>
2016-07-30 21:30:14 -0700

    [70. Climbing Stairs][Accepted]committed by Krunning

--- 70-Climbing-Stairs/solution.java
+++ 70-Climbing-Stairs/solution.java
@@ -3,13 +3,13 @@ public class Solution {
         if(n <= 2){
             return n;
         }
-        int[] dp = new int[n + 1];
+        int[] dp = new int[3];
         dp[0] = 0;
         dp[1] = 1;
         dp[2] = 2;
         for(int i = 3; i <= n; i++){
-            dp[i] = dp[i - 1] + dp[i - 2];
+            dp[i % 3] = dp[(i - 1) % 3] + dp[(i - 2) % 3];
         }
-        return dp[n];
+        return dp[n % 3];
     }
 }
