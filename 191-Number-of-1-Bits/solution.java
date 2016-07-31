fdaf05e9111c6657ed2e324ccc513f6286caa206
krunning2 <zhidongke@yahoo.com>
2016-07-31 00:38:28 -0700

    [191. Number of 1 Bits][Accepted]committed by Krunning

--- 191-Number-of-1-Bits/solution.java
+++ 191-Number-of-1-Bits/solution.java
@@ -4,7 +4,7 @@ public class Solution {
         int count = 0;
         for(int i = 0; i < 32; i++){
             if((n & 1) == 1) count++;
-            n >> = 1;
+            n >>= 1;
         }
         return count;
     }
