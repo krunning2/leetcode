cf5ff9ace6784fd8c5171a8c205c304cd1eb6c20
krunning2 <zhidongke@yahoo.com>
2016-07-30 23:09:35 -0700

    [190. Reverse Bits][Accepted]committed by Krunning

--- 190-Reverse-Bits/solution.java
+++ 190-Reverse-Bits/solution.java
@@ -4,7 +4,7 @@ public class Solution {
         int res = 0;
         for(int i = 0; i < 32; i++){
             res += n & 1;
-            n >>>= 1;
+            n >>= 1;
             if(i < 31) res <<= 1;
         }
         return res;
