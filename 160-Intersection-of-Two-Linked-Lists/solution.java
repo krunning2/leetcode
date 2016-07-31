b1b8860ae7e280731d7c6b39f85a0e4d71632e55
krunning2 <zhidongke@yahoo.com>
2016-07-31 09:55:47 -0700

    [160. Intersection of Two Linked Lists][Accepted]committed by Krunning

--- 160-Intersection-of-Two-Linked-Lists/solution.java
+++ 160-Intersection-of-Two-Linked-Lists/solution.java
@@ -11,24 +11,33 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null) return null;
         ListNode tailB = getTail(headB);
         tailB.next = headB;
         ListNode fast = headA;
         ListNode slow = headA;
         while(true){
-            if(fast == null) return null;
+            if(fast == null) break;
             fast = fast.next;
-            if(fast == null) return null;
+            if(fast == null) break;
             fast = fast.next;
             slow = slow.next;
             if(slow == fast) break;
+        }
+        if(fast == null){
+            reset(tailB);
+            return null;
         }
         slow = headA;
         while(slow != fast){
             slow = slow.next;
             fast = fast.next;
         }
+        reset(tailB);
         return fast;
+    }
+    private void reset(ListNode tail){
+        tail.next = null;
     }
     private ListNode getTail(ListNode head){
         while(head != null && head.next != null){
