ad0fc1495c1a958755d396a49b48e09d739a3a93
krunning2 <zhidongke@yahoo.com>
2016-07-31 09:45:30 -0700

    [242. Valid Anagram][Accepted]committed by Krunning

--- 242-Valid-Anagram/solution.java
+++ 242-Valid-Anagram/solution.java
@@ -2,7 +2,7 @@ public class Solution {
     public boolean isAnagram(String s, String t) {
         int[] map = new int[26];
         for(int i = 0; i < s.length(); i++){
-            map[s.chatAt(i) - 'a']++;
+            map[s.charAt(i) - 'a']++;
         }
         for(int i = 0; i < t.length(); i++){
             map[t.charAt(i) - 'a'] --;
