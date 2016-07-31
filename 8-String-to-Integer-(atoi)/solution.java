70357453401d7a3c003dfdcd811a1439afc51603
krunning2 <zhidongke@yahoo.com>
2016-07-31 10:10:08 -0700

    [8. String to Integer (atoi)][Compile Error]committed by Krunning

---
+++ 8-String-to-Integer-(atoi)/solution.java
@@ -0,0 +1,33 @@
+public class Solution {
+    public int myAtoi(String str) {
+        if(str == null || str.length() == 0){
+            throw new Exception("invalid input");
+        }
+        str = str.trim();
+        int p = 0;
+        int sign = 1;
+        if(str.charAt(0) == '+'){
+            sign = 1;
+            p++;
+        }else if(str.charAt(0) == '-'){
+            sign = -1;
+            p++;
+        }
+        long res = 0;
+        while(p < str.length()){
+            if(isValid(str.charAt(p))){
+                res = res * 10 + (str.charAt(p) - '0');
+                p++;
+            }else{
+               throw new Exception("invalid input"); 
+            }
+        }
+        if(res > Integer.MAX_VALUE || res < Integer.MIN_VALUE){
+            throw new Exception("invalid input"); 
+        }
+        return (int) res;
+    }
+    private boolean isValid(char c){
+        return c <= '9' && c >= '0';
+    }
+}
