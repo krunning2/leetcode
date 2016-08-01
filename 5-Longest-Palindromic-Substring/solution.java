4882e991825bb854a7f24032acb96c01fb6cb283
krunning2 <zhidongke@yahoo.com>
2016-07-31 23:23:52 -0700

    [5. Longest Palindromic Substring][Compile Error]committed by Krunning

---
+++ 5-Longest-Palindromic-Substring/solution.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public String longestPalindrome(String s) {
+        if(s == null || s.length() <= 1) return true;
+        int size = s.length();
+        boolean[][] map = new boolean[size][size];
+        for(int i = 0; i < size; i++){
+            map[i][i] = true;
+        }
+        int max = 1;
+        String res = "";
+        // len 表示加上的长度，并非总长度
+        for(int len = 1; len < size; len ++){
+            for(int start = 0; start + len < size; start ++){
+                if(s.charAt(start) == s.charAt(start + len) && (map[start + 1][start + len - 1] || len == 1)){
+                    map[start][start + len] = true;
+                    if(max < len + 1){
+                        max = len + 1;
+                        res = s.substring(start, start + len + 1);
+                    }
+                }
+                
+            }
+        }
+        return res;
+    }
+    
+}
