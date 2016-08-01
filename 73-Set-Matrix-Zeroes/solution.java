8ca23d3de41e79b73e64d7f1c5b9d561b2bc12bc
krunning2 <zhidongke@yahoo.com>
2016-07-31 21:54:29 -0700

    [73. Set Matrix Zeroes][Accepted]committed by Krunning

--- 73-Set-Matrix-Zeroes/solution.java
+++ 73-Set-Matrix-Zeroes/solution.java
@@ -24,18 +24,18 @@ public class Solution {
         }
         for(int i = 1; i < matrix.length; i++){
             for(int j = 1; j < matrix[0].length; j++){
-                if(matrix[0][j] == 0 || matrix[i][0]){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0){
                     matrix[i][j] = 0;
                 }
             }
         }
         for(int i = 0; i < matrix.length; i++){
-            if(row = true){
+            if(row){
                 matrix[i][0] = 0;
             }
         }
         for(int i = 0; i < matrix[0].length; i++){
-            if(col = true){
+            if(col){
                 matrix[0][i] = 0;
             }
         }
