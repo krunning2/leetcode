927313c716396bf6ffa21883dac39225899696a5
krunning2 <zhidongke@yahoo.com>
2016-07-31 22:59:40 -0700

    [89. Gray Code][Compile Error]committed by Krunning

---
+++ 89-Gray-Code/solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> res = 0;
+        res.add(0);
+        if(n == 0) return res;
+        for(int i = 1; i <= n; i++){
+            int size = res.size();
+            int x = 1 << (i - 1);
+            for(int j = size - 1; j >= 0; j--){
+                res.add(res.get(i) + x);
+            }
+        }
+        return res;
+    }
+}
