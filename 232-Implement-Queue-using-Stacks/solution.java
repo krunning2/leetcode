2c67fe59eaa8811bfddc253a4a6edb2024e9338a
krunning2 <zhidongke@yahoo.com>
2016-08-02 21:12:15 -0700

    [232. Implement Queue using Stacks][Compile Error]committed by Krunning

---
+++ 232-Implement-Queue-using-Stacks/solution.java
@@ -0,0 +1,34 @@
+class MyQueue {
+    // Push element x to the back of queue.
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    // Removes the element from in front of queue.
+    public void pop() {
+        if(stack2.isEmpty()){
+            dump();
+        }
+        return stack2.pop();
+    }
+
+    // Get the front element.
+    public int peek() {
+        if(stack2.isEmpty()){
+            dump();
+        }
+        return stack2.peek();
+    }
+
+    // Return whether the queue is empty.
+    public boolean empty() {
+        
+    }
+    private void dump(){
+        while(!stack1.isEmpty()){
+            stack2.push(stack1.pop());
+        }
+    }
+}
