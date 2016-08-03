c49f6ad2e0cc9db2f36903b238ece9942a79e00e
krunning2 <zhidongke@yahoo.com>
2016-08-02 22:37:39 -0700

    [365. Water and Jug Problem][Accepted]committed by Krunning

--- 365-Water-and-Jug-Problem/solution.java
+++ 365-Water-and-Jug-Problem/solution.java
@@ -1,8 +1,8 @@
 public class Solution {
     public boolean canMeasureWater(int x, int y, int z) {
-        return z == 0 || (x + y > z && z % gcd(x, y) == 0); 
+        return z == 0 || (x + y >= z && z % gcd(x, y) == 0); 
     }
     private int gcd(int x, int y){
-        return y == 0 ? x : gcd(y, x % y)
+        return y == 0 ? x : gcd(y, x % y);
     }
 }
