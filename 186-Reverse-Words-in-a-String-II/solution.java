121cb70c1170f8433f5de8786790df20cee3b7bf
krunning2 <zhidongke@yahoo.com>
2016-08-04 09:05:14 -0700

    [186. Reverse Words in a String II][Time Limit Exceeded]committed by Krunning

--- 186-Reverse-Words-in-a-String-II/solution.java
+++ 186-Reverse-Words-in-a-String-II/solution.java
@@ -5,7 +5,7 @@ public class Solution {
         int p1 = 0, p2 = 0;
         while(p2 < s.length){
             if(s[p2] == ' '){
-                reverse(c, p1, p2 - 1);
+                reverse(s, p1, p2 - 1);
                 p2++;
                 p1 = p2;
             }else{
