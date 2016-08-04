de6b088b494235a8b0f146ad6e98a7f22d4b7d01
krunning2 <zhidongke@yahoo.com>
2016-08-04 08:38:46 -0700

    [333. Largest BST Subtree][Compile Error]committed by Krunning

---
+++ 333-Largest-BST-Subtree/solution.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    class Result{
+        int size;
+        int max;
+        int upper;
+        int lower;
+        Result(int s, int m, int u, int l){
+            size = s;
+            max = m;
+            upper = u;
+            lower = l;
+        }
+    }
+    
+    public int largestBSTSubtree(TreeNode root) {
+        Result res = find(root);
+        return res.max;
+    }
+    
+    private Result find(TreeNode root){
+        if(root == null) return new Result(0, 0, Integer.MAX_VALUE, Integer.MIN_VALUE);
+        Result left = find(root.left);
+        Result right = find(root.right);
+        if(left.size == -1 || right.size == -1 || root.val <= left.upper || root.val >= right.lower){
+            return Result(-1, Math.max(left.max, right.max), Integer.MAX_VALUE, Integer.MIN_VALUE);
+        }
+        int size = left.size + right.size + 1;
+        return new Result(size, Math.max(size, Math.max(left.max, right.max)), Math.min(root.val, left.lower), Math.max(root.val, right.upper));
+    }
+
+}
