10bedeb73e947b61d9df50c59c1f7bc58e703aeb
krunning2 <zhidongke@yahoo.com>
2016-08-04 09:03:33 -0700

    [186. Reverse Words in a String II][Compile Error]committed by Krunning

---
+++ 186-Reverse-Words-in-a-String-II/solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public void reverseWords(char[] s) {
+        if(s == null) return;
+        Arrays.reverse(s);
+        int p1 = 0, p2 = 0;
+        while(p2 != null){
+            if(s[p2] == ' '){
+                reverse(c, p1, p2 - 1);
+                p2++;
+                p1 = p2;
+            }else{
+                p2++;
+            }
+        }
+    }
+    private void reverse(char[] c, int s, int e){
+        while(s <= e){
+            char tmp = c[s];
+            c[s] = c[e];
+            c[e] = tmp;
+        }
+    }
+}
