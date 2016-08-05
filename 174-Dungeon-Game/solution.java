7287cba3d02bae67ef76207bb4d2fb59b426c4f8
krunning2 <zhidongke@yahoo.com>
2016-08-04 23:03:15 -0700

    [174. Dungeon Game][Compile Error]committed by Krunning

---
+++ 174-Dungeon-Game/solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int calculateMinimumHP(int[][] dungeon) {
+        if(dungeon == null || dungeon.length == 0 || dungeon[0].length == 0){
+            return 1;
+        }
+        int m = dungeon.length - 1;
+        int n = dungeon[0].length - 1;
+        int[][] dp = new int[m + 1][n + 1];
+        dp[m][n] = Math.max(1, 1 - dungeon[m][n]);
+        for(int i = m - 1; i >= 0; i--){
+            dungeon[i][n] = Math.max(1, dp[i + 1][n] - dungeon[i][n]); 
+        }
+        for(int i = n - 1; i >= 0; i--){
+            dungeon[m][i] = Math.max(1, dp[m][i + 1] - dungeon[m][i])
+        }
+        for(int i = m - 1; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                dp[i][j] = Math.max(1, Math.min(dp[i - 1][j], dp[i][j - 1]) - dungeon[i][j]);
+            }
+        }
+        return dp[0][0];
+    }
+}
