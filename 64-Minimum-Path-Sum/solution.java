6a4b25b291f4e17d6411329c5f366ccab09ee8ca
krunning2 <zhidongke@yahoo.com>
2016-08-05 00:13:01 -0700

    [64. Minimum Path Sum][Accepted]committed by Krunning

--- 64-Minimum-Path-Sum/solution.java
+++ 64-Minimum-Path-Sum/solution.java
@@ -11,7 +11,7 @@ public class Solution {
             dp[0][j] = dp[0][j-1] + grid[0][j];
         }
         for(int i = 1; i < m; i++){
-            for(int j = 1; j < m; j++){
+            for(int j = 1; j < n; j++){
                 dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
             }
         }
