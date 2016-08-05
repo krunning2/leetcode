8c4da8966fff3fc35cff1b1fff50954da36582fc
krunning2 <zhidongke@yahoo.com>
2016-08-05 00:12:51 -0700

    [64. Minimum Path Sum][Runtime Error]committed by Krunning

--- 64-Minimum-Path-Sum/solution.java
+++ 64-Minimum-Path-Sum/solution.java
@@ -4,7 +4,7 @@ public class Solution {
         int n = grid[0].length;
         int[][]dp = new int[m][n];
         dp[0][0] = grid[0][0];
-        for(int i = 1; i < n; i++){
+        for(int i = 1; i < m; i++){
             dp[i][0] = dp[i - 1][0] + grid[i][0];
         }
         for(int j = 1; j < n; j++){
