acef82edd08faedc3b3ffea15f45494cbfd8dc7d
krunning2 <zhidongke@yahoo.com>
2016-08-04 23:27:20 -0700

    [174. Dungeon Game][Compile Error]committed by Krunning

--- 174-Dungeon-Game/solution.java
+++ 174-Dungeon-Game/solution.java
@@ -6,18 +6,18 @@ public class Solution {
         int m = dungeon.length - 1;
         int n = dungeon[0].length - 1;
         int[][] dp = new int[m + 1][n + 1];
-        dp[m][n] = Math.max(1, 1 - dungeon[m][n]);
+        dungeon[m][n] = Math.max(1, 1 - dungeon[m][n]);
         for(int i = m - 1; i >= 0; i--){
-            dp[i][n] = Math.max(1, dp[i + 1][n] - dungeon[i][n]); 
+            dungeon[i][n] = Math.max(1, dungeon[i + 1][n] - dungeon[i][n]); 
         }
         for(int i = n - 1; i >= 0; i--){
-            dp[m][i] = Math.max(1, dp[m][i + 1] - dungeon[m][i]);
+            dungeon[m][i] = Math.max(1, dungeon[m][i + 1] - dungeon[m][i]);
         }
         for(int i = m - 1; i >= 0; i--){
             for(int j = n - 1; j >= 0; j--){
-                dp[i][j] = Math.max(1, Math.min(dp[i + 1][j], dp[i][j + 1]) - dungeon[i][j]);
+                dpdungeon[j] = Math.max(1, Math.min(dungeon[i + 1][j], dungeon[i][j + 1]) - dungeon[i][j]);
             }
         }
-        return dp[0][0];
+        return dungeon[0][0];
     }
 }
