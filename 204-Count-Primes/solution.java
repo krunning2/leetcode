73bf7f84d34d5382b6467355ce452dc457656ae1
krunning2 <zhidongke@yahoo.com>
2016-08-04 21:53:32 -0700

    [204. Count Primes][Accepted]committed by Krunning

--- 204-Count-Primes/solution.java
+++ 204-Count-Primes/solution.java
@@ -10,7 +10,7 @@ public class Solution {
             }
         }
         int count = 0;
-        for(int i = 2; i <= n; i++){
+        for(int i = 2; i < n; i++){
             if(isPrime[i]){
                 count++;
             }
