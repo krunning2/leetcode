f3cd7b4846e87c1d91b6b93f0e8c4d5ff9955f07
krunning2 <zhidongke@yahoo.com>
2016-08-07 11:44:52 -0700

    [355. Design Twitter][Accepted]committed by Krunning

--- 355-Design-Twitter/solution.java
+++ 355-Design-Twitter/solution.java
@@ -1,35 +1,17 @@
 public class Twitter {
-    private static int timeStamp=0;
     
-    // easy to find if user exist
-    private Map<Integer, User> userMap;
-    
-    // Tweet link to next Tweet so that we can save a lot of time
-    // when we execute getNewsFeed(userId)
-    private class Tweet{
-        public int id;
-        public int time;
-        public Tweet next;
+    public static int counter = 0;
+    private static Map<Integer, User> u_map;
+    class User {
+        int uId;
+        Set<Integer> followed;
+        Tweet head;
         
-        public Tweet(int id){
-            this.id = id;
-            time = timeStamp++;
-            next=null;
-        }
-    }
-    
-    
-    // OO design so User can follow, unfollow and post itself
-    public class User{
-        public int id;
-        public Set<Integer> followed;
-        public Tweet tweet_head;
-        
-        public User(int id){
-            this.id=id;
-            followed = new HashSet<>();
-            follow(id); // first follow itself
-            tweet_head = null;
+        public User(int uId){
+            this.uId = uId;
+            followed = new HashSet<Integer>();
+            follow(uId);
+            head = null;
         }
         
         public void follow(int id){
@@ -40,86 +22,73 @@ public class Twitter {
             followed.remove(id);
         }
         
-        
-        // everytime user post a new tweet, add it to the head of tweet list.
         public void post(int id){
-            Tweet t = new Tweet(id);
-            t.next=tweet_head;
-            tweet_head=t;
+            Tweet t = new Tweet(id, counter++);
+            t.next = head;
+            head = t;
         }
     }
     
+    class Tweet {
+        Tweet next;
+        int id;
+        int time;
+        public Tweet(int id, int time){
+            this.id = id;
+            this.time = time;
+            next = null;
+        }
+    }
     
-    
-
     /** Initialize your data structure here. */
     public Twitter() {
-        userMap = new HashMap<Integer, User>();
+        u_map = new HashMap<>();
     }
     
     /** Compose a new tweet. */
     public void postTweet(int userId, int tweetId) {
-        if(!userMap.containsKey(userId)){
-            User u = new User(userId);
-            userMap.put(userId, u);
-        }
-        userMap.get(userId).post(tweetId);
-            
+        u_map.putIfAbsent(userId, new User(userId));
+        u_map.get(userId).post(tweetId);
     }
     
-
-    
-    // Best part of this.
-    // first get all tweets lists from one user including itself and all people it followed.
-    // Second add all heads into a max heap. Every time we poll a tweet with 
-    // largest time stamp from the heap, then we add its next tweet into the heap.
-    // So after adding all heads we only need to add 9 tweets at most into this 
-    // heap before we get the 10 most recent tweet.
+    /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
     public List<Integer> getNewsFeed(int userId) {
-        List<Integer> res = new LinkedList<>();
-
-        if(!userMap.containsKey(userId))   return res;
-        
-        Set<Integer> users = userMap.get(userId).followed;
-        PriorityQueue<Tweet> q = new PriorityQueue<Tweet>(users.size(), (a,b)->(b.time-a.time));
-        for(int user: users){
-            Tweet t = userMap.get(user).tweet_head;
-            // very imporant! If we add null to the head we are screwed.
-            if(t!=null){
-                q.add(t);
+        List<Integer> res = new ArrayList<>();
+        User u = u_map.get(userId);
+        if(u == null) return res;
+        Set<Integer> users = u.followed;
+        PriorityQueue<Tweet> queue = new PriorityQueue<Tweet>(users.size(), (t1, t2) -> (t2.time - t1.time));
+        users.forEach(uId ->{
+        	Tweet head = u_map.get(uId).head;
+            if(head != null){
+                queue.offer(head);
             }
+        });
+        int n = 0;
+        while(!queue.isEmpty() && n < 10){
+            Tweet cur = queue.poll();
+            res.add(cur.id);
+            if(cur.next != null){
+                queue.offer(cur.next);
+            }
+            n++;
         }
-        int n=0;
-        while(!q.isEmpty() && n<10){
-          Tweet t = q.poll();
-          res.add(t.id);
-          n++;
-          if(t.next!=null)
-            q.add(t.next);
-        }
-        
         return res;
-        
     }
     
     /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
     public void follow(int followerId, int followeeId) {
-        if(!userMap.containsKey(followerId)){
-            User u = new User(followerId);
-            userMap.put(followerId, u);
-        }
-        if(!userMap.containsKey(followeeId)){
-            User u = new User(followeeId);
-            userMap.put(followeeId, u);
-        }
-        userMap.get(followerId).follow(followeeId);
+        u_map.putIfAbsent(followerId, new User(followerId));
+        u_map.putIfAbsent(followeeId, new User(followeeId));
+        u_map.get(followerId).follow(followeeId);
     }
     
     /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
     public void unfollow(int followerId, int followeeId) {
-        if(!userMap.containsKey(followerId) || followerId==followeeId)
+        if(!u_map.containsKey(followerId) || followeeId == followerId){
             return;
-        userMap.get(followerId).unfollow(followeeId);
+        }
+        u_map.get(followerId).unfollow(followeeId);
     }
 }
 
