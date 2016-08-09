e6a078a67ea39905386baa895260c6487b301980
krunning2 <zhidongke@yahoo.com>
2016-08-09 09:58:13 -0700

    [217. Contains Duplicate][Time Limit Exceeded]committed by Krunning

--- 217-Contains-Duplicate/solution.java
+++ 217-Contains-Duplicate/solution.java
@@ -2,7 +2,8 @@ public class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set = new HashSet<Integer>();
         for(int n : nums){
-            if(!set.add(n)) return true;
+            if(set.contains(n)) return true;
+            set.add(n);
         }
         return false;
     }
