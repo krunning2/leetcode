c7573a7cd5b40b5adc5617a6550c02408cf14d5d
krunning2 <zhidongke@yahoo.com>
2016-08-09 09:51:33 -0700

    [373. Find K Pairs with Smallest Sums][Accepted]committed by Krunning

--- 373-Find-K-Pairs-with-Smallest-Sums/solution.java
+++ 373-Find-K-Pairs-with-Smallest-Sums/solution.java
@@ -1,29 +1,17 @@
 public class Solution {
     public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        int p1 = 0, p2 = 0;
-        List<int[]> res = new ArrayList<int[]>();
-        while(p1 < nums1.length && p2 < nums2.length && k > 0){
-            if(nums1[p1] < nums[p2]){
-                res.add(new int[]{nums1[p1], nums2[p2]});
-                p2 ++;
-            }else if(nums1[p1] > nums[p2]){
-                res.add(new int[]{nums2[p2], nums1[p1]});
-                p1 ++;
-            }else{
-                res.add(new int[]{nums2[p2], nums1[p1]});
-                if(p1 < nums1.length - 1 && p2 < nums2.length - 1){
-                    if(nums1[p1 + 1] < nums2[p2 + 1]){
-                        p1 ++;
-                    }else{
-                        p2++;
-                    }
-                }else if(p1 == nums1.length - 1){
-                    p2++;
-                }else{
-                    p1++;
-                }
-            }
+        List<int[]> res = new ArrayList<int[]> ();
+        if(nums1 == null || nums2 == null || nums1.length == 0 || nums2.length == 0 || k == 0) return res;
+        PriorityQueue<int[]> queue = new PriorityQueue<int[]>(k, (a, b) -> (a[0] + a[1] - b[0] - b[1]));
+        for(int i = 0; i < nums1.length && i < k; i++){
+            queue.offer(new int[]{nums1[i], nums2[0], 0});
+        }
+        while(k > 0 && !queue.isEmpty()){
             k--;
+            int[] cur = queue.poll();
+            res.add(new int[] {cur[0], cur[1]});
+            if(cur[2] == nums2.length - 1) continue;
+            queue.offer(new int[]{cur[0], nums2[cur[2] + 1], cur[2] + 1});
         }
         return res;
     }
