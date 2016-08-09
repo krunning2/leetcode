32c4b87aaa8904e8798d56433091e852d3f099e6
krunning2 <zhidongke@yahoo.com>
2016-08-08 23:04:12 -0700

    [37. Sudoku Solver][Runtime Error]committed by Krunning

--- 37-Sudoku-Solver/solution.java
+++ 37-Sudoku-Solver/solution.java
@@ -23,20 +23,18 @@ public class Solution {
         for(int i = 0; i < 9; i++){
             Arrays.fill(visited, false);
             for(int j = 0; j < 9; j++){
-                if(visited[board[i][j] - '1'] || !isValid(board[i][j])){
+                if(!isValid(board[i][j], visited)){
                     return false;
                 }
-                visited[board[i][j] - '1'] = true;
             }
         }
         
         for(int i = 0; i < 9; i++){
             Arrays.fill(visited, false);
             for(int j = 0; j < 9; j++){
-                if(visited[board[i][j] - '1'] || !isValid(board[j][i])){
+                if(!isValid(board[j][i], visited)){
                     return false;
                 }
-                visited[board[i][j] - '1'] = true;
             }
         }
         
@@ -44,17 +42,19 @@ public class Solution {
             for(int j = 0; j < 0; j = j + 3){
                 Arrays.fill(visited, false);
                 for(int k = 0; k < 9; k ++){
-                    if(visited[board[i / 3 + k][j / 3 + k] - '1'] || !isValid(board[i / 3 + k][j / 3 + k])){
+                    if(!isValid(board[i / 3 + k][j / 3 + k], visited)){
                         return false;
                     }
-                    visited[board[i / 3 + k][j / 3 + k] - '1'] = true;
                 }
             }
         }
         return true;
     }
     
-    private boolean isValid(char c){
-        return c < '9' && c > '0' || c == '.';
+    private boolean isValid(char c, boolean visited[]){
+        if(c == '.') return true;
+        if(c < '0' || c > '9' || visited[c - '1']) return false;
+        visited[c - '1'] = true;
+        return true;
     }
 }
