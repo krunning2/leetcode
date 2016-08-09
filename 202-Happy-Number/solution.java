048dd3ca78c93c6b795aa2afc8993b2799fd1eed
krunning2 <zhidongke@yahoo.com>
2016-08-08 22:34:52 -0700

    [202. Happy Number][Accepted]committed by Krunning

--- 202-Happy-Number/solution.java
+++ 202-Happy-Number/solution.java
@@ -1,16 +1,15 @@
 public class Solution {
     public boolean isHappy(int n) {
-        int sum = 0;
         Set<Integer> set = new HashSet<Integer>();
-        set.add(n);
-        while(n != 1 && !set.contains(n)){
+        while(set.add(n)){
+            int sum = 0;
             while(n > 0){
                 sum += (n % 10) * (n % 10);
                 n /= 10;
             }
+            if(sum == 1) return true;
             n = sum;
-            set.add(n);
         }
-        return n == 1;
+        return false;
     }
 }
