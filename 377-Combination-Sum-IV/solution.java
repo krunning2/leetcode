614ecb489fd16d9adb890d27a37d6fceff6e5e8b
krunning2 <zhidongke@yahoo.com>
2016-08-12 09:32:48 -0700

    [377. Combination Sum IV][Accepted]committed by Krunning

--- 377-Combination-Sum-IV/solution.java
+++ 377-Combination-Sum-IV/solution.java
@@ -1,21 +1,14 @@
 public class Solution {
     public int combinationSum4(int[] nums, int target) {
-        List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(nums);
-        helper(nums, target, res, new ArrayList<>(), 0);
-        return res.size();
-    }
-    private void helper(int[] nums, int target, List<List<Integer>> res, List<Integer> cur, int sum){
-        if(sum > target) return;
-        if(sum == target){
-            res.add(new ArrayList(cur));
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for(int i = 1; i <= target; i++){
+            for(int n : nums){
+                if(i >= n){
+                    dp[i] += dp[i - n];
+                }
+            }
         }
-        for(int i = 0; i < nums.length; i++){
-            sum += nums[i];
-            cur.add(nums[i]);
-            helper(nums, target, res, cur, sum);
-            sum -= nums[i];
-            cur.remove(cur.size() - 1);
-        }
+        return dp[target];
     }
 }
