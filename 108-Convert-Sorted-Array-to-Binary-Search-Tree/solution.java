1eb60ddf5dc9f3c06f80c632564b719061e608f4
krunning2 <zhidongke@yahoo.com>
2016-08-11 22:12:28 -0700

    [108. Convert Sorted Array to Binary Search Tree][Compile Error]committed by Krunning

---
+++ 108-Convert-Sorted-Array-to-Binary-Search-Tree/solution.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums == null || nums.length == 0) return null; 
+        return build(nums, 0, nums.length - 1);
+    }
+    private TreeNode build(int[] nums, int s, int e){
+        if(s > e) return null;
+        int m = s - (s - e) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = build(nums, s, mid - 1);
+        root.right = build(nums, mid + 1, e);
+        return root;
+    }
+}
