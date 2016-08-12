a53322d0143dbe841f7090f1168f22ef815e187b
krunning2 <zhidongke@yahoo.com>
2016-08-11 22:03:43 -0700

    [220. Contains Duplicate III][Compile Error]committed by Krunning

--- 220-Contains-Duplicate-III/solution.java
+++ 220-Contains-Duplicate-III/solution.java
@@ -1,7 +1,7 @@
 public class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
         if(nums == null || nums.length < 2 ||k < 1 || t < 0) return false;
-        Set<Integer> set = new TreeSet<>();
+        TreeSet<Integer> set = new TreeSet<>();
         for(int i = 0; i < nums.length; i++){
             int n = nums[i];
             Integer ceiling = set.ceiling(n);
