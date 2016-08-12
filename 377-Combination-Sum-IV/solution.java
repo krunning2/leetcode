9f869eb606094070a3ea87b6920df073ee323390
krunning2 <zhidongke@yahoo.com>
2016-08-11 22:56:56 -0700

    [377. Combination Sum IV][Runtime Error]committed by Krunning

--- 377-Combination-Sum-IV/solution.java
+++ 377-Combination-Sum-IV/solution.java
@@ -3,17 +3,13 @@ public class Solution {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
         helper(nums, target, 0, res, new ArrayList<>(), 0);
-        return res;
+        return res.size();
     }
     private void helper(int[] nums, int target, int pos, List<List<Integer>> res, List<Integer> cur, int sum){
-        if(sum > target) return;
-        if(sum <= target){
-            res.add(new ArrayList(cur));
-            return;
-        }
         for(int i = pos; i < nums.length; i++){
             sum += nums[i];
             cur.add(nums[i]);
+            if(sum <= target)  res.add(new ArrayList(cur));
             helper(nums, target, i, res, cur, sum);
             sum -= nums[i];
             cur.remove(cur.size() - 1);
