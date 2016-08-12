f1be4735bd249b2ba5fc38ed35ddcaa33d967dde
krunning2 <zhidongke@yahoo.com>
2016-08-11 22:29:46 -0700

    [40. Combination Sum II][Compile Error]committed by Krunning

--- 40-Combination-Sum-II/solution.java
+++ 40-Combination-Sum-II/solution.java
@@ -3,10 +3,10 @@ public class Solution {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         if(nums == null) return res;
         Arrays.sort(nums);
-        helper(nums, target, 0, 0, res, new ArrayList<>(), new boolean[nums.length]);
+        helper(nums, target, 0, 0, res, new ArrayList<>());
         return res;
     }
-    private void helper(int[] nums, int target, int sum, int pos, List<List<Integer>> res, List<Integer> cur, boolean[] visited){
+    private void helper(int[] nums, int target, int sum, int pos, List<List<Integer>> res, List<Integer> cur){
         if(sum > target) return;
         if(sum == target){
             res.add(new ArrayList<>(cur));
@@ -14,13 +14,14 @@ public class Solution {
         }
         for(int i = pos; i < nums.length; i++){
             if(i != 0 && nums[i] == nums[i - 1] && !visited[i - 1]) continue;
-            visited[i] = true;
             sum += nums[i];
             cur.add(nums[i]);
             helper(nums, target, sum, i + 1, res, cur, visited);
             sum -= nums[i];
             cur.remove(cur.size() - 1);
-            visited[i] = false;
+            while(i < nums.length - 1 && nums[i] == nums[i + 1]){
+                i++;
+            }
         }
     }
 }
