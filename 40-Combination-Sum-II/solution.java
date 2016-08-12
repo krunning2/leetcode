109bc472b304d3ca1402fbed6ff32eb192b212da
krunning2 <zhidongke@yahoo.com>
2016-08-11 22:26:46 -0700

    [40. Combination Sum II][Compile Error]committed by Krunning

---
+++ 40-Combination-Sum-II/solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public List<List<Integer>> combinationSum2(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(nums == null) return res;
+        Arrays.sort(nums);
+        helper(nums, target, 0, 0, res, new ArrayList<>(), new boolean[nums.length]);
+    }
+    private void helper(int[] nums, int target, int sum, int pos, List<List<Integer>> res, List<Integer> cur, boolean[] visited){
+        if(sum > target) return;
+        if(sum == target){
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+        for(int i = pos; i < nums.length; i++){
+            if(i != 0 && nums[i] == nums[i - 1] && !visited[i - 1]) continue;
+            sum += nums[i];
+            cur.add(nums[i]);
+            helper(nums, target, sum, i + 1, res, cur, visited);
+            sum -= nums[i];
+            cur.remove(cur.size() - 1);
+        }
+    }
+}
