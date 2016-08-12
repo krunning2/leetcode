4fb68f0590b1b0c4db00dfd843f6435d5b7761c3
krunning2 <zhidongke@yahoo.com>
2016-08-11 22:27:42 -0700

    [40. Combination Sum II][Runtime Error]committed by Krunning

--- 40-Combination-Sum-II/solution.java
+++ 40-Combination-Sum-II/solution.java
@@ -4,6 +4,7 @@ public class Solution {
         if(nums == null) return res;
         Arrays.sort(nums);
         helper(nums, target, 0, 0, res, new ArrayList<>(), new boolean[nums.length]);
+        return res;
     }
     private void helper(int[] nums, int target, int sum, int pos, List<List<Integer>> res, List<Integer> cur, boolean[] visited){
         if(sum > target) return;
@@ -13,11 +14,13 @@ public class Solution {
         }
         for(int i = pos; i < nums.length; i++){
             if(i != 0 && nums[i] == nums[i - 1] && !visited[i - 1]) continue;
+            visited[i - 1] = true;
             sum += nums[i];
             cur.add(nums[i]);
             helper(nums, target, sum, i + 1, res, cur, visited);
             sum -= nums[i];
             cur.remove(cur.size() - 1);
+            visited[i - 1] = false;
         }
     }
 }
