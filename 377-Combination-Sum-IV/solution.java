52fc5f41be6dcaa22338fe0706d3d393d65dcd7d
krunning2 <zhidongke@yahoo.com>
2016-08-11 23:05:04 -0700

    [377. Combination Sum IV][Time Limit Exceeded]committed by Krunning

--- 377-Combination-Sum-IV/solution.java
+++ 377-Combination-Sum-IV/solution.java
@@ -2,15 +2,18 @@ public class Solution {
     public int combinationSum4(int[] nums, int target) {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        helper(nums, target, 0, res, new ArrayList<>(), 0);
+        helper(nums, target, res, new ArrayList<>(), 0);
         return res.size();
     }
-    private void helper(int[] nums, int target, int pos, List<List<Integer>> res, List<Integer> cur, int sum){
-        for(int i = pos; i < nums.length; i++){
+    private void helper(int[] nums, int target, List<List<Integer>> res, List<Integer> cur, int sum){
+        if(sum > target) return;
+        if(sum == target){
+            res.add(new ArrayList(cur));
+        }
+        for(int i = 0; i < nums.length; i++){
             sum += nums[i];
             cur.add(nums[i]);
-            if(sum <= target)  res.add(new ArrayList(cur));
-            helper(nums, target, i, res, cur, sum);
+            helper(nums, target, res, cur, sum);
             sum -= nums[i];
             cur.remove(cur.size() - 1);
         }
