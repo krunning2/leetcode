33bd176e20b6337d75a4b1b79e01d9f688da5dc1
krunning2 <zhidongke@yahoo.com>
2016-08-11 22:51:56 -0700

    [377. Combination Sum IV][Compile Error]committed by Krunning

---
+++ 377-Combination-Sum-IV/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);
+        helper(candidates, target, 0, res, new ArrayList<>(), 0);
+        return res;
+    }
+    private void helper(int[] nums, int target, int pos, List<List<Integer>> res, List<Integer> cur, int sum){
+        if(sum > target) return;
+        if(sum <= target){
+            res.add(new ArrayList(cur));
+            return;
+        }
+        for(int i = pos; i < nums.length; i++){
+            sum += nums[i];
+            cur.add(nums[i]);
+            helper(nums, target, i, res, cur, sum);
+            sum -= nums[i];
+            cur.remove(cur.size() - 1);
+        }
+    }
+}
