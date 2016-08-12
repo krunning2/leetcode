e7a8e0068c8fa22827d767ed76ad13866b8e773f
krunning2 <zhidongke@yahoo.com>
2016-08-11 22:09:27 -0700

    [220. Contains Duplicate III][Accepted]committed by Krunning

--- 220-Contains-Duplicate-III/solution.java
+++ 220-Contains-Duplicate-III/solution.java
@@ -1,15 +1,17 @@
 public class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        if(nums == null || nums.length < 2 ||k < 1 || t < 0) return false;
+        if(nums == null || nums.length < 2 || k < 1 || t < 0) return false;
         TreeSet<Integer> set = new TreeSet<>();
         for(int i = 0; i < nums.length; i++){
             int n = nums[i];
             Integer ceiling = set.ceiling(n);
             Integer floor = set.floor(n);
-            if(ceiling != null && n - floor <= t || floor != null && floor - x <= t){
+            //to avoid overflow
+            if(floor != null && n <= t + floor || ceiling != null && ceiling - n <= t){
                 return true;
             }
-            if(i > k) set.remove(nums[i - k]);
+            set.add(n);
+            if(i >= k) set.remove(nums[i - k]);
         }
         return false;
     }
