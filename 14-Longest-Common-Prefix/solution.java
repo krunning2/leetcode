1ef5e75cccf99cea943c35d440756c5884508209
krunning2 <zhidongke@yahoo.com>
2016-08-12 22:57:36 -0700

    [14. Longest Common Prefix][Compile Error]committed by Krunning

---
+++ 14-Longest-Common-Prefix/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        int p = 0;
+        char pre = ' '
+        if(strs == null || strs.length == 0) return "";
+        if(strs.length == 1) return strs[0];
+        String first = strs[0];
+        while(p < first.length()){
+            char cur = first.charAt(p);
+            for(int i = 1; i < strs.length; i++){
+                if(p >= strs[i].length() || strs[i].charAt(p) != cur){
+                    return first.substring(0, p);
+                }
+            }
+            p++;
+        }
+        return first;
+    }
+}
