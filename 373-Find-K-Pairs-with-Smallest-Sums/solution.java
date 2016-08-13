3e992e380fbd01ea80f6e36a31e0cd5d7d835829
krunning2 <zhidongke@yahoo.com>
2016-08-13 14:53:50 -0700

    [373. Find K Pairs with Smallest Sums][Runtime Error]committed by Krunning

--- 373-Find-K-Pairs-with-Smallest-Sums/solution.java
+++ 373-Find-K-Pairs-with-Smallest-Sums/solution.java
@@ -1,17 +1,17 @@
 public class Solution {
     public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        List<int[]> res = new ArrayList<int[]> ();
-        if(nums1 == null || nums2 == null || nums1.length == 0 || nums2.length == 0 || k == 0) return res;
-        PriorityQueue<int[]> queue = new PriorityQueue<int[]>(k, (a, b) -> (a[0] + a[1] - b[0] - b[1]));
-        for(int i = 0; i < nums1.length && i < k; i++){
+        List<int[]> res = new ArrayList<int[]>();
+        if(nums1 == null || nums2 == null || k <= 0) return res;
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> (a[0] + a[1] - b[0] - b[1]));
+        for(int i = 0; i < Math.min(k, nums1.length); i++){
             queue.offer(new int[]{nums1[i], nums2[0], 0});
         }
-        while(k > 0 && !queue.isEmpty()){
-            k--;
+        for(int i = 0; i < k; i++){
             int[] cur = queue.poll();
             res.add(new int[] {cur[0], cur[1]});
-            if(cur[2] == nums2.length - 1) continue;
-            queue.offer(new int[]{cur[0], nums2[cur[2] + 1], cur[2] + 1});
+            if(cur[2] < nums2.length - 1){
+                queue.offer(new int[] {cur[0], nums2[cur[2] + 1], cur[2] + 1});
+            }
         }
         return res;
     }
