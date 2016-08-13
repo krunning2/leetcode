31fa55a2bd568417985a6825f467012d8b449739
krunning2 <zhidongke@yahoo.com>
2016-08-12 18:09:16 -0700

    [322. Coin Change][Accepted]committed by Krunning

---
+++ 322-Coin-Change/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if(coins == null || coins.length == 0 || amount < 0) return -1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for(int i = 1; i <= amount; i++){
+            for(int coin : coins){
+                if(i >= coin){
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
