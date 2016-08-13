d499f6235e5770127cac4a9cf111b6fb357a8544
krunning2 <zhidongke@yahoo.com>
2016-08-12 21:21:33 -0700

    [84. Largest Rectangle in Histogram][Accepted]committed by Krunning

--- 84-Largest-Rectangle-in-Histogram/solution.java
+++ 84-Largest-Rectangle-in-Histogram/solution.java
@@ -1,16 +1,17 @@
 public class Solution {
     // Try to find the left first element and right element which are less than current element, so we can get the max area.
     // So we can maintain a increasing stack
-    public int largestRectangleArea(int[] height) {
-        if(height == null || height.length == 0) return 0;
-        Stack<Integer> stack = new Stack<Integer> ();
+    public int largestRectangleArea(int[] heights) {
+        if(heights == null || heights.length == 0) return 0;
+        Stack<Integer> stack = new Stack<Integer>();
         int max = 0;
-        for(int i = 0; i <= height.length; i++){
-            int cur = i == height.length ? -1 : height[i];
-            while(!stack.isEmpty() && height[stack.peek()] > cur){
+        for(int i = 0; i <= heights.length; i++){
+            int cur = i == heights.length ? -1 : heights[i];
+            while(!stack.isEmpty() && heights[stack.peek()] >= cur){
                 int index = stack.pop();
-                int w = stack.isEmpty() ? i : i - stack.peek() - 1;
-                max = Math.max(max, w * height[index]);
+                int h = heights[index];
+                int len = stack.isEmpty() ? i : i - stack.peek() - 1;
+                max = Math.max(max, h * len);
             }
             stack.push(i);
         }
