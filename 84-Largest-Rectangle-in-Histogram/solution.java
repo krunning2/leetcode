0c694a17cd6c59ced97c87e5f784066a9b38cc43
krunning2 <zhidongke@yahoo.com>
2016-08-12 22:26:27 -0700

    [84. Largest Rectangle in Histogram][Compile Error]committed by Krunning

--- 84-Largest-Rectangle-in-Histogram/solution.java
+++ 84-Largest-Rectangle-in-Histogram/solution.java
@@ -1,18 +1,17 @@
 public class Solution {
     
-    public int largestRectangleArea(int[] height) {
-        if(height == null || height.length == 0) return 0;
+    public int largestRectangleArea(int[] heights) {
+        if(heights == null || heights.length == 0) return 0;
         Stack<Integer> stack = new Stack<Integer>();
-         int i = 0;
-         int maxArea = 0;
-         while(i <= height.length){
-             if(stack.isEmpty() || height[stack.peek()] <= height[i]){
-                 stack.push(i++);
-             }else {
-                 int t = stack.pop();
-                 maxArea = Math.max(maxArea, height[t] * (stack.isEmpty() ? i : i - stack.peek() - 1));
-             }
-         }
-         return maxArea;
+        int max = 0;
+        for(int i = 0; i <= height.length; i++){
+            int cur = i == height.length ? -1 : height[i];
+            while(!stack.isEmpty() && height[stack.peek()] >= cur){
+                int h = height[stack.pop()];
+                int len = stack.isEmpty() ? i : i - stack.peek() - 1;
+                max = Math.max(max, h * len);
+            }
+        }
+        return max;
     }
 }
