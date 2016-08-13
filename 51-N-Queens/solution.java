f71041a3c248d9b4b9a07fb94fed5f302bf31042
krunning2 <zhidongke@yahoo.com>
2016-08-13 09:33:58 -0700

    [51. N-Queens][Compile Error]committed by Krunning

---
+++ 51-N-Queens/solution.java
@@ -0,0 +1,48 @@
+public class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>> res = new ArrayList<List<String>>();
+        search(0, new int[n], n, res);
+        return res;
+    }
+    
+    private void search(int row, int[] queen, int n, List<List<String>> res){
+        if(row == n) {
+            res.add(build(queen));
+            return;
+        }
+        for(int i = 0; i < n; i++){
+            if(isValid(row, i, queen)){
+                queen[row] = i;
+                search(row + 1, queen, n, res);
+                queen[row] = 0;
+            }
+        }
+    }
+    
+    private List<String> build(int[] queen){
+        List<String> res = new ArrayList<String>();
+        StringBuilder sb = new StirngBuilder();
+        for(int i = 0; i < queen.length; i++){
+            for(int j = 0; j < queen.length; j++){
+                if(queen[i] == j){
+                    sb.append("Q");
+                }else{
+                    sb.append(".");
+                }
+            }
+            res.add(sb.toString());
+            sb.setLength(0);
+        }
+        return res;
+    }
+    
+    private isValid(int row, int col, int[] queen){
+        for(int preR = 0; preR < row; preR++){
+            int preC = queen[preR];
+            if(preC == col || Math.abs(preR - row) == Math.abs(preC - col)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
