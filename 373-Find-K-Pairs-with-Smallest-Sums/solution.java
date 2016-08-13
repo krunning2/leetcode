6d5d39bf91730e90899a7cd4b13a37f6f0eb91a6
krunning2 <zhidongke@yahoo.com>
2016-08-13 14:54:30 -0700

    [373. Find K Pairs with Smallest Sums][Runtime Error]committed by Krunning

--- 373-Find-K-Pairs-with-Smallest-Sums/solution.java
+++ 373-Find-K-Pairs-with-Smallest-Sums/solution.java
@@ -1,7 +1,7 @@
 public class Solution {
     public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
         List<int[]> res = new ArrayList<int[]>();
-        if(nums1 == null || nums2 == null || k <= 0) return res;
+        if(nums1 == null || nums2 == null || k <= 0 || nums1.length == 0 || nums2.length == 0) return res;
         PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> (a[0] + a[1] - b[0] - b[1]));
         for(int i = 0; i < Math.min(k, nums1.length); i++){
             queue.offer(new int[]{nums1[i], nums2[0], 0});
