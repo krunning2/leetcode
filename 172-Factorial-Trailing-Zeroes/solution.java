d9a58bc1cc3fdc6708478e9db99d8a1cb8e6c9d1
krunning2 <zhidongke@yahoo.com>
2016-08-13 15:43:46 -0700

    [172. Factorial Trailing Zeroes][Compile Error]committed by Krunning

--- 172-Factorial-Trailing-Zeroes/solution.java
+++ 172-Factorial-Trailing-Zeroes/solution.java
@@ -1,13 +1,9 @@
 public class Solution {
     public int trailingZeroes(int n) {
         int count = 0;
-        for(int i = 5; i <= n; i+=5){
-            int cur = i;
-            while(cur != 0 && cur % 5 == 0){
-                count ++;
-                cur /= 5;
-            }
-        }
-        return count;
+          for (int i=5; (n/i)>=1;n /= 5;) {
+              count += n / i;
+          }
+          return count;
     }
 }
