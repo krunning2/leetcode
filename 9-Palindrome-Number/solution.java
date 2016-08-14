dd219325b6861b0039a58ab816e65c8104c28b0d
krunning2 <zhidongke@yahoo.com>
2016-08-13 23:27:38 -0700

    [9. Palindrome Number][Accepted]committed by Krunning

--- 9-Palindrome-Number/solution.java
+++ 9-Palindrome-Number/solution.java
@@ -1,12 +1,13 @@
 public class Solution {
     public boolean isPalindrome(int x) {
         if(x < 0) return false;
-        int res = 0;
+        long res = 0;
         int y = x;
         while(x > 0){
             res = res * 10 + x % 10;
             x /= 10;
         }
-        return y == res;
+        if(res > Integer.MAX_VALUE) return false;
+        return y == ((int)res);
     }
 }
