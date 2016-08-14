3cf9a0666c17b6e2a004b6d133e53115ebc9e4f0
krunning2 <zhidongke@yahoo.com>
2016-08-13 23:43:13 -0700

    [148. Sort List][Runtime Error]committed by Krunning

---
+++ 148-Sort-List/solution.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head == null) return head;
+        ListNode mid = findMid(head);
+        ListNode next = mid.next;
+        mid.next = null;
+        return merge(sortList(head), sortList(next));
+    }
+    
+    private ListNode findMid(ListNode head){
+        ListNode fast = head;
+        ListNode slow = head;
+        while(true){
+            fast = fast.next;
+            if(fast == null) break;
+            fast = fast.next;
+            if(fast == null) break;
+            slow = slow.next;
+        }
+        return slow;
+    }
+    
+    private ListNode merge(ListNode l1, ListNode l2){
+        ListNode dummy = new ListNode(-1);
+        ListNode p = dummy;
+        while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                p.next = l1;
+                l1 = l1.next;
+            }else{
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+        if(l1 != null) p.next = l1;
+        if(l2 != null) p.next = l2;
+        return dummy.next;
+    }
+}
