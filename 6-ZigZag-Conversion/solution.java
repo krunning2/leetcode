178bb92381bd92951148590da6062b032a775b2a
krunning2 <zhidongke@yahoo.com>
2016-08-13 23:02:39 -0700

    [6. ZigZag Conversion][Accepted]committed by Krunning

--- 6-ZigZag-Conversion/solution.java
+++ 6-ZigZag-Conversion/solution.java
@@ -1,24 +1,22 @@
 public class Solution {
     public String convert(String s, int numRows) {
-        if(s == null || s.length() == 0 || numRows <= 0) return "";
-        StringBuilder[] sbs = new StringBuilder[numRows];
-        Arrays.fill(sbs, new StringBuilder());
-        int incre = 1;
-        int index = 0;
-        for(int i = 0; i < s.length(); i++){
-            sbs[index].append(s.charAt(i));
-            if(index == 0){
-                incre = 1;
-            }
-            if(index == numRows - 1){
-                incre = -1;
-            }
-            index += incre;
+        if(numRows<=1)return s;
+        StringBuilder[] sb=new StringBuilder[numRows];
+        for(int i=0;i<sb.length;i++){
+            sb[i]=new StringBuilder("");   //init every sb element **important step!!!!
         }
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < sbs.length; i++){
-            sb.append(sbs[i]);
+        int incre=1;
+        int index=0;
+        for(int i=0;i<s.length();i++){
+            sb[index].append(s.charAt(i));
+            if(index==0){incre=1;}
+            if(index==numRows-1){incre=-1;}
+            index+=incre;
         }
-        return sb.toString();
+        String re="";
+        for(int i=0;i<sb.length;i++){
+            re+=sb[i];
+        }
+        return re.toString();
     }
 }
