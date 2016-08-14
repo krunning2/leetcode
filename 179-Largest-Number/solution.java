e706b0fd9edfddd2588e6b0981f4c29397d6a817
krunning2 <zhidongke@yahoo.com>
2016-08-14 09:33:18 -0700

    [179. Largest Number][Compile Error]committed by Krunning

---
+++ 179-Largest-Number/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public String largestNumber(int[] nums) {
+        if(nums == null || nums.length == 0) return "";
+        String[] s_nums = new String[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            s_nums[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(nums.length, (a, b) -> (b+a).compareTo(a+b));
+        if(nums[0].equals("0")) return "0";
+        StringBuilder sb = new StringBuilder();
+        for(String s : s_nums){
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+}
