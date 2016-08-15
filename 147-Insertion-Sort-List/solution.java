8b41c611456594269fde09edd029577c7eff7011
krunning2 <zhidongke@yahoo.com>
2016-08-14 18:15:19 -0700

    [147. Insertion Sort List][Runtime Error]committed by Krunning

---
+++ 147-Insertion-Sort-List/solution.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode dummy = new ListNode(Integer.MIN_VALUE);
+        ListNode cur = head;
+        while(cur != null){
+            ListNode pre = findInsertLocation(dummy, cur.val);
+            ListNode next = cur.next;
+            cur.next = pre.next;
+            pre.next = cur;
+            cur = next;
+        }
+        return dummy.next;
+    }
+    private ListNode findInsertLocation(ListNode node, int val){
+        ListNode pre = null;
+        ListNode cur = node;
+        while(cur != null && cur.val >= val){
+            pre = cur;
+            cur = cur.next;
+        }
+        return pre;
+    }
+}
