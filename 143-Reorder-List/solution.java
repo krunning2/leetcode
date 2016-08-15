dcebe833cdacd2606c79ae2a007568338816c761
krunning2 <zhidongke@yahoo.com>
2016-08-14 21:53:33 -0700

    [143. Reorder List][Compile Error]committed by Krunning

---
+++ 143-Reorder-List/solution.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void reorderList(ListNode head) {
+        if(head == null || head.next == null) return;
+        ListNode mid = getMid(head);
+        ListNode next = mid.next;
+        mid.next = null;
+        ListNode newHead = reverse(next);
+        boolean flag = true;
+        while(head != null && newHead != null){
+            if(flag){
+                ListNode next = head.next;
+                head.next = newHead;
+                head = next;
+            }else{
+                ListNode next = newHead.next;
+                newHead.next = head;
+                newHead = next;
+            }
+            flag = !flag;
+        }
+    }
+    
+    private ListNode reverse(ListNode head){
+        ListNode pre = null, cur = head;
+        while(cur != null){
+            ListNode next = cur.next;
+            cur.next = pre;
+            cur = next;
+        }
+        return pre;
+    }
+    
+    private ListNode getMid(ListNode head){
+        ListNode fast = head, slow = head;
+        while(true){
+            fast = fast.next;
+            if(fast == null) break;
+            fast = fast.next;
+            if(fast == null) break;
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
