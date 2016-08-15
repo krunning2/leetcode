c5ff9e7496fbb2b4253ea5cfab1be7e6e9c8cbf3
krunning2 <zhidongke@yahoo.com>
2016-08-14 22:13:01 -0700

    [328. Odd Even Linked List][Accepted]committed by Krunning

---
+++ 328-Odd-Even-Linked-List/solution.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode newHead = head.next;
+        ListNode p = head;
+        while(p != null && p.next != null){
+            ListNode next = p.next;
+            p.next = p.next.next;
+            p = next;
+        }
+        p = head;
+        while(p != null && p.next != null){
+            p = p.next;
+        }
+        p.next = newHead;
+        return head;
+    }
+}
