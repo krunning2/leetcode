fc4c11bd2d9d5dd8c9a11e4a21126c678a81c5be
krunning2 <zhidongke@yahoo.com>
2016-08-14 23:39:01 -0700

    [74. Search a 2D Matrix][Accepted]committed by Krunning

--- 74-Search-a-2D-Matrix/solution.java
+++ 74-Search-a-2D-Matrix/solution.java
@@ -1,12 +1,12 @@
 public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if(matrix == null || matrix.length == 0 || matrix[0][0] < target) return false;
+        if(matrix == null || matrix.length == 0 || matrix[0][0] > target) return false;
         int start = 0;
         int end = matrix.length - 1;
         while(start + 1 < end){
             int mid = start - (start - end) / 2;
             if(matrix[mid][0] == target){
-                return mid;
+                return true;
             } else if(matrix[mid][0] > target){
                 end = mid;
             }else{
@@ -19,7 +19,7 @@ public class Solution {
         while(start + 1 < end){
             int mid = start - (start - end) / 2;
             if(matrix[row][mid] == target){
-                return mid;
+                return true;
             } else if(matrix[row][mid] > target){
                 end = mid;
             }else{
