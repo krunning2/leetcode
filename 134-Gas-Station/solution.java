14f6f5039897a4b8a902f21ab99e830e15d7c3ef
krunning2 <zhidongke@yahoo.com>
2016-08-14 23:18:00 -0700

    [134. Gas Station][Accepted]committed by Krunning

---
+++ 134-Gas-Station/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int sum = 0;
+        int total = 0;
+        int res = 0;
+        for(int i = 0; i < gas.length; i++){
+            total += gas[i] - cost[i];
+            sum += gas[i] - cost[i];
+            if(sum < 0){
+                sum = 0;
+                res = i + 1;
+            }
+        }
+        return total < 0 ? -1 : res;
+    }
+}
