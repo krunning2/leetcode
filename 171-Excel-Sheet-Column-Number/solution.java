22a4d51dea344390f59848196795bfb45297e1ad
krunning2 <zhidongke@yahoo.com>
2016-08-17 09:28:20 -0700

    [171. Excel Sheet Column Number][Time Limit Exceeded]committed by Krunning

--- 171-Excel-Sheet-Column-Number/solution.java
+++ 171-Excel-Sheet-Column-Number/solution.java
@@ -1,8 +1,11 @@
 public class Solution {
     public int titleToNumber(String s) {
         int res = 0;
-        for(int i = 0; i < s.length(); i++){
-            res = res * 26 + (s.charAt(i) - 'A' + 1);            
+        if(s == null || s.length() == 0) return 0;
+        int p = 0;
+        while(p < s.length()){
+            int cur = s.charAt(p) - 'A' + 1;
+            res = res * 26 + cur;
         }
         return res;
     }
