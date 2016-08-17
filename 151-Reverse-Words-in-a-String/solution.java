3073a8d4146ee998fd80c213c5e19eff257de8d4
krunning2 <zhidongke@yahoo.com>
2016-08-16 21:44:22 -0700

    [151. Reverse Words in a String][Compile Error]committed by Krunning

--- 151-Reverse-Words-in-a-String/solution.java
+++ 151-Reverse-Words-in-a-String/solution.java
@@ -2,7 +2,7 @@ public class Solution {
     public String reverseWords(String s) {
         String[] parts = s.split(" ");
         StringBuilder sb = new StringBuilder();
-        for(int i = parts.length() - 1; i >= 0; i--){
+        for(int i = parts.length - 1; i >= 0; i--){
             String s = parts[i];
             if(! "".equals(s)){
                 sb.append(s).append(" ");;
