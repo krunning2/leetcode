1233bba294ea8fbc098bd2bc51699305de50d7a6
krunning2 <zhidongke@yahoo.com>
2016-08-20 16:47:13 -0700

    [299. Bulls and Cows][Accepted]committed by Krunning

--- 299-Bulls-and-Cows/solution.java
+++ 299-Bulls-and-Cows/solution.java
@@ -1,16 +1,29 @@
 public class Solution {
     public String getHint(String secret, String guess) {
-        HashSet<Character> table = new HashSet<>();
+        HashMap<Character, Integer> map = new HashMap<>();
         int cow = 0;
         int bull = 0;
-        for(char c : secret){
-            table.add(c);
+        for(char c : secret.toCharArray()){
+            if(!map.containsKey(c)){
+                map.put(c, 1);
+            }else{
+                map.put(c, map.get(c) + 1);
+            }
         }
         for(int i = 0; i < guess.length(); i++){
-            if(guess.charAt(i) == secret.charAt(i)){
+            char g = guess.charAt(i);
+            char s = secret.charAt(i);
+            if(g == s){
                 bull++;
-            }else if(table.contains(guess.charAt(i))){
+                map.put(g, map.get(g) - 1);
+            }
+        }
+        for(int i = 0; i < guess.length(); i++){
+            char g = guess.charAt(i);
+            char s = secret.charAt(i);
+            if(g != s && map.containsKey(g) && map.get(g) > 0){
                 cow++;
+                map.put(g, map.get(g) - 1);
             }
         }
         return bull + "A" + cow + "B";
