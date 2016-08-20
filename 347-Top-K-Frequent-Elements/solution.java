9d9dc7be079fd0a2978223cc87dead1909bf4139
krunning2 <zhidongke@yahoo.com>
2016-08-20 12:32:20 -0700

    [347. Top K Frequent Elements][Compile Error]committed by Krunning

---
+++ 347-Top-K-Frequent-Elements/solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int n : nums){
+            if(map.containsKey(n)){
+                map.put(n, map.get(n) + 1);
+            }else{
+                map.put(n, 1);
+            }
+        }
+        PriorityQueue<Pair> queue = new PriorityQueue<Pair>((p1, p2) -> (p1.freq - p2.freq));
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            queue.offer(new Pair(entry.getKey(), entry.getValue()));
+            if(queue.size() > k) queue.poll();
+        }
+        return new ArrayList<Integer>(queue);
+    }
+    class Pair{
+        int val;
+        int freq;
+        Pair(int val, int freq){
+            this.val = val;
+            this.freq = freq;
+        }
+    }
+}
