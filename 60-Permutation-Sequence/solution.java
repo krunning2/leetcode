08aacc11b83509dce9d4160228d7b82ab50f5bf7
krunning2 <zhidongke@yahoo.com>
2016-08-20 10:20:35 -0700

    [60. Permutation Sequence][Runtime Error]committed by Krunning

---
+++ 60-Permutation-Sequence/solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public String getPermutation(int n, int k) {
+        List<Integer> nums = new ArrayList<>();
+        int fac = 1;
+        for(int i = 1; i <= n; i++){
+            nums.add(i);
+            fac *= i;
+        }
+        k--;
+        StringBuilder sb = new StringBuilder();
+        for(int i = n; i >= 1; i--){
+            fac /= i;
+            int index = k / fac;
+            sb.append(nums.get(i));
+            nums.remove(index);
+            k %= fac;
+        }
+        return sb.toString();
+    }
+}
