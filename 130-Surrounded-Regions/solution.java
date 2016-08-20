9299dfff74ffe6e4cafd1ab0b46616d47835c003
krunning2 <zhidongke@yahoo.com>
2016-08-20 12:14:44 -0700

    [130. Surrounded Regions][Compile Error]committed by Krunning

---
+++ 130-Surrounded-Regions/solution.java
@@ -0,0 +1,57 @@
+public class Solution {
+    private static final char GATE = 'O';
+    private static final char PASS = 'P';
+    private static final char WALL = 'X';
+    public void solve(char[][] board) {
+        if(board == null || board.length == 0) return;
+        Queue<Point> queue = new LinkedList<>();
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(board[i][j] == GATE){
+                    board[i][j] = PASS;
+                    if(i == 0 || j == 0 || i == board.length - 1 || j == board[0].length - 1){
+                        queue.offer(new Point(i, j));
+                        board[i][j] = GATE;
+                    }
+                }
+            }
+        }
+        int dx[] = {1, -1, 0, 0};
+        int dy[] = {0, 0, -1, 1};
+        while(! queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                Point p = queue.poll();
+                for(int i = 0;i < 4; i++){
+                    if(isValid(p.row + dx[i], p.col + dy[i], board)){
+                        queue.offer(new Point(p.row + dx[i], p.col + dy[i]));
+                        board[p.row + dx[i]][p.col + dy[i]] = GATE;
+                    }
+                }
+            }
+        }
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(board[i][j] != GATE){
+                    board[i][j] = WALL;
+                }
+            }
+        }
+    }
+    
+    private boolean isValid(int row, int col, char[][] b){
+        if(col < 0 || row < 0 || col >= b[0].length || row >= b.length || b[p.row][p.col] != PASS){
+            return false;
+        }
+        return true;
+    }
+    
+    class Point{
+        int row;
+        int col;
+        Point(int row, int col){
+            this.row = row;
+            this.col = co;
+        }
+    }
+}
