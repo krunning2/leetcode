f7aaa42bc8c887ba981a1c00e5a7d78b88d49b83
krunning2 <zhidongke@yahoo.com>
2016-08-20 16:04:01 -0700

    [347. Top K Frequent Elements][Compile Error]committed by Krunning

--- 347-Top-K-Frequent-Elements/solution.java
+++ 347-Top-K-Frequent-Elements/solution.java
@@ -9,16 +9,50 @@ public class Solution {
                 map.put(n, 1);
             }
         }
-        PriorityQueue<Pair> queue = new PriorityQueue<Pair>((p1, p2) -> (p1.freq - p2.freq));
+        Pair[] pairs = new Pair[map.size()];
+        int i = 0;
         for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-            queue.offer(new Pair(entry.getKey(), entry.getValue()));
-            if(queue.size() > k) queue.poll();
+            pairs[i++] = new Pair(entry.getKey(), entry.getValue());
         }
-        for(Pair p : queue){
-            res.add(p.val);
+        quickSelect(0, pairs.length - 1, pairs, k);
+        for(int i = 0; i < k; i++){
+            res.add(pairs[i].val);
         }
         return res;
     }
+    
+    private void quickSelect(int start, int end, Pair[] pairs, int k){
+        if(start > end){
+            return;
+        }
+        int index = partition(pairs, start, end, k);
+        if(index == k - 1){
+            return;
+        } else if(index > k - 1){
+            quickSelect(start, index - 1, pairs, k);
+        } else {
+            quickSelect(index + 1, end, pairs, k - index - 1);
+        }
+    }
+    
+    private int partition(Pair[] pairs, int start, int end){
+        int index = start;
+        Pair pivot = pairs[end];
+        for(int i = start; i < end; i++){
+            if(pairs[i] >= pivot.freq){
+                Pair tmp = pairs[i];
+                pairs[i] = pairs[index];
+                pairs[index] = tmp;
+                index++;
+            }
+        }
+        Pair tmp = pairs[end];
+        pairs[end] = pairs[index];
+        pairs[index] = tmp;
+        return index;
+    }
+    
+    
     class Pair{
         int val;
         int freq;
