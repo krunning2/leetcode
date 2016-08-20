8634d694e37d0888d983eab70cd7c6f171b8624a
krunning2 <zhidongke@yahoo.com>
2016-08-20 12:22:00 -0700

    [135. Candy][Compile Error]committed by Krunning

---
+++ 135-Candy/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int candy(int[] ratings) {
+        if(ratings == null) return 0;
+        int[] can = new int[ratings];
+        Arrays.fill(can, 1);
+        for(int i = 1; i < ratings.length; i++){
+            if(ratings[i] > ratings[i - 1]){
+                can[i] = can[i - 1] + 1;
+            }
+        }
+        for(int i = ratings.length - 2; i >= 0; i--){
+            if(ratings[i] > ratings[i + 1]){
+                can[i] = Math.max(can[i], can[i + 1] + 1);
+            }
+        }
+        int count = 0;
+        for(int x : can){
+            count += x;
+        }
+        return count;
+    }
+}
