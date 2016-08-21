24d2e32149462dac077ce59ec6d6b2e143ee1769
krunning2 <zhidongke@yahoo.com>
2016-08-21 10:48:32 -0700

    [65. Valid Number][Accepted]committed by Krunning

--- 65-Valid-Number/solution.java
+++ 65-Valid-Number/solution.java
@@ -1,28 +1,34 @@
 public class Solution {
     public boolean isNumber(String s) {
         s = s.trim();
-        boolean numSeen = false;
-        boolean eSeen = false;
-        boolean dotSeen = false;
+        boolean seenNum = false;
+        boolean seenDot = false;
+        boolean seenE = false;
         boolean numAfterE = false;
         for(int i = 0; i < s.length(); i++){
             char cur = s.charAt(i);
-            if(cur >= '0' && cur <= '9'){
-                numSeen = true;
+            if(cur <= '9' && cur >= '0'){
+                seenNum = true;
                 numAfterE = true;
-            }else if(cur == '.'){
-                if(eSeen || dotSeen) return false;
-                dotSeen = true;
             }else if(cur == 'e'){
-                if(eSeen || !numSeen) return false;
-                eSeen = true;
+                if(seenE || !seenNum){
+                    return false;
+                }
+                seenE = true;
                 numAfterE = false;
+            }else if(cur == '.'){
+                if(seenDot || seenE){
+                    return false;
+                }
+                seenDot = true;
             }else if(cur == '+' || cur == '-'){
-                if(i != 0 && s.charAt(i - 1) != 'e') return false;
-            }else {
+                if(i != 0 && s.charAt(i - 1) != 'e'){
+                    return false;
+                }
+            }else{
                 return false;
             }
         }
-        return numSeen && numAfterE;
+        return seenNum && numAfterE;
     }
 }
