6053fc378ecff792a7993bef6b454a2076cf92d6
krunning2 <zhidongke@yahoo.com>
2016-08-21 15:09:47 -0700

    [1. Two Sum][Accepted]committed by Krunning

--- 1-Two-Sum/solution.java
+++ 1-Two-Sum/solution.java
@@ -1,12 +1,14 @@
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
-            if(map.containsKey(target - nums[i]) && i != map.get(target - nums[i])){
-                return new int[]{map.get(target - nums[i]), i};
+            int cur = nums[i];
+            if(map.containsKey(target - cur)){
+                return new int[] {map.get(target - cur), i};
+            }else{
+                map.put(cur, i);
             }
-            map.put(nums[i], i);
         }
-        return new int[] {0,0};
+        return new int[]{-1, -1};
     }
 }
