0df0aa00f6bb3789a4a1d2002c835d45af9276e0
krunning2 <zhidongke@yahoo.com>
2016-08-21 15:06:23 -0700

    [245. Shortest Word Distance III][Accepted]committed by Krunning

--- 245-Shortest-Word-Distance-III/solution.java
+++ 245-Shortest-Word-Distance-III/solution.java
@@ -1,14 +1,25 @@
 public class Solution {
     public int shortestWordDistance(String[] words, String word1, String word2) {
-        HashMap<String, Integer> map = new HashMap<>();
+        int p1 = -1, p2 = -1;
         int res = Integer.MAX_VALUE;
         for(int i = 0; i < words.length; i++){
-            if(words[i].equals(word1) && map.containsKey(word2)){
-                res = Math.min(res, i - map.get(word2));
-            }else if(words[i].equals(word2) && map.containsKey(word1)){
-                res = Math.min(res, i - map.get(word1));
+            if(word1.equals(word2) && words[i].equals(word1)){
+                if(p1 == -1 || p2 > p1){
+                    p1 = i;
+                }else {
+                    p2 = i;
+                }
+            } else {
+               if(words[i].equals(word1)){
+                    p1 = i;
+                }
+                if(words[i].equals(word2)){
+                    p2 = i;
+                }
             }
-            map.put(words[i], i);
+            if(p1 != -1 && p2 != -1){
+                res = Math.min(Math.abs(p1 - p2), res);
+            } 
         }
         return res;
     }
