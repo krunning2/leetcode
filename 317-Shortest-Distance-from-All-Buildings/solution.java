d56d41c32d03d9b18f8ec4e2eb5283db3e8c7d62
krunning2 <zhidongke@yahoo.com>
2016-08-21 00:17:02 -0700

    [317. Shortest Distance from All Buildings][Compile Error]committed by Krunning

---
+++ 317-Shortest-Distance-from-All-Buildings/solution.java
@@ -0,0 +1,56 @@
+public class Solution {
+    public int shortestDistance(int[][] grid) {
+        int num = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dis = new int[m][n];
+        int[][] reach = new int[m][n];
+        boolean[][] visited = new boolean[m][n];
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 1){
+                    num++;
+                    int level = 0;
+                    Queue<Integer> q = new LinkedList<>();
+                    q.offer(i * n + j);
+                    Arrays.fill(visited, false);
+                    visited[i][j] = true;
+                    while(! q.isEmpty()){
+                        int size = 0;
+                        for(int i = 0; i < size; i++){
+                            int cur = q.poll();
+                            int row = cur / n;
+                            int col = cur % n;
+                            for(int i = 0; i < 4; i++){
+                                int x = row + dx[i];
+                                int y = col + dy[i];
+                                if(isValid(x, y, visited)){
+                                    q.offer(x * n + y);
+                                    dis[x][y] += level + 1;
+                                    visited[x][y] = true;
+                                    reach[x][y]++;
+                                }
+                            }
+                        }
+                    }
+                    level++;
+                }
+            }
+        }
+        int minDist = Integer.MAX_VALUE;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 0 && reach[i][j] == num){
+                    minDist = Math.min(minDist, dis[i][j]);
+                }
+            }
+        }
+        return minDist == Integer.MAX_VALUE ? -1 : minDist;
+    }
+    private boolean isValid(int x, int y, boolean[][] visited){
+        if(x < 0 || y < 0 || y >= visited[0].length || x >= visited.length || visited[x][y]) return false;
+        return true;
+    }
+}
