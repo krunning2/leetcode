f45f926155847f4d279b71b7bed41e5c8995f89b
krunning2 <zhidongke@yahoo.com>
2016-08-21 15:28:10 -0700

    [170. Two Sum III - Data structure design][Accepted]committed by Krunning

--- 170-Two-Sum-III---Data-structure-design/solution.java
+++ 170-Two-Sum-III---Data-structure-design/solution.java
@@ -4,7 +4,7 @@ public class TwoSum {
 
     // Add the number to an internal data structure.
 	public void add(int number) {
-	    if (map.containsKey(number)) map.put(number, map.get(number) + 1);
+	    if (map.containsKey(number)) map.put(number, 2);
 	    else {
 	        map.put(number, 1);
 	        list.add(number);
