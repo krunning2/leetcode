b1b87fc0167a8a03131e61c27428118e1092f999
krunning2 <zhidongke@yahoo.com>
2016-08-20 23:36:53 -0700

    [304. Range Sum Query 2D - Immutable][Accepted]committed by Krunning

--- 304-Range-Sum-Query-2D---Immutable/solution.java
+++ 304-Range-Sum-Query-2D---Immutable/solution.java
@@ -1,6 +1,7 @@
 public class NumMatrix {
     private int[][] sum;
     public NumMatrix(int[][] matrix) {
+        if(matrix == null || matrix.length == 0) return;
         int m = matrix.length;
         int n = matrix[0].length;
         sum = new int[m + 1][n + 1];
@@ -12,7 +13,8 @@ public class NumMatrix {
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        return sum[row2 + 1][col2 + 1] - sum[row2 + 1][col1 + 1] - sum[row1 + 1][col2 + 1] + sum[row1 + 1][col1 + 1];
+        if(sum == null) return 0;
+        return sum[row2 + 1][col2 + 1] - sum[row2 + 1][col1] - sum[row1][col2 + 1] + sum[row1][col1];
     }
 }
 
