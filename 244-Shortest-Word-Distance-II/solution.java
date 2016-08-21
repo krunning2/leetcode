78c4f771d1e30e4ed5817a90a97b9ee39653ab21
krunning2 <zhidongke@yahoo.com>
2016-08-21 14:45:31 -0700

    [244. Shortest Word Distance II][Compile Error]committed by Krunning

--- 244-Shortest-Word-Distance-II/solution.java
+++ 244-Shortest-Word-Distance-II/solution.java
@@ -1,32 +1,32 @@
 public class WordDistance {
-    Map<String, List<Integer>> map;
+    Map<String, List<String>> map;
     public WordDistance(String[] words) {
-        map = new HashMap<String, List<Integer>>();
+        map = new HashMap<>();
         for(int i = 0; i < words.length; i++){
-            String s = words[i];
-            if(!map.containsKey(s)){
-                map.put(s, new ArrayList<Integer>());
+            if(!map.containsKey(words[i])){
+                map.put(words[i], new ArrayList<>());
             }
-            map.get(s).add(i);
+            map.get(words[i]).add(i);
         }
-        
     }
 
     public int shortest(String word1, String word2) {
-        List<Integer> l1 = map.get(word1);
-        List<Integer> l2 = map.get(word2);
-        int i = 0;
-        int j = 0;
-        int min = Integer.MAX_VALUE;
-        while(i < l1.size() && j < l2.size()){
-            min = Math.min(min, Math.abs(l1.get(i) - l2.get(j)));
-            if(l1.get(i) > l2.get(j)){
-                j++;
+        List<String> l1 = map.get(word1);
+        List<String> l2 = map.get(word2);
+        int res = Integer.MAX_VALUE;
+        int p1 = 0, p2 = 0;
+        while(p1 < l1.size() && p2 < l2.size()){
+            if(l1.get(p1) == l2.get(p2)){
+                return 0;
+            }else if(l1.get(p1) > l2.get(p2)){
+                res = Math.min(l1.get(p1) - l2.get(p2));
+                p2++;
             }else{
-                i++;                
+                res = Math.min(l2.get(p2) - l1.get(p1));
+                p1++;
             }
         }
-        return min;
+        return res;
     }
 }
 
