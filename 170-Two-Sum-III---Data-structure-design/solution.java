95b9b6390ef72170565babf5f8497599968d788b
krunning2 <zhidongke@yahoo.com>
2016-08-21 15:29:21 -0700

    [170. Two Sum III - Data structure design][Accepted]committed by Krunning

--- 170-Two-Sum-III---Data-structure-design/solution.java
+++ 170-Two-Sum-III---Data-structure-design/solution.java
@@ -1,23 +1,29 @@
 public class TwoSum {
-    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+    private List<Integer> list = new ArrayList<Integer>();
+    private Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
     // Add the number to an internal data structure.
-	public void add(int num) {
-	    if(map.containsKey(num)){
-	        map.put(num, 2);
-	    }else{
-	        map.put(num, 1);
+	public void add(int number) {
+	    if (map.containsKey(number)){
+	        map.put(number, 2);
+	    } else {
+	        map.put(number, 1);
+	        list.add(number);
 	    }
 	}
 
     // Find if there exists any pair of numbers which sum is equal to the value.
 	public boolean find(int value) {
-	    for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-	        int num1 = entry.getKey();
-	        int num2 = value - num1;
-	        if(map.containsKey(num2) && (entry.getValue() > 1 || num1 != num2)){
-	              return true;
-	        }
+	    for (int i = 0; i < list.size(); i++){
+	        int num1 = list.get(i), num2 = value - num1;
+	        if ((num1 == num2 && map.get(num1) > 1) || (num1 != num2 && map.containsKey(num2))) return true;
 	    }
 	    return false;
 	}
 }
+
+
+// Your TwoSum object will be instantiated and called as such:
+// TwoSum twoSum = new TwoSum();
+// twoSum.add(number);
+// twoSum.find(value);
