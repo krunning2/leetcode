c0e8f9d5a2cc70cdb83fc712487822cffee75f6d
krunning2 <zhidongke@yahoo.com>
2016-08-21 14:28:52 -0700

    [243. Shortest Word Distance][Compile Error]committed by Krunning

--- 243-Shortest-Word-Distance/solution.java
+++ 243-Shortest-Word-Distance/solution.java
@@ -1,19 +1,16 @@
 public class Solution {
     public int shortestDistance(String[] words, String word1, String word2) {
-        int p1 = -1;
-        int p2 = -1;
-        int min = Integer.MAX_VALUE;
+        HashMap<String, Integer> map = new HashMap<>();
+        int res = Integer.MAX_VALUE;
         for(int i = 0; i < words.length; i++){
-            if(word1.equals(words[i])){
-                p1 = i;
-            }
-            if(word2.equals(words[i])){
-                p2 = i;
-            }
-            if(p1 != -1 && p2 != -1){
-                min = Math.min(min, Math.abs(p1 - p2));
+            if(words[i].equals(word1) && map.containsKey(word2)){
+                res = Math.min(res, i - map.get(word2));
+            }else if(words[i].equals(word2) && map.containsKey(word1)){
+                res = Math.min(res, i - map.get(word1)):
+            }else{
+                map.put(words[i], i);
             }
         }
-        return min;
+        return res;
     }
 }
