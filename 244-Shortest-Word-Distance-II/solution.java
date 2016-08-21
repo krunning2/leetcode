33466fc27c3f1ba473a13e43ccff0f797d87daeb
krunning2 <zhidongke@yahoo.com>
2016-08-21 14:46:30 -0700

    [244. Shortest Word Distance II][Compile Error]committed by Krunning

--- 244-Shortest-Word-Distance-II/solution.java
+++ 244-Shortest-Word-Distance-II/solution.java
@@ -11,8 +11,8 @@ public class WordDistance {
     }
 
     public int shortest(String word1, String word2) {
-        List<String> l1 = map.get(word1);
-        List<String> l2 = map.get(word2);
+        List<Integer> l1 = map.get(word1);
+        List<Integer> l2 = map.get(word2);
         int res = Integer.MAX_VALUE;
         int p1 = 0, p2 = 0;
         while(p1 < l1.size() && p2 < l2.size()){
