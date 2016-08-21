d5d755861c85d045cce350127eb8786ebcf433ce
krunning2 <zhidongke@yahoo.com>
2016-08-21 00:31:47 -0700

    [317. Shortest Distance from All Buildings][Accepted]committed by Krunning

--- 317-Shortest-Distance-from-All-Buildings/solution.java
+++ 317-Shortest-Distance-from-All-Buildings/solution.java
@@ -1,56 +1,53 @@
 public class Solution {
     public int shortestDistance(int[][] grid) {
-        int num = 0;
+        if (grid==null || grid.length==0 || grid[0].length==0) return -1;
         int m = grid.length;
         int n = grid[0].length;
-        int[][] dis = new int[m][n];
+        int[][] dist = new int[m][n];
         int[][] reach = new int[m][n];
+        int houseNum = 0;
+        int[][] directions = {{-1,0},{1,0},{0,-1},{0,1}};
         
-        int[] dx = {1, -1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(grid[i][j] == 1){
-                    num++;
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (grid[i][j] == 1) {
+                    houseNum++;
                     int level = 0;
-                    Queue<Integer> q = new LinkedList<>();
-                    q.offer(i * n + j);
                     boolean[][] visited = new boolean[m][n];
+                    LinkedList<Integer> queue = new LinkedList<Integer>();
+                    queue.offer(i*n+j);
                     visited[i][j] = true;
-                    while(! q.isEmpty()){
-                        int size = q.size();
-                        for(int k = 0; k < size; k++){
-                            int cur = q.poll();
-                            int row = cur / n;
-                            int col = cur % n;
-                            for(int l = 0; l < 4; l++){
-                                int x = row + dx[l];
-                                int y = col + dy[l];
-                                if(isValid(x, y, visited)){
-                                    q.offer(x * n + y);
-                                    dis[x][y] += level + 1;
-                                    visited[x][y] = true;
-                                    reach[x][y]++;
+                    while (!queue.isEmpty()) {
+                        int size = queue.size();
+                        for (int t=0; t<size; t++) {
+                            int cur = queue.poll();
+                            int x = cur/n;
+                            int y = cur%n;
+                            for (int[] dir : directions) {
+                                int xnew = x + dir[0];
+                                int ynew = y + dir[1];
+                                if (xnew>=0 && xnew<m && ynew>=0 && ynew<n && !visited[xnew][ynew] && grid[xnew][ynew]==0) {
+                                    queue.offer(xnew*n+ynew);
+                                    visited[xnew][ynew] = true;
+                                    dist[xnew][ynew] += level+1;
+                                    reach[xnew][ynew]++;
                                 }
                             }
                         }
+                        level++;
                     }
-                    level++;
                 }
             }
         }
+        
         int minDist = Integer.MAX_VALUE;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(grid[i][j] == 0 && reach[i][j] == num){
-                    minDist = Math.min(minDist, dis[i][j]);
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (grid[i][j]==0 && reach[i][j] == houseNum) {
+                    minDist = Math.min(minDist, dist[i][j]);
                 }
             }
         }
-        return minDist == Integer.MAX_VALUE ? -1 : minDist;
-    }
-    private boolean isValid(int x, int y, boolean[][] visited, int[][] grid){
-        if(x < 0 || y < 0 || y >= visited[0].length || x >= visited.length || visited[x][y] || grid[x][y] != 0) return false;
-        return true;
+        return minDist==Integer.MAX_VALUE? -1 : minDist;
     }
 }
