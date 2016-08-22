f9eed37618b541eabe040821bdc5d34889d8528e
krunning2 <zhidongke@yahoo.com>
2016-08-21 22:55:23 -0700

    [150. Evaluate Reverse Polish Notation][Runtime Error]committed by Krunning

--- 150-Evaluate-Reverse-Polish-Notation/solution.java
+++ 150-Evaluate-Reverse-Polish-Notation/solution.java
@@ -1,25 +1,24 @@
 public class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<String> stack = new Stack();
-        for(int i = 0 ; i < tokens.length; i++){
-            String cur = tokens[i];
-            if("+-*/".contains(cur)){
-                int d1 = Integer.valueOf(stack.pop());
-                int d2 = Integer.valueOf(stack.pop());
-                stack.push(String.valueOf(cal(d1, d2, cur)));
+        Stack<String> digs = new Stack<>();
+        for(String t : tokens){
+            if("+-*/".contains(t)){
+                int d1 = Integer.valueOf(digs.pop());
+                int d2 = Integer.valueOf(digs.pop());
+                digs.push(String.valueOf(cal(d1, d2, t)));
             }else{
-                stack.push(cur);
+                digs.push(t);
             }
         }
-        return Integer.valueOf(stack.pop());
+        return Integer.valueOf(digs.pop());
     }
     private int cal(int d1, int d2, String op){
         switch(op){
-            case "+" : return (d1 + d2);
-            case "*" : return (d1 * d2);
-            case "-" : return (d2 - d1);
-            case "/" : return (d2 / d1);
-            default : return 0;
+            case "+" : return d1 + d2;
+            case "*" : return d1 * d2;
+            case "/" : return d2 - d1;
+            case "-" : return d2 / d1;
+            default: return 0;
         }
     }
 }
