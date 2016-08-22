d8051c2e0695f1f49500e8374f548dcd7a95829e
krunning2 <zhidongke@yahoo.com>
2016-08-21 17:15:03 -0700

    [205. Isomorphic Strings][Accepted]committed by Krunning

--- 205-Isomorphic-Strings/solution.java
+++ 205-Isomorphic-Strings/solution.java
@@ -1,14 +1,19 @@
 public class Solution {
     public boolean isIsomorphic(String s, String t) {
-        Map<Character, Character> map = new HashMap<Character, Character>();
-        Set<Character> set = new HashSet<Character>();
         if(s == null || t == null) return s == null && t == null;
+        Map<Character, Character> map = new HashMap<>();
+        Set<Character> set = new HashSet<>();
         for(int i = 0; i < s.length(); i++){
-            if(map.containsKey(s.charAt(i)) && t.charAt(i) != map.get(s.charAt(i)) ||  set.contains(t.charAt(i))&& !map.containsKey(s.charAt(i))){
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            if(map.containsKey(c1) && map.get(c1) != c2){
                 return false;
+            }else if(!map.containsKey(c1) && set.contains(c2)){
+                return false;
+            }else{
+                map.put(c1, c2);
+                set.add(c2);
             }
-            set.add(t.charAt(i));
-            map.put(s.charAt(i), t.charAt(i));
         }
         return true;
     }
