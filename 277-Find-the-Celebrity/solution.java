e467366dcc33005f0645ade90703036632f04f97
krunning2 <zhidongke@yahoo.com>
2016-08-22 09:18:29 -0700

    [277. Find the Celebrity][Accepted]committed by Krunning

--- 277-Find-the-Celebrity/solution.java
+++ 277-Find-the-Celebrity/solution.java
@@ -3,19 +3,17 @@
 
 public class Solution extends Relation {
     public int findCelebrity(int n) {
-        int res = 0;
+        int cel = 0;
         for(int i = 0; i < n; i++){
-            if(!knows(i, res)){
-                res = i;
+            if(!knows(i, cel)){
+                cel = i;
             }
         }
         for(int i = 0; i < n; i++){
-            if(res != i){
-                if(knows(res, i) || !knows(i, res)){
-                    return -1;
-                }
+            if(i != cel && (knows(cel, i) || !knows(i, cel))){
+                return -1;
             }
         }
-        return res;
+        return cel;
     }
 }
