332cb718b23e395da01978a77528bb06a8eb2ecc
krunning2 <zhidongke@yahoo.com>
2016-08-21 18:50:18 -0700

    [256. Paint House][Accepted]committed by Krunning

--- 256-Paint-House/solution.java
+++ 256-Paint-House/solution.java
@@ -1,18 +1,16 @@
 public class Solution {
     public int minCost(int[][] costs) {
         if(costs == null || costs.length == 0) return 0;
-        int lastR = costs[0][0];
-        int lastY = costs[0][1];
-        int lastG = costs[0][2];
+        int[] preColCost = {costs[0][0], costs[0][1], costs[0][2]};
+        
         for(int i = 1; i < costs.length; i++){
-            int curR = Math.min(lastY, lastG) + costs[i][0];
-            int curY = Math.min(lastR, lastG) + costs[i][1];
-            int curG = Math.min(lastR, lastY) + costs[i][2];
-            lastR = curR;
-            lastY = curY;
-            lastG = curG;
+            int red = Math.min(preColCost[1], preColCost[2]) + costs[i][0];
+            int blue = Math.min(preColCost[0], preColCost[2]) + costs[i][1];
+            int green = Math.min(preColCost[1], preColCost[0]) + costs[i][2];
+            preColCost[0] = red;
+            preColCost[1] = blue;
+            preColCost[2] = green;
         }
-        return Math.min(Math.min(lastG, lastY), lastR);
+        return Math.min(preColCost[0], Math.min(preColCost[1], preColCost[2]));
     }
 }
-// Defind LsatR as the Minimum cost that paint current house as Red and including the cost of painting previous house
