5e02ff4d9f4e55c5a2f444cb695f3eb2a7f16928
krunning2 <zhidongke@yahoo.com>
2016-08-21 23:31:36 -0700

    [238. Product of Array Except Self][Runtime Error]committed by Krunning

--- 238-Product-of-Array-Except-Self/solution.java
+++ 238-Product-of-Array-Except-Self/solution.java
@@ -1,15 +1,14 @@
 public class Solution {
     public int[] productExceptSelf(int[] nums) {
-        if(nums == null || nums.length == 0) return new int[0];
         int[] res = new int[nums.length];
         res[0] = 1;
-        for(int i = 1; i < nums.length; i++){
-            res[i] = nums[i - 1] * res[i - 1];
+        for(int i = 2; i < nums.length; i++){
+            res[i] = res[i - 1] * nums[i - 1];
         }
-        int tmp = nums[nums.length - 1];
-        for(int i = nums.length - 2; i >= 0; i--){
-            res[i] *= tmp;
-            tmp *= nums[i];
+        int right = res[nums.length - 1] ;
+        for(int i = nums.length - 2; i >= 0; i++){
+            res[i] *= right;
+            right *= nums[i];
         }
         return res;
     }
