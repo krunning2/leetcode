c51d0221b6469ff41773369a6534788b3a5c4663
krunning2 <zhidongke@yahoo.com>
2016-08-23 09:11:21 -0700

    [44. Wildcard Matching][Runtime Error]committed by Krunning

--- 44-Wildcard-Matching/solution.java
+++ 44-Wildcard-Matching/solution.java
@@ -1,26 +1,29 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
-        int p1 = 0, p2 = 0, s1 = 0, s2 = 0;
-        boolean is_star = false;
-        while(s1 < s.length()){
-            if(p1 < p.length() && (s.charAt(s1) == p.charAt(p1) || p.charAt(p1) == '?')){
-                s1 ++;
-                p1 ++;
-            }else if(p1 < p.length() && p.charAt(p1) == '*'){
-                is_star = true;
-                p2 = ++p1;
-                s2 = s1;
-               
-            }else if(p1 == p.length() || p.charAt(p1) != s.charAt(s1)){
-                if(!is_star) return false;
-                s2++;
-                p1 = p2;
-                s1 = s2;
+        int star1 = -1;
+        int star2 = -1;
+        int p1 = 0, p2 = 0;
+        while(p1 <= s.length()){
+            if(p1 >= s.length() && p2 >= p.length()){
+                return true;
+            }else if(p2 < p.length() && (p.charAt(p2) == s.charAt(p1) || p.charAt(p2) == '?')){
+                p1++;
+                p2++;
+            }else if(p2 < p.length() && p.charAt(p2) == '*'){
+                star1 = p1;
+                p2 ++;
+                star2 = p2;
+            }else if(p2 >= p.length() || p.charAt(p2) != s.charAt(p1)){
+                if(star1 == -1){
+                    return false;
+                }
+                p2 = star2;
+                p1 = star1;
+                p1++;
+                star2++;
+                star1++;
             }
         }
-        while(p1 < p.length() && p.charAt(p1) == '*'){
-            p1 ++;
-        }
-        return p1 == p.length();
+        return false;
     }
 }
