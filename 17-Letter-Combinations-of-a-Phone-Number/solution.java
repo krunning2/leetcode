e002e92c56d9dcdd3992410e405dae2b28e499ee
krunning2 <zhidongke@yahoo.com>
2016-08-23 08:29:01 -0700

    [17. Letter Combinations of a Phone Number][Runtime Error]committed by Krunning

--- 17-Letter-Combinations-of-a-Phone-Number/solution.java
+++ 17-Letter-Combinations-of-a-Phone-Number/solution.java
@@ -1,21 +1,23 @@
 public class Solution {
     public List<String> letterCombinations(String digits) {
-        String[] dict = {"","", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        List<String> res = new ArrayList<String> ();
-        if(digits == null || digits.length() == 0) return res;
-        DFS(dict, new StringBuilder(), digits, 0, res);
+        String[] letters = {"", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        List<String> res = new ArrayList<String>();
+        if(digits == null || digits.length() == 0){
+            return res;
+        }
+        DFS(digits, 0, new StringBuilder(), res, letters);
         return res;
     }
-    private void DFS(String[] dict, StringBuilder sb, String digits, int pos, List<String> res){
+    private void DFS(String digits, int pos, StringBuilder sb, List<String> res, String[] letters){
         if(pos == digits.length()){
             res.add(sb.toString());
             return;
         }
-        String chars = dict[digits.charAt(pos) - '0'];
-        for(int i = 0; i < chars.length(); i++){
+        String s = letters[digits.charAt(pos) - '0'];
+        for(int i = 0; i < s.length(); i++){
             int len = sb.length();
-            sb.append(chars.charAt(i));
-            DFS(dict, sb, digits, pos + 1, res);
+            sb.append(s.charAt(i));
+            DFS(digits, pos + 1, sb, res, letters);
             sb.setLength(len);
         }
     }
