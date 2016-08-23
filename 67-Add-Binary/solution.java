2805e2510e63b624effe005179e28646023a3458
krunning2 <zhidongke@yahoo.com>
2016-08-22 23:10:17 -0700

    [67. Add Binary][Accepted]committed by Krunning

--- 67-Add-Binary/solution.java
+++ 67-Add-Binary/solution.java
@@ -1,23 +1,27 @@
 public class Solution {
     public String addBinary(String a, String b) {
         int carry = 0;
-        int p1 = 0, p2 = 0;
+        int p1 = a.length() - 1, p2 = b.length() - 1;
         StringBuilder sb = new StringBuilder();
-        while(p1 < a.length() && p2 < b.length()){
+        while(p1 >= 0 && p2 >= 0){
             int x1 = a.charAt(p1) - '0';
             int x2 = b.charAt(p2) - '0';
             sb.insert(0, (carry + x1 + x2) % 2);
             carry = (carry + x1 + x2) / 2;
+            p1--;
+            p2--;
         }
-        while(p1 < a.length()){
+        while(p1 >= 0){
             int x1 = a.charAt(p1) - '0';
             sb.insert(0, (carry + x1) % 2);
             carry = (carry + x1) / 2;
+            p1--;
         }
-        while(p2 < b.length()){
+        while(p2 >= 0){
             int x2 = b.charAt(p2) - '0';
             sb.insert(0, (carry + x2) % 2);
             carry = (carry + x2) / 2;
+            p2--;
         }
         if(carry > 0) sb.insert(0, 1);
         return sb.toString();
