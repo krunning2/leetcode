7b31fce5f13af868af4f94884503e8e0b6d46d19
krunning2 <zhidongke@yahoo.com>
2016-08-23 09:35:12 -0700

    [44. Wildcard Matching][Accepted]committed by Krunning

--- 44-Wildcard-Matching/solution.java
+++ 44-Wildcard-Matching/solution.java
@@ -10,8 +10,8 @@ public class Solution {
                 seen_star = true;
                 p2 = p1;
                 s2 = s1;
-                // p2++;
-                // p1 = p2;
+                p2++;
+                p1 = p2;
             }else if(p1 == p.length() || p.charAt(p1) != s.charAt(s1)){
                 if(!seen_star) return false;
                 s2++;
