1920525c7515ea326b27f362e8e0e5c6c3e97bad
krunning2 <zhidongke@yahoo.com>
2016-08-22 23:05:36 -0700

    [67. Add Binary][Time Limit Exceeded]committed by Krunning

--- 67-Add-Binary/solution.java
+++ 67-Add-Binary/solution.java
@@ -1,35 +1,25 @@
 public class Solution {
     public String addBinary(String a, String b) {
         int carry = 0;
-        int l1 = a.length() - 1;
-        int l2 = b.length() - 1;
+        int p1 = 0, p2 = 0;
         StringBuilder sb = new StringBuilder();
-        while(l1 >= 0 && l2 >= 0){
-            int d1 = a.charAt(l1) - '0';
-            int d2 = b.charAt(l2) - '0';
-            int cur = d1 + d2 + carry;
-            sb.append(cur % 2);
-            carry = cur / 2;
-            l1 --;
-            l2 --;
+        while(p1 < a.length() && p2 < b.length()){
+            int x1 = a.charAt(p1) - '0';
+            int x2 = b.charAt(p2) - '0';
+            sb.insert(0, (carry + x1 + x2) % 2);
+            carry = (carry + x1 + x2) / 2;
         }
-        while(l1 >= 0){
-            int d1 = a.charAt(l1) - '0';
-            int cur = d1 + carry;
-            sb.append(cur % 2);
-            carry = cur / 2;
-            l1 --;
+        while(p1 < a.length()){
+            int x1 = a.charAt(p1) - '0';
+            sb.insert(0, (carry + x1) % 2);
+            carry = (carry + x1) / 2;
         }
-        while(l2 >= 0){
-            int d2 = b.charAt(l2) - '0';
-            int cur = d2 + carry;
-            sb.append(cur % 2);
-            carry = cur / 2;
-            l2 --;
+        while(p2 < b.length()){
+            int x2 = b.charAt(p2) - '0';
+            sb.insert(0, (carry + x2) % 2);
+            carry = (carry + x2) / 2;
         }
-        if(carry > 0){
-            sb.append(1);
-        }
-        return sb.reverse().toString();
+        if(carry > 0) sb.insert(0, 1);
+        return sb.toString();
     }
 }
