c72ca87e66fdf2bb408e90ca3d9c9d6a10e56657
krunning2 <zhidongke@yahoo.com>
2016-08-23 08:17:59 -0700

    [91. Decode Ways][Accepted]committed by Krunning

--- 91-Decode-Ways/solution.java
+++ 91-Decode-Ways/solution.java
@@ -1,30 +1,28 @@
 public class Solution {
     public int numDecodings(String s) {
-        if(s == null || s.length() == 0) return 0;
-        int n = s.length();
-        if(n == 1) return check1(s.charAt(0));
+        if(s == null || s.length() == 0){
+            return 0;
+        }
         int[] dp = new int[3];
-        dp[0] = check1(s.charAt(0));
-        
-        for(int i = 1; i < n; i++){
-            int first = Integer.parseInt(s.substring(i, i + 1));
-            int second = Integer.parseInt(s.substring(i - 1, i + 1));
+        dp[0] = s.charAt(0) == '0' ? 0 : 1;
+        if(s.length() == 1){
+            return dp[0];
+        }
+        for(int i = 1; i < s.length(); i++){
+            int one = Integer.valueOf(s.substring(i, i + 1));
+            int two = Integer.valueOf(s.substring(i - 1, i + 1));
             dp[i % 3] = 0;
-            if(first >= 1 && first <= 9){
-                dp[i % 3] = dp[(i - 1) % 3];
+            if(one >= 1 && one <= 9){
+                dp[i % 3] += dp[(i - 1) % 3];
             }
-            if(second >= 10 && second <= 26){
-                if(i > 1){
-                    dp[i % 3] += dp[(i - 2) % 3];
+            if(two >= 10 && two <= 26){
+                if(i == 1){
+                    dp[i % 3] += 1;
                 }else{
-                    dp[i % 3] += 1; 
+                    dp[i % 3] += dp[(i - 2) % 3];
                 }
             }
         }
-        return dp[(n - 1) % 3];
+        return dp[(s.length() - 1) % 3];
     }
-    private int check1(char c){
-        return c == '0' ? 0 : 1;
-    }
-    
 }
