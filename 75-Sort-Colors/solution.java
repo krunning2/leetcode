b7cf36da380209314f33480544ed48e76e029c37
krunning2 <zhidongke@yahoo.com>
2016-08-24 09:20:23 -0700

    [75. Sort Colors][Accepted]committed by Krunning

--- 75-Sort-Colors/solution.java
+++ 75-Sort-Colors/solution.java
@@ -1,15 +1,16 @@
 public class Solution {
     public void sortColors(int[] nums) {
-        if(nums == null) return;
-        int left = 0, right = nums.length - 1, p = 0;
-        while(p <= right){
+        int start = 0, end = nums.length - 1, p = 0;
+        while(p <= end){
             if(nums[p] == 0){
-                nums[p] = nums[left];
-                nums[left++] = 0;
+                nums[p] = nums[start];
+                nums[start] = 0;
+                start ++;
                 p++;
             }else if(nums[p] == 2){
-                nums[p] = nums[right];
-                nums[right--] = 2;
+                nums[p] = nums[end];
+                nums[end] = 2;
+                end--;
             }else{
                 p++;
             }
