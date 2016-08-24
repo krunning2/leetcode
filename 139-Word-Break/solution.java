a55db502badb6978c021c932b7028dd7d00524de
krunning2 <zhidongke@yahoo.com>
2016-08-23 21:10:50 -0700

    [139. Word Break][Accepted]committed by Krunning

--- 139-Word-Break/solution.java
+++ 139-Word-Break/solution.java
@@ -1,23 +1,15 @@
 public class Solution {
     public boolean wordBreak(String s, Set<String> wordDict) {
         if(s == null) return false;
-        return DFS(s, 0, wordDict, new int[s.length() + 1]);
-    }
-    private boolean DFS(String s, int pos, Set<String> wordDict, int[] mem){
-        if(pos == s.length()){
-            return true;
-        }
-        if(mem[pos] != 0) return mem[pos] == 1;
-        for(int i = pos; i < s.length(); i++){
-            String sub = s.substring(pos, i + 1);
-            if(wordDict.contains(sub)){
-                if(DFS(s, i + 1, wordDict, mem)){
-                    mem[pos] = 1;
-                    return true;
-                } 
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for(int i = 0; i <= s.length(); i++){
+            for(int j = 0; j <= i; j++){
+                if(dp[j] && wordDict.contains(s.substring(j, i))){
+                    dp[i] = true;
+                }
             }
         }
-        mem[pos] = -1;
-        return false;
+        return dp[s.length()];
     }
 }
