9ba0fdcfb84df7c65af039e16429659019548b1a
krunning2 <zhidongke@yahoo.com>
2016-08-23 21:11:41 -0700

    [139. Word Break][Accepted]committed by Krunning

--- 139-Word-Break/solution.java
+++ 139-Word-Break/solution.java
@@ -4,7 +4,7 @@ public class Solution {
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
         for(int i = 0; i <= s.length(); i++){
-            for(int j = 0; j <= i; j++){
+            for(int j = 0; j < i; j++){
                 if(dp[j] && wordDict.contains(s.substring(j, i))){
                     dp[i] = true;
                 }
