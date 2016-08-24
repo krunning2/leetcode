39dfe2835c3d9145eb07d4ce6cea384e1eaa72f3
krunning2 <zhidongke@yahoo.com>
2016-08-23 20:43:47 -0700

    [10. Regular Expression Matching][Accepted]committed by Krunning

--- 10-Regular-Expression-Matching/solution.java
+++ 10-Regular-Expression-Matching/solution.java
@@ -1,25 +1,25 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
-        if(s == null || p == null) return p == null && s == null;
-        return helper(s, 0, p, 0, new int[s.length() + 1][p.length() + 1]);
+        return helper(s, p, 0, 0, new int[s.length() + 1][p.length() + 1]);
     }
-    private boolean helper(String s, int ps, String p, int pp, int[][] mem){
+    private boolean helper(String s, String p, int ps, int pp, int[][] mem){
         if(pp == p.length()){
             return ps == s.length();
         }
-        if(mem[ps][pp] != 0) return mem[ps][pp] == 1;
-        //
+        if(mem[ps][pp] != 0){
+            return mem[ps][pp] == 1;
+        }
         if(pp < p.length() - 1 && p.charAt(pp + 1) == '*'){
             while(pp < p.length() && ps < s.length() && (p.charAt(pp) == s.charAt(ps) || p.charAt(pp) == '.')){
-                if(helper(s, ps, p, pp + 2, mem)){
+                if(helper(s, p, ps, pp + 2, mem)){
                     mem[ps][pp] = 1;
                     return true;
                 }
                 ps++;
             }
-            return helper(s, ps, p, pp + 2, mem);
+            return helper(s, p, ps, pp + 2, mem);
         }else if(pp < p.length() && ps < s.length() && (p.charAt(pp) == s.charAt(ps) || p.charAt(pp) == '.')){
-            return helper(s, ps + 1, p, pp + 1, mem);
+            return helper(s, p, ps + 1, pp + 1, mem);
         }
         mem[ps][pp] = -1;
         return false;
