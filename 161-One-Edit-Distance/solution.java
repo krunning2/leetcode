81e4cbba040508897539b38633c84e1e40fce749
krunning2 <zhidongke@yahoo.com>
2016-08-24 21:42:47 -0700

    [161. One Edit Distance][Accepted]committed by Krunning

--- 161-One-Edit-Distance/solution.java
+++ 161-One-Edit-Distance/solution.java
@@ -2,20 +2,20 @@ public class Solution {
     public boolean isOneEditDistance(String s, String t) {
         if(s == null && t != null) return t.length() == 1;
         if(t == null && s != null) return s.length() == 1;
-        if(s.length() == t.length()){
-            return isModified(s, t);
-        }else if(s.length() - t.length() == 1){
+        if(s.length() - t.length() == 1){
             return isDeleted(s, t);
-        }else if(t.length() - s.length() == 1){
+        } else if(t.length() - s.length() == 1){
             return isDeleted(t, s);
+        }else if(s.length() == t.length()){
+            return isModified(s, t);
         }else{
             return false;
         }
     }
-    private boolean isModified(String t, String s){
+    private boolean isModified(String s, String t){
         boolean flag = false;
-        for(int i = 0; i < t.length(); i++){
-            if(t.charAt(i) != s.charAt(i)){
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) != t.charAt(i)){
                 if(flag) return false;
                 flag = true;
             }
