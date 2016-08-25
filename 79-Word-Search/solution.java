6ef7956fce1b2c629e2feefbce699bc12dbffd4a
krunning2 <zhidongke@yahoo.com>
2016-08-24 20:29:19 -0700

    [79. Word Search][Accepted]committed by Krunning

--- 79-Word-Search/solution.java
+++ 79-Word-Search/solution.java
@@ -1,35 +1,30 @@
 public class Solution {
     public boolean exist(char[][] board, String word) {
-        if(board == null || board.length == 0 || word == null || word.length() == 0){
-            return false;
-        }
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[0].length; j++){
-                if(board[i][j] == word.charAt(0) && DFS(board, word, i, j, new boolean[board.length][board[0].length], 0)){
+        if(word == null || word.length() == 0) return false;
+        int m = board.length;
+        int n = board[0].length;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(board[i][j] == word.charAt(0) && DFS(board, word, new boolean[m][n], i, j, 0)){
                     return true;
                 }
             }
         }
         return false;
     }
-    private boolean DFS(char[][] board, String word, int x, int y, boolean[][] visited, int cur){
-        if(x < 0 || y < 0 || x >= board.length || y >= board[0].length || visited[x][y]){
+    
+    private boolean DFS(char[][] board, String word, boolean[][] visited, int x, int y, int p){
+        if(p == word.length()) return true;
+        if(x < 0 || y < 0 || x >= board.length || y >= board[0].length || visited[x][y] || word.charAt(p) != board[x][y]){
             return false;
         }
-        if(board[x][y] != word.charAt(cur)){
-            return false;
-        }
-        if(cur + 1 == word.length()) return true;
         visited[x][y] = true;
-        int[] dx = {0, 0, -1, 1};
-        int[] dy = {1, -1, 0, 0};
-        for(int i = 0; i < 4; i++){
-            if( DFS(board, word, x + dx[i], y + dy[i], visited, cur + 1)){
-                visited[x][y] = false;
-                return true;
-            }
-        }
+        boolean res = 
+           DFS(board, word, visited, x + 1, y, p + 1)
+        || DFS(board, word, visited, x - 1, y, p + 1)
+        || DFS(board, word, visited, x, y + 1, p + 1)
+        || DFS(board, word, visited, x, y - 1, p + 1);
         visited[x][y] = false;
-        return false;
+        return res;
     }
 }
