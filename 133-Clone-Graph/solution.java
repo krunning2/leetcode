9b14fa44a67a97bb859322e9e48f60c22e932dee
krunning2 <zhidongke@yahoo.com>
2016-08-24 19:52:23 -0700

    [133. Clone Graph][Accepted]committed by Krunning

--- 133-Clone-Graph/solution.java
+++ 133-Clone-Graph/solution.java
@@ -8,27 +8,29 @@
  */
 public class Solution {
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
-        if(node == null) return node;
-        UndirectedGraphNode root = new UndirectedGraphNode(node.label);
-        HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<UndirectedGraphNode, UndirectedGraphNode>();
-        map.put(node, root);
-        Queue<UndirectedGraphNode> queue = new LinkedList<UndirectedGraphNode> ();
-        // Add the nodes
+        if(node == null) return null;
+        HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
+        Queue<UndirectedGraphNode> queue = new LinkedList<>();
         queue.offer(node);
+        UndirectedGraphNode newNode = new UndirectedGraphNode(node.label);
+        map.put(node, newNode);
         while(!queue.isEmpty()){
-            for(UndirectedGraphNode cur : queue.poll().neighbors){
-                if(!map.containsKey(cur)){
-                    map.put(cur, new UndirectedGraphNode(cur.label));
-                    queue.offer(cur);
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                UndirectedGraphNode cur = queue.poll();
+                for(UndirectedGraphNode nei : cur.neighbors){
+                    if(!map.containsKey(nei)){
+                        map.put(nei, new UndirectedGraphNode(nei.label));
+                        queue.offer(nei);
+                    }
                 }
             }
         }
-        // Build the edges
-        for(UndirectedGraphNode key : map.keySet()){
-            for(UndirectedGraphNode n : key.neighbors){
-                map.get(key).neighbors.add(map.get(n));
+        for(Map.Entry<UndirectedGraphNode, UndirectedGraphNode> entry : map.entrySet()){
+            for(UndirectedGraphNode nei : entry.getKey().neighbors){
+                entry.getValue().neighbors.add(map.get(nei));
             }
         }
-        return map.get(node);
+        return newNode;
     }
 }
