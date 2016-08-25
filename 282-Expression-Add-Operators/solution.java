71bfc9583e76feb7f7bb8bb7bafcde2dc52c32a4
krunning2 <zhidongke@yahoo.com>
2016-08-24 22:08:53 -0700

    [282. Expression Add Operators][Accepted]committed by Krunning

--- 282-Expression-Add-Operators/solution.java
+++ 282-Expression-Add-Operators/solution.java
@@ -1,27 +1,26 @@
 public class Solution {
     public List<String> addOperators(String num, int target) {
         List<String> res = new ArrayList<String>();
-        if(num == null || num.length() == 0) return res;
-        helper(num, target, res, "", 0, 0, 0);
+        helper(num, target, 0, "", 0, 0, res);
         return res;
     }
-    private void helper(String num, int target, List<String> res, String path, int pos, long eval, long multi){
+    private void helper(String num, int target, int pos, String s, long eval, long last, List<String> res){
         if(pos == num.length()){
             if(target == eval){
-                res.add(path);
+                res.add(s);
             }
             return;
         }
         for(int i = pos; i < num.length(); i++){
-            //if it starts with "0", this digit should only be 0, not 05,0005.....
-            if(i != pos && num.charAt(pos) == '0') break;
-            long cur = Long.parseLong(num.substring(pos, i + 1));
+            String sub = num.substring(pos, i + 1);
+            if(i != pos && sub.startsWith("0")) break;
+            long val = Long.valueOf(sub);
             if(pos == 0){
-                helper(num, target, res, path + cur, i + 1, cur, cur);
+                helper(num, target, i + 1, s + val, val, val, res);
             }else{
-                helper(num, target, res, path + "+" +cur, i + 1, eval + cur, cur);
-                helper(num, target, res, path + "-" +cur, i + 1, eval - cur, -cur);
-                helper(num, target, res, path + "*" +cur, i + 1, eval - multi + cur * multi, cur * multi);
+                helper(num, target, i + 1, s + "+" + val, eval + val, val, res);
+                helper(num, target, i + 1, s + "-" + val, eval - val, -val, res);
+                helper(num, target, i + 1, s + "*" + val, eval - last + last * val, last * val, res);
             }
         }
     }
