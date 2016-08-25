44f4c68ec17d751a6ccce1056266cad4eb4cddee
krunning2 <zhidongke@yahoo.com>
2016-08-24 21:22:27 -0700

    [314. Binary Tree Vertical Order Traversal][Accepted]committed by Krunning

--- 314-Binary-Tree-Vertical-Order-Traversal/solution.java
+++ 314-Binary-Tree-Vertical-Order-Traversal/solution.java
@@ -11,41 +11,39 @@ public class Solution {
     public List<List<Integer>> verticalOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         if(root == null) return res;
-        Map<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        Queue<Pair> q = new LinkedList<Pair>();
-        Pair p = new Pair(0, root);
-        q.offer(p);
-        while(!q.isEmpty()){
-            int size = q.size();
+        int max = 0, min = 0;
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        Queue<NodeWrapper> queue = new LinkedList<>();
+        queue.offer(new NodeWrapper(root, 0));
+        while(! queue.isEmpty()){
+            int size = queue.size();
             for(int i = 0; i < size; i++){
-                Pair cur = q.poll();
-                map.putIfAbsent(cur.col, new ArrayList<Integer>());
-                map.get(cur.col).add(cur.node.val);
+                NodeWrapper cur = queue.poll();
+                max = Math.max(max, cur.v);
+                min = Math.min(min, cur.v);
+                if(!map.containsKey(cur.v)){
+                    map.put(cur.v, new ArrayList<>());
+                }
+                map.get(cur.v).add(cur.node.val);
                 if(cur.node.left != null){
-                    Pair tmp = new Pair(cur.col - 1, cur.node.left);
-                    q.offer(tmp);
+                    queue.offer(new NodeWrapper(cur.node.left, cur.v - 1));
                 }
                 if(cur.node.right != null){
-                    Pair tmp = new Pair(cur.col + 1, cur.node.right);
-                    q.offer(tmp);
+                    queue.offer(new NodeWrapper(cur.node.right, cur.v + 1));
                 }
-                max = Math.max(max, cur.col);
-                min = Math.min(min, cur.col);
             }
         }
-        for(int i = min; i<=max; i++){
+        for(int i = min; i <= max; i++){
             res.add(map.get(i));
         }
         return res;
     }
-    class Pair{
+    class NodeWrapper{
         TreeNode node;
-        int col;
-        Pair(int c, TreeNode n){
-            node = n;
-            col = c;
+        int v;
+        NodeWrapper(TreeNode node, int v){
+            this.v = v;
+            this.node = node;
         }
     }
 }
