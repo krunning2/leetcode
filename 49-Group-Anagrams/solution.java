5a5e080f81f29d9c20059ae05a325f5617b952c9
krunning2 <zhidongke@yahoo.com>
2016-08-24 22:32:42 -0700

    [49. Group Anagrams][Accepted]committed by Krunning

--- 49-Group-Anagrams/solution.java
+++ 49-Group-Anagrams/solution.java
@@ -1,18 +1,19 @@
 public class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         List<List<String>> res = new ArrayList<List<String>>();
-        if(strs == null || strs.length == 0) return res;
-        Map<String, List<String>> map = new HashMap();
+        if(strs == null || strs.length == 0){
+            return res;
+        }
+        Map<String, List<String>> map = new HashMap<>();
         for(String s : strs){
             char[] tmp = s.toCharArray();
             Arrays.sort(tmp);
-            String key = String.valueOf(tmp);
-            map.putIfAbsent(key, new ArrayList());
+            String key = new String(tmp);
+            if(!map.containsKey(key)){
+                map.put(key, new ArrayList<>());
+            }
             map.get(key).add(s);
         }
-        for(List<String> list : map.values()){
-            Collections.sort(list);
-        }
-        return new ArrayList<List<String>>(map.values());
+        return new ArrayList<>(map.values());
     }
 }
