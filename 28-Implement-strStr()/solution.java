3e48e148ea80d74b07d9c5eeda4e34308124102b
krunning2 <zhidongke@yahoo.com>
2016-08-25 09:04:43 -0700

    [28. Implement strStr()][Accepted]committed by Krunning

--- 28-Implement-strStr()/solution.java
+++ 28-Implement-strStr()/solution.java
@@ -1,21 +1,35 @@
 public class Solution {
-    public int strStr(String h, String n) {
-        if(n == null || n.length() == 0){
-            return 0;
-        }
-        if(h == null){
-            return -1;
-        }
-        for(int i = 0; i <= h.length() - n.length(); i++){
-            for(int j = 0; j < n.length(); j++){
-                if(h.charAt(i + j) != n.charAt(j)){
-                    break;
+    public int strStr(String s, String p) {
+        if(s == null && p != null || s.length() == 0 && p.length() != 0) return -1;
+        if(p == null || p.length() == 0) return 0;
+        int[] next = getNext(p);
+        int i = 0, j = 0;
+        while(i < s.length()){
+            if(j== -1 || p.charAt(j) == s.charAt(i)){
+                j++;
+                i++;
+                if(j == p.length()){
+                    return i - j;
                 }
-                if(j == n.length() - 1){
-                    return i;
-                }
+            }else{
+                j = next[j];
             }
         }
         return -1;
+    }
+    private int[] getNext(String p){
+        int[] next = new int[p.length() + 1];
+        int j = 0, k = -1;
+        next[0] = -1;
+        while(j < p.length()){
+            if(k == -1 || p.charAt(j) == p.charAt(k)){
+                k++;
+                j++;
+                next[j] = k;
+            }else{
+                k = next[k];
+            }
+        }
+        return next;
     }
 }
