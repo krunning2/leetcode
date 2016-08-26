1bc845f639ad886b55a6fbc67a20e5192304b31e
krunning2 <zhidongke@yahoo.com>
2016-08-25 23:47:41 -0700

    [128. Longest Consecutive Sequence][Accepted]committed by Krunning

--- 128-Longest-Consecutive-Sequence/solution.java
+++ 128-Longest-Consecutive-Sequence/solution.java
@@ -1,29 +1,27 @@
 public class Solution {
     public int longestConsecutive(int[] nums) {
-        Map<Integer, Boolean> map = new HashMap<Integer, Boolean>();
+        HashMap<Integer, Boolean> map = new HashMap<>();
         for(int n : nums){
             map.put(n, false);
         }
-        int res = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(map.get(nums[i])){
-                continue;
-            }
+        int max = 1;
+        for(int i = 0; i < nums.length; i ++){
+            if(map.get(nums[i])) continue;
             int cur = nums[i];
             int count = 1;
-            while(map.containsKey(cur + 1)){
-                count++;
+            while(map.containsKey(cur + 1) && !map.get(cur + 1)){
                 map.put(cur + 1, true);
-                cur++;
+                cur ++;
+                count++;
             }
             cur = nums[i];
-            while(map.containsKey(cur - 1)){
-                count++;
+            while(map.containsKey(cur - 1) && !map.get(cur - 1)){
                 map.put(cur - 1, true);
                 cur--;
+                count++;
             }
-            res = Math.max(res, count);
+            max = Math.max(count, max);
         }
-        return res;
+        return max;
     }
 }
