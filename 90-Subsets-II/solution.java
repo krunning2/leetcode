5d29f82f532391374a328195ae1e6588e2f77a78
krunning2 <zhidongke@yahoo.com>
2016-08-25 23:53:36 -0700

    [90. Subsets II][Accepted]committed by Krunning

--- 90-Subsets-II/solution.java
+++ 90-Subsets-II/solution.java
@@ -1,18 +1,17 @@
 public class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(nums == null || nums.length == 0) return res;
         Arrays.sort(nums);
-        helper(res, nums, 0, new ArrayList());
+        DFS(nums, 0, new ArrayList<>(), res, new boolean[nums.length]);
         return res;
     }
-    private void helper(List<List<Integer>> res, int[] nums, int pos, List<Integer> cur){
-        res.add(new ArrayList<Integer>(cur));
+    private void DFS(int[] nums, int pos, List<Integer> cur, List<List<Integer>> res, boolean[] visited){
+        res.add(new ArrayList<>(cur));
         for(int i = pos; i < nums.length; i++){
-            if(i != pos && nums[i] == nums[i - 1]){
-                continue;
-            }
+            if(i != pos && !visited[i - 1] && nums[i] == nums[i - 1]) continue;
             cur.add(nums[i]);
-            helper(res, nums, i + 1, cur);
+            DFS(nums, i + 1, cur, res, visited);
             cur.remove(cur.size() - 1);
         }
     }
