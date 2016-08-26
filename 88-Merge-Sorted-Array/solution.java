8f69a378976768ffba9f9ee5904d027be17a24a4
krunning2 <zhidongke@yahoo.com>
2016-08-26 09:13:59 -0700

    [88. Merge Sorted Array][Accepted]committed by Krunning

--- 88-Merge-Sorted-Array/solution.java
+++ 88-Merge-Sorted-Array/solution.java
@@ -1,19 +1,18 @@
 public class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p = m + n - 1;
         m--;
         n--;
-        while(n >= 0 && m >= 0){
+        while(m >= 0 && n >= 0){
             if(nums1[m] >= nums2[n]){
-                nums1[m + n + 1] = nums1[m];
-                m--;
+                nums1[p--] = nums1[m--];
             }else{
-                nums1[m + n + 1] = nums2[n];
-                n--;
+                nums1[p--] = nums2[n--];
             }
         }
+        if(m > 0) return;
         while(n >= 0){
-            nums1[n] = nums2[n];
-            n--;
+            nums1[p--] = nums2[n--];
         }
     }
 }
