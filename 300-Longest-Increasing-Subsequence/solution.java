4904a1c65f30959a5eac26746cfe8dc6040c9dce
krunning2 <zhidongke@yahoo.com>
2016-08-27 10:54:01 -0700

    [300. Longest Increasing Subsequence][Accepted]committed by Krunning

--- 300-Longest-Increasing-Subsequence/solution.java
+++ 300-Longest-Increasing-Subsequence/solution.java
@@ -1,15 +1,13 @@
 public class Solution {
     public int lengthOfLIS(int[] nums) {
-        if(nums == null || nums.length == 0){
-            return 0;
-        }
+        if(nums == null || nums.length == 0) return 0;
         int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
         int max = 1;
         for(int i = 0; i < nums.length; i++){
-            dp[i] = 1;
             for(int j = 0; j < i; j++){
                 if(nums[i] > nums[j]){
-                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                     max = Math.max(max, dp[i]);
                 }
             }
