d3a0f2e03b0d4792024bfa570564dee450db4d0f
krunning2 <zhidongke@yahoo.com>
2016-08-27 13:24:47 -0700

    [207. Course Schedule][Accepted]committed by Krunning

--- 207-Course-Schedule/solution.java
+++ 207-Course-Schedule/solution.java
@@ -1,30 +1,35 @@
 public class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        Map<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();
-        int indegree[] = new int[numCourses];
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int[] indegree = new int[numCourses];
         for(int i = 0; i < numCourses; i++){
             map.put(i, new ArrayList<Integer>());
         }
         for(int i = 0; i < prerequisites.length; i++){
-            map.get(prerequisites[i][1]).add(prerequisites[i][0]);
             indegree[prerequisites[i][0]]++;
+            map.get(prerequisites[i][1]).add(prerequisites[i][0]);
         }
-        Queue<Integer> q = new LinkedList<Integer>();
-        for(int i = 0; i < indegree.length; i++){
+        Queue<Integer> queue = new LinkedList<>();
+        int count = 0;
+        for(int i = 0; i < numCourses; i++){
             if(indegree[i] == 0){
-                q.offer(i);
+                queue.offer(i);
+                count++;
             }
         }
-        int count = numCourses;
-        while(!q.isEmpty()){
-            int cur = q.poll();
-            for(int i : map.get(cur)){
-                if(--indegree[i] == 0){
-                    q.offer(i);
+        while(! queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                int cur = queue.poll();
+                for(int d : map.get(cur)){
+                    indegree[d]--;
+                    if(indegree[d] == 0){
+                        queue.offer(d);
+                        count++;
+                    }
                 }
             }
-            count--;
         }
-        return count == 0;
+        return count == numCourses;
     }
 }
