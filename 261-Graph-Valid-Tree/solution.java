9bdcb63b556993cdff0e74cceae5345a46109a65
krunning2 <zhidongke@yahoo.com>
2016-08-27 15:21:45 -0700

    [261. Graph Valid Tree][Accepted]committed by Krunning

--- 261-Graph-Valid-Tree/solution.java
+++ 261-Graph-Valid-Tree/solution.java
@@ -1,51 +1,46 @@
 public class Solution {
     public boolean validTree(int n, int[][] edges) {
-        if(n <= 1){
-            return true;
-        }
         if(n - 1 != edges.length){
             return false;
         }
         UnionFind uf = new UnionFind(n);
         for(int i = 0; i < edges.length; i++){
-            int n0 = edges[i][0];
-            int n1 = edges[i][1];
-            if(uf.find(n0) == uf.find(n1)){
+            int fa1 = uf.find(edges[i][0]);
+            int fa2 = uf.find(edges[i][1]);
+            if(fa1 == fa2){
                 return false;
+            }else{
+                uf.union(fa1, fa2);
             }
-            uf.union(n0, n1);
         }
         return true;
     }
+    
     class UnionFind{
-        Map<Integer, Integer> father;
+        HashMap<Integer, Integer> father;
         UnionFind(int n){
-            father = new HashMap<Integer, Integer>();
-            for(int j = 0; j < n; j++){
-                father.put(j, j);
-            }
+            father = new HashMap<>();
+            for(int i = 0; i < n; i++)
+                father.put(i, i);
         }
         
-        public int find(int key){
-            int f = father.get(key);
-            while(f != father.get(f)){
-                f = father.get(f);
+        int find(int x){
+            int fa = father.get(x);
+            while(fa != father.get(fa)){
+                fa = father.get(fa);
             }
-            int c = father.get(key);
-            int tmp = c;
-            while(c != father.get(c)){
-                tmp = father.get(c);
-                father.put(c, f);
-                c = tmp;
+            int node = x;
+            while(node != father.get(node)){
+                father.put(node, fa);
+                node = father.get(node);
             }
-            return f;
+            return fa;
         }
-        
-        public void union(int key1, int key2){
-            int f1 = find(key1);
-            int f2 = find(key2);
-            if(f1 != f2)
-                father.put(f1, f2);
+        void union(int x, int y){
+            int fa1 = find(x);
+            int fa2 = find(y);
+            father.put(fa1, fa2);
         }
     }
+    
 }
