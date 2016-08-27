b2f8c67c4bb7afafe8d7460c0367b860a73853b2
krunning2 <zhidongke@yahoo.com>
2016-08-27 10:34:40 -0700

    [171. Excel Sheet Column Number][Accepted]committed by Krunning

--- 171-Excel-Sheet-Column-Number/solution.java
+++ 171-Excel-Sheet-Column-Number/solution.java
@@ -1,12 +1,8 @@
 public class Solution {
     public int titleToNumber(String s) {
         int res = 0;
-        if(s == null || s.length() == 0) return 0;
-        int p = 0;
-        while(p < s.length()){
-            int cur = s.charAt(p) - 'A' + 1;
-            res = res * 26 + cur;
-            p++;
+        for(char c : s.toCharArray()){
+            res = res * 26 + c - 'A' + 1;
         }
         return res;
     }
