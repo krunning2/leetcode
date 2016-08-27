12678bf530d5b6a72c949319a1cb9af8b6578b1f
krunning2 <zhidongke@yahoo.com>
2016-08-26 22:49:08 -0700

    [274. H-Index][Accepted]committed by Krunning

--- 274-H-Index/solution.java
+++ 274-H-Index/solution.java
@@ -1,21 +1,21 @@
 public class Solution {
     public int hIndex(int[] citations) {
-        if(citations == null){
-            return 0;
-        }
+        if(citations == null) return 0;
         int len = citations.length;
-        int[] arr = new int[len + 1];
+        int[] bucket = new int[len + 1];
         for(int i = 0; i < len; i++){
             if(citations[i] >= len){
-                arr[len] ++; 
+                bucket[len] ++;
             }else{
-                arr[citations[i]] ++;
+                bucket[citations[i]]++;
             }
         }
         int res = 0;
         for(int i = len; i >= 0; i--){
-            res += arr[i];
-            if(res >= i) return i;
+            res += bucket[i];
+            if(res >= i){
+                return i;
+            }
         }
         return 0;
     }
