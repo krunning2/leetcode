dc96e629b3a13c8e161eae1bb0ff887f9f9d8b5f
krunning2 <zhidongke@yahoo.com>
2016-08-27 16:41:23 -0700

    [265. Paint House II][Accepted]committed by Krunning

--- 265-Paint-House-II/solution.java
+++ 265-Paint-House-II/solution.java
@@ -1,33 +1,34 @@
 public class Solution {
     public int minCostII(int[][] costs) {
         if(costs == null || costs.length == 0) return 0;
-        int m = costs.length;
-        int n = costs[0].length;
-        int[][] dp = new int[2][n];
-        for(int i = 0; i < n; i++){
-            dp[0][i] = costs[0][i];
+        int n = costs.length;
+        int k = costs[0].length;
+        int[][] minCosts = new int[2][k];
+        int res = Integer.MAX_VALUE;
+        
+        for(int i = 0; i < k; i++){
+            minCosts[0][i] = costs[0][i];
         }
-        for(int i = 1; i < m; i ++){
-            int min2 = Integer.MAX_VALUE;
+        
+        for(int i = 1; i < costs.length; i++){
             int min1 = Integer.MAX_VALUE;
-            for(int j = 0; j < n; j ++){
-                int cur = dp[(i - 1) % 2][j];
-                if(cur < min1){
+            int min2 = Integer.MAX_VALUE;
+            for(int j = 0; j < k; j++){
+                int cur = minCosts[(i - 1) % 2][j];
+                if(min1 > cur){
                     min2 = min1;
                     min1 = cur;
-                }else if(cur < min2){
+                }else if(min2 > cur){
                     min2 = cur;
                 }
             }
-            for(int j = 0; j < n; j ++){
-                dp[i % 2][j] = dp[(i-1) % 2][j] == min1 ? min2 + costs[i][j] : min1 + costs[i][j];
+            for(int j = 0; j < k; j++){
+                minCosts[i % 2][j] = min1 == minCosts[(i - 1) % 2][j] ? min2 + costs[i][j] : min1 + costs[i][j];
             }
         }
-        int res = Integer.MAX_VALUE;
-        for(int i = 0; i < n; i++){
-            res = Math.min(res, dp[(m - 1) % 2][i]);
+        for(int i = 0; i < k; i++){
+            res = Math.min(res, minCosts[(n - 1) % 2][i]);
         }
         return res;
     }
 }
-
