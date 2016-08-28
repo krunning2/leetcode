cde98d6951a32313cacd5131668a8a4fa0e8afe8
krunning2 <zhidongke@yahoo.com>
2016-08-28 00:03:16 -0700

    [228. Summary Ranges][Accepted]committed by Krunning

--- 228-Summary-Ranges/solution.java
+++ 228-Summary-Ranges/solution.java
@@ -1,15 +1,15 @@
 public class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> res = new ArrayList<String>();
-        if(nums == null || nums.length == 0) return res;
+        if(nums == null || nums.length == 0){
+            return res;
+        }
         int next = nums[0];
         int pre = nums[0];
         for(int i = 0; i < nums.length; i++){
-            if(next == nums[i]){
+            if(nums[i]  == next){
                 next++;
-                continue;
-            }
-            if(next < nums[i]){
+            }else{
                 res.add(print(pre, next - 1));
                 pre = nums[i];
                 next = nums[i] + 1;
@@ -19,6 +19,6 @@ public class Solution {
         return res;
     }
     private String print(int a, int b){
-        return a == b ? String.valueOf(a) : a + "->" + b;
+        return a == b ? String.valueOf(a) : a + "->" + b;   
     }
 }
