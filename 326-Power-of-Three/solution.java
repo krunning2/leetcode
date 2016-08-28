813759d2e5b0c08cb0ae491c69ba3251f961fdb8
krunning2 <zhidongke@yahoo.com>
2016-08-27 22:11:28 -0700

    [326. Power of Three][Accepted]committed by Krunning

--- 326-Power-of-Three/solution.java
+++ 326-Power-of-Three/solution.java
@@ -1,10 +1,10 @@
 public class Solution {
     public boolean isPowerOfThree(int n) {
-        if(n>1){
-            while(n%3==0) {
+        if(n > 1){
+            while(n % 3 == 0){
                 n /= 3;
             }
         }
-        return n==1;
+        return n == 1;
     }
 }
