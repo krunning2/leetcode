4e2cdac7bbf8565fce432e385f76dfbdd2fb8335
krunning2 <zhidongke@yahoo.com>
2016-08-28 23:28:24 -0700

    [350. Intersection of Two Arrays II][Accepted]committed by Krunning

--- 350-Intersection-of-Two-Arrays-II/solution.java
+++ 350-Intersection-of-Two-Arrays-II/solution.java
@@ -1,26 +1,27 @@
 public class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        int p1 = 0;
-        int p2 = 0;
-        List<Integer> tmp = new ArrayList<>();
-        while(p1 < nums1.length && p2 < nums2.length){
-            if(nums1[p1] == nums2[p2]){
-                tmp.add(nums1[p1]);
-                p2++;
-                p1++;
-            }else if(nums1[p1] > nums2[p2]){
-                p2++;
-            }else{
-                p1++;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int i = 0; i < nums1.length; ++i) {
+            if (map.containsKey(nums1[i]))
+                map.put(nums1[i], map.get(nums1[i]) + 1); 
+            else
+                map.put(nums1[i], 1);
+        }
+
+        List<Integer> results = new ArrayList<Integer>();
+
+        for (int i = 0; i < nums2.length; ++i){
+            if (map.containsKey(nums2[i]) &&
+                map.get(nums2[i]) > 0) {
+                results.add(nums2[i]);
+                map.put(nums2[i], map.get(nums2[i]) - 1); 
             }
         }
-        int[] result = new int[tmp.size()];
-        int i = 0;
-        for(int n : tmp){
-            result[i++] = n;
-        }
+
+        int result[] = new int[results.size()];
+        for(int i = 0; i < results.size(); ++i)
+            result[i] = results.get(i);
+
         return result;
     }
 }
