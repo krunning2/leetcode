17c14f88be21f41e7c71fecababb29bbec9390ae
krunning2 <zhidongke@yahoo.com>
2016-08-28 21:26:13 -0700

    [81. Search in Rotated Sorted Array II][Accepted]committed by Krunning

--- 81-Search-in-Rotated-Sorted-Array-II/solution.java
+++ 81-Search-in-Rotated-Sorted-Array-II/solution.java
@@ -1,6 +1,6 @@
 public class Solution {
     public boolean search(int[] nums, int target) {
-        if(nums == null || nums.length == 0){
+        if(nums == null){
             return false;
         }
         int start = 0;
@@ -9,24 +9,22 @@ public class Solution {
             int mid = start - (start - end) / 2;
             if(nums[mid] == target){
                 return true;
-            }else{
-                if(nums[mid] > nums[start]){
-                    if(target >= nums[start] && target <= nums[mid]){
-                        end = mid;
-                    }else{
-                        start = mid;
-                    }
-                }else if(nums[mid] < nums[start]){
-                    if(target >= nums[mid] && target <= nums[end]){
-                        start = mid;
-                    }else{
-                        end = mid;
-                    }
+            }else if(nums[mid] > nums[start]){
+                if(target >= nums[start] && target <= nums[mid]){
+                    end = mid;
                 }else{
-                    start++;
+                    start = mid;
                 }
+            }else if(nums[mid] < nums[start]){
+                if(target >= nums[mid] && target <= nums[end]){
+                    start = mid;
+                }else{
+                    end = mid;
+                }
+            }else{
+                start ++;
             }
         }
-        return (nums[start] == target) || (nums[end] == target);
+        return nums[start] == target || nums[end] == target;
     }
 }
