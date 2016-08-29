ac1e5eaf0823e3697cf46263bea1b266c292f525
krunning2 <zhidongke@yahoo.com>
2016-08-28 21:39:46 -0700

    [189. Rotate Array][Accepted]committed by Krunning

--- 189-Rotate-Array/solution.java
+++ 189-Rotate-Array/solution.java
@@ -1,18 +1,18 @@
 public class Solution {
     public void rotate(int[] nums, int k) {
+        if(nums == null || k <= 0) return;
         k = k % nums.length;
-        int p = 0;
-        swap(nums, 0, nums.length - k - 1);
-        swap(nums, nums.length - k, nums.length - 1);
-        swap(nums, 0, nums.length - 1);
+        reverse(nums, 0, nums.length - k - 1);
+        reverse(nums, nums.length - k, nums.length - 1);
+        reverse(nums, 0, nums.length - 1);
     }
-    private void swap(int[] nums, int s, int e){
-        while(s <= e){
-            int tmp = nums[s];
-            nums[s] = nums[e];
-            nums[e] = tmp;
-            s++;
-            e--;
+    private void reverse(int[] nums, int start, int end){
+        while(start <= end){
+            int tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+            start ++;
+            end --;
         }
     }
 }
