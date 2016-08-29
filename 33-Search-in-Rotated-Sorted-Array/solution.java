67c16351149ff20fa792c70d4a39ada8f7c68e79
krunning2 <zhidongke@yahoo.com>
2016-08-28 21:19:05 -0700

    [33. Search in Rotated Sorted Array][Accepted]committed by Krunning

--- 33-Search-in-Rotated-Sorted-Array/solution.java
+++ 33-Search-in-Rotated-Sorted-Array/solution.java
@@ -7,7 +7,7 @@ public class Solution {
             if(nums[mid] == target){
                 return mid;
             }
-            if(nums[mid] > nums[start]){
+            if(nums[mid] >= nums[start]){
                 if(target >= nums[start] && target <= nums[mid]){
                     end = mid;
                 }else{
