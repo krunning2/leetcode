490e02b35297b1fbfac76698ef96c23aa42ac939
krunning2 <zhidongke@yahoo.com>
2016-08-28 21:18:46 -0700

    [33. Search in Rotated Sorted Array][Accepted]committed by Krunning

--- 33-Search-in-Rotated-Sorted-Array/solution.java
+++ 33-Search-in-Rotated-Sorted-Array/solution.java
@@ -1,27 +1,23 @@
 public class Solution {
     public int search(int[] nums, int target) {
-        if(nums == null || nums.length == 0){
-            return -1;
-        }
         int start = 0;
         int end = nums.length - 1;
         while(start + 1 < end){
             int mid = start - (start - end) / 2;
             if(nums[mid] == target){
                 return mid;
-            }else{
-                if(nums[mid] > nums[start]){
-                    if(target >= nums[start] && target <= nums[mid]){
-                        end = mid;
-                    }else{
-                        start = mid;
-                    }
+            }
+            if(nums[mid] > nums[start]){
+                if(target >= nums[start] && target <= nums[mid]){
+                    end = mid;
                 }else{
-                    if(target >= nums[mid] && target <= nums[end]){
-                        start = mid;
-                    }else{
-                        end = mid;
-                    }
+                    start = mid;
+                }
+            }else{
+                if(target >= nums[mid] && target <= nums[end]){
+                    start = mid;
+                }else{
+                    end = mid;
                 }
             }
         }
