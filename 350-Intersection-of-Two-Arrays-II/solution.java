71125a87d2835d337d2b3fe634207a1278e3426f
krunning2 <zhidongke@yahoo.com>
2016-08-28 23:25:53 -0700

    [350. Intersection of Two Arrays II][Accepted]committed by Krunning

--- 350-Intersection-of-Two-Arrays-II/solution.java
+++ 350-Intersection-of-Two-Arrays-II/solution.java
@@ -10,12 +10,13 @@ public class Solution {
 
         List<Integer> results = new ArrayList<Integer>();
 
-        for (int i = 0; i < nums2.length; ++i)
+        for (int i = 0; i < nums2.length; ++i){
             if (map.containsKey(nums2[i]) &&
                 map.get(nums2[i]) > 0) {
                 results.add(nums2[i]);
                 map.put(nums2[i], map.get(nums2[i]) - 1); 
             }
+        }
 
         int result[] = new int[results.size()];
         for(int i = 0; i < results.size(); ++i)
