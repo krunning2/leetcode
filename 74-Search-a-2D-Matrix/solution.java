9a4aa397966282fd27b709a416772012f0846c0e
krunning2 <zhidongke@yahoo.com>
2016-08-28 22:58:38 -0700

    [74. Search a 2D Matrix][Accepted]committed by Krunning

--- 74-Search-a-2D-Matrix/solution.java
+++ 74-Search-a-2D-Matrix/solution.java
@@ -1,31 +1,24 @@
 public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        if(matrix == null || matrix.length == 0 || matrix[0][0] > target) return false;
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
+            return false;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
         int start = 0;
-        int end = matrix.length - 1;
+        int end = m * n - 1;
         while(start + 1 < end){
             int mid = start - (start - end) / 2;
-            if(matrix[mid][0] == target){
+            int row = mid / n;
+            int col = mid % n;
+            if(matrix[row][col] == target){
                 return true;
-            } else if(matrix[mid][0] > target){
+            }else if(matrix[row][col] > target){
                 end = mid;
             }else{
                 start = mid;
             }
         }
-        int row = matrix[end][0] <= target ? end : start;
-        start = 0;
-        end = matrix[0].length - 1;
-        while(start + 1 < end){
-            int mid = start - (start - end) / 2;
-            if(matrix[row][mid] == target){
-                return true;
-            } else if(matrix[row][mid] > target){
-                end = mid;
-            }else{
-                start = mid;
-            }
-        }
-        return matrix[row][start] == target || matrix[row][end] == target;
+        return matrix[start / n][start % n] == target || matrix[end / n][end % n] == target;
     }
 }
