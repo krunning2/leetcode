e5571f3cdcb8706ddd311dcf34d786532db8f6ea
krunning2 <zhidongke@yahoo.com>
2016-08-30 08:16:32 -0700

    [345. Reverse Vowels of a String][Accepted]committed by Krunning

--- 345-Reverse-Vowels-of-a-String/solution.java
+++ 345-Reverse-Vowels-of-a-String/solution.java
@@ -1,28 +1,28 @@
 public class Solution {
     public String reverseVowels(String s) {
-        if(s == null || s.length() <= 1){
-            return s;
-        }
+        if(s == null) return s;
+        int start = 0;
+        int end = s.length() - 1;
         char[] chars = s.toCharArray();
-        int start = 0, end = chars.length - 1;
-        while(start <= end){
-            if(checkVowel (chars[start]) && checkVowel(chars[end]) ){
+        while(start < end){
+            if(isVowel(chars[end]) && isVowel(chars[start])){
                 char tmp = chars[start];
                 chars[start] = chars[end];
                 chars[end] = tmp;
                 start++;
-                end --;
-            }else if(!checkVowel(chars[start])){
+                end--;
+            }else if(!isVowel(chars[end])){
+                end--;
+            }else if(!isVowel(chars[start])){
                 start++;
-            }else if(!checkVowel(chars[end])){
-                end --;
             }
         }
         return new String(chars);
     }
-    private boolean checkVowel(char c){
-        return c == 'a' || c == 'e' || c == 'i' ||
-        c == 'o' || c == 'u' || c == 'A' || c == 'E' ||
-        c == 'I' || c == 'O' || c == 'U';
+    private boolean isVowel(char c){
+        if(c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' || c == 'I' || c == 'o' || c == 'O' || c == 'u' || c == 'U'){
+            return true;
+        }
+        return false;
     }
 }
