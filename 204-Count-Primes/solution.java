d82ae37c771d25094916acd23dab5dceb070cab5
krunning2 <zhidongke@yahoo.com>
2016-08-31 10:03:25 -0700

    [204. Count Primes][Accepted]committed by Krunning

--- 204-Count-Primes/solution.java
+++ 204-Count-Primes/solution.java
@@ -1,19 +1,17 @@
 public class Solution {
     public int countPrimes(int n) {
-        boolean isPrime[] = new boolean[n + 1];
+        boolean isPrime[] = new boolean[n];
         Arrays.fill(isPrime, true);
-        for(int i = 2; i * i< n; i++){
+        for(int i = 2; i * i < n; i++){
             if(isPrime[i]){
-                for(int j = i * i; j < n; j = j + i){
+                for(int j = i * i; j < n; j = j +i){
                     isPrime[j] = false;
                 }
             }
         }
         int count = 0;
         for(int i = 2; i < n; i++){
-            if(isPrime[i]){
-                count++;
-            }
+            if(isPrime[i]) count++;
         }
         return count;
     }
