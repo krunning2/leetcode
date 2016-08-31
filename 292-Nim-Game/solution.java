c620f9963e2c8f5f8875bdad6701d5e592a5c5f2
krunning2 <zhidongke@yahoo.com>
2016-08-30 20:47:05 -0700

    [292. Nim Game][Accepted]committed by Krunning

--- 292-Nim-Game/solution.java
+++ 292-Nim-Game/solution.java
@@ -1,5 +1,16 @@
 public class Solution {
     public boolean canWinNim(int n) {
-        return n % 4 != 0;
+        if(n <= 3) return true;
+        if(n >= 100){
+            return n % 4 != 0;
+        }
+        boolean[] dp = new boolean[3];
+        dp[0] = true;
+        dp[1] = true;
+        dp[2] = true;
+        for(int i = 3; i < n; i++){
+            dp[i % 3] = !dp[(i - 1) % 3] || !dp[(i - 2) % 3] || !dp[(i - 3) % 3];
+        }
+        return dp[(n - 1) % 3];
     }
 }
