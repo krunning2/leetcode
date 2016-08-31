2780222333a1f82320319adeecde69aed32951ec
krunning2 <zhidongke@yahoo.com>
2016-08-30 20:04:47 -0700

    [293. Flip Game][Accepted]committed by Krunning

--- 293-Flip-Game/solution.java
+++ 293-Flip-Game/solution.java
@@ -1,21 +1,19 @@
 public class Solution {
     public List<String> generatePossibleNextMoves(String s) {
         List<String> res = new ArrayList<String>();
-        if(s == null || s.length() <= 1){
+        if(s == null || s.length() == 0){
             return res;
         }
         char[] chars = s.toCharArray();
         for(int i = 0; i < chars.length - 1; i++){
             if(chars[i] == '+' && chars[i + 1] == '+'){
-                res.add(getNext(chars, i));
-                chars[i] = chars[i+1] = '+';
+                chars[i] = '-';
+                chars[i + 1] = '-';
+                res.add(new String(chars));
+                chars[i] = '+';
+                chars[i + 1] = '+';
             }
         }
         return res;
-    }
-    private String getNext(char[] chars, int i){
-        chars[i] = '-';
-        chars[i + 1] = '-';
-        return new String(chars);
     }
 }
