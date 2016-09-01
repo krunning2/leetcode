0789ec34f1bdce460b13519ee09e0a316a3ca0cb
krunning2 <zhidongke@yahoo.com>
2016-08-31 22:16:48 -0700

    [279. Perfect Squares][Accepted]committed by Krunning

--- 279-Perfect-Squares/solution.java
+++ 279-Perfect-Squares/solution.java
@@ -5,9 +5,10 @@ public class Solution {
         for(int i = 1; i * i <= n; i++){
             dp[i * i] = 1;
         }
+        dp[1] = 1;
         for(int i = 1; i <= n; i++){
             for(int j = 1; j * j + i <= n; j++){
-                dp[i + j * j] = Math.min(dp[i + j * j], dp[i] + 1);
+                dp[i + j * j] = Math.min(dp[i] + 1, dp[i + j * j]);
             }
         }
         return dp[n];
