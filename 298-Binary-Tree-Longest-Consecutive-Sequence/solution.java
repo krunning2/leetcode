0274fe3b2f989af61e8f750799300773507a7ed3
krunning2 <zhidongke@yahoo.com>
2016-08-31 21:33:33 -0700

    [298. Binary Tree Longest Consecutive Sequence][Accepted]committed by Krunning

--- 298-Binary-Tree-Longest-Consecutive-Sequence/solution.java
+++ 298-Binary-Tree-Longest-Consecutive-Sequence/solution.java
@@ -12,13 +12,11 @@ public class Solution {
         if(root == null) return 0;
         return find(root, root.val - 1, 0);
     }
-    private int find(TreeNode root, int pre, int length){
-        if(root == null){
-            return length;
-        }
-        length = pre + 1 == root.val ? length + 1 : 1;
-        int lengthLeft = find(root.left, root.val, length);
-        int lengthRight = find(root.right, root.val, length);
-        return Math.max(lengthLeft, Math.max(length, lengthRight));
+    private int find(TreeNode root, int pre, int len){
+        if(root == null) return len;
+        len = pre + 1 == root.val ? len + 1 : 1;
+        int left = find(root.left, root.val, len);
+        int right = find(root.right, root.val, len);
+        return Math.max(len, Math.max(right, left));
     }
 }
