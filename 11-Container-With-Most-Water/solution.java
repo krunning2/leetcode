a27136b89e3e55ff4cccc72043307ce84e10eed5
krunning2 <zhidongke@yahoo.com>
2016-08-31 22:02:55 -0700

    [11. Container With Most Water][Accepted]committed by Krunning

--- 11-Container-With-Most-Water/solution.java
+++ 11-Container-With-Most-Water/solution.java
@@ -1,17 +1,15 @@
 public class Solution {
     public int maxArea(int[] height) {
-        if(height == null || height.length < 2) return 0;
-        int max = 0;
         int start = 0;
         int end = height.length - 1;
+        int max = 0;
         while(start < end){
-            int min = Math.min(height[start], height[end]);
-            int len = end - start;
-            max = Math.max(max, len * min);
-            if(height[start] > height[end]){
-                end --;
+            if(height[start] < height[end]){
+                max = Math.max(max, height[start] * (end - start));
+                start++;
             }else{
-                start ++;
+                max = Math.max(max, height[end] * (end - start));
+                end --;
             }
         }
         return max;
