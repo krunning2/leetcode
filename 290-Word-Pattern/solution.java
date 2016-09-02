f6cee087e4227d0b437243cd219859932346f72b
krunning2 <zhidongke@yahoo.com>
2016-09-01 23:29:15 -0700

    [290. Word Pattern][Accepted]committed by Krunning

--- 290-Word-Pattern/solution.java
+++ 290-Word-Pattern/solution.java
@@ -1,18 +1,25 @@
 public class Solution {
     public boolean wordPattern(String pattern, String str) {
-        String[] s = str.split(" ");
-        if(s.length != pattern.length()) return false;
+        if(pattern == null || str == null){
+            return false;
+        }
         Map<Character, String> map = new HashMap<>();
         Set<String> set = new HashSet<>();
+        String[] strs = str.split(" ");
+        if(strs.length != pattern.length()){
+            return false;
+        }
         for(int i = 0; i < pattern.length(); i++){
             char c = pattern.charAt(i);
-            if(map.containsKey(c) && !map.get(c).equals(s[i])){
+            if(map.containsKey(c) && !map.get(c).equals(strs[i])){
                 return false;
-            }else if(!map.containsKey(c) && set.contains(s[i])){
+            }
+            if(!map.containsKey(c) && set.contains(strs[i])){
                 return false;
-            }else if(!map.containsKey(c) && !set.contains(s[i])){
-                map.put(c, s[i]);
-                set.add(s[i]);
+            }
+            if(!map.containsKey(c) && !set.contains(strs[i])){
+                map.put(c, strs[i]);
+                set.add(strs[i]);
             }
         }
         return true;
