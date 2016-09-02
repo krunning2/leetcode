ef8333d33e0f7bdb4f2439541cf7bfed6b36e630
krunning2 <zhidongke@yahoo.com>
2016-09-01 23:45:34 -0700

    [291. Word Pattern II][Accepted]committed by Krunning

--- 291-Word-Pattern-II/solution.java
+++ 291-Word-Pattern-II/solution.java
@@ -3,36 +3,27 @@ public class Solution {
         if(pattern == null || pattern.length() == 0) return str == null || str.length() == 0;
         return helper(pattern, str, 0, 0, new HashMap<>(), new HashSet<>());
     }
-    private boolean helper(String pattern, String str, int pos1, int pos2, Map<Character, String> map, Set<String> set){
-        if(pos1 == pattern.length() && pos2 == str.length()){
+    private boolean helper(String pattern, String str, int p1, int s1, Map<Character, String> map, Set<String> set){
+        if(p1 == pattern.length() && s1 == str.length()){
             return true;
         }
-        if(pos1 >= pattern.length() || pos2 >= str.length()){
+        if(p1 >= pattern.length() || s1 >= str.length()){
             return false;
         }
-        
-        char cur = pattern.charAt(pos1);
-        for(int i = pos2; i < str.length(); i++){
-            String sub = str.substring(pos2, i + 1);
-            if(!map.containsKey(cur) && !set.contains(sub)){
-                map.put(cur, sub);
-                set.add(sub);
-                boolean res = helper(pattern, str, pos1 + 1, i + 1, map, set);
-                if(res) return true;
-                map.remove(cur);
-                set.remove(sub);
-            }else if(map.containsKey(cur) && map.get(cur).equals(sub)){
-                if(helper(pattern, str, pos1 + 1, i + 1, map, set)) return true;
+        for(int i = s1; i < str.length(); i++){
+            String cur = str.substring(s1, i + 1);
+            char c = pattern.charAt(p1);
+            if(!map.containsKey(c) && !set.contains(cur)){
+                map.put(c, cur);
+                set.add(cur);
+                boolean flag = helper(pattern, str, p1 + 1, i + 1, map, set);
+                if(flag) return true;
+                map.remove(c);
+                set.remove(cur);
+            }else if(map.containsKey(c) && map.get(c).equals(cur)){
+                if(helper(pattern, str, p1 + 1, i + 1, map, set)) return true;
             }
         }
         return false;
     }
-    // private boolean isValid(Map<Character, String> map, Set<String> set, char c, String s){
-    //     if(map.containsKey(c) && !map.get(c).equals(s)){
-    //         return false;
-    //     }else if(!map.containsKey(c) && set.contains(s)){
-    //         return false;
-    //     }
-    //     return true;
-    // }
 }
