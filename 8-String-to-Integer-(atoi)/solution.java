d1c691426037792fa195c92f2b0275c6875e10d4
krunning2 <zhidongke@yahoo.com>
2016-09-02 08:42:14 -0700

    [8. String to Integer (atoi)][Accepted]committed by Krunning

--- 8-String-to-Integer-(atoi)/solution.java
+++ 8-String-to-Integer-(atoi)/solution.java
@@ -1,39 +1,27 @@
 public class Solution {
     public int myAtoi(String str) {
         if(str == null || str.length() == 0){
-            //throw new Exception("invalid input");
             return 0;
         }
         str = str.trim();
-        int p = 0;
         int sign = 1;
-        if(str.charAt(0) == '+'){
-            sign = 1;
-            p++;
-        }else if(str.charAt(0) == '-'){
+        int start = 0;
+        if(str.charAt(0) == '-'){
+            start++;
             sign = -1;
-            p++;
+        }else if(str.charAt(0) == '+'){
+            start++;
         }
         long res = 0;
-        while(p < str.length()){
-            if(isValid(str.charAt(p))){
-                res = res * 10 + (str.charAt(p) - '0');
-                p++;
-                if(res * sign > Integer.MAX_VALUE){
-                    //throw new Exception("invalid input"); 
-                    return Integer.MAX_VALUE;
-                }else if( res * sign < Integer.MIN_VALUE){
-                    return Integer.MIN_VALUE;
-                }
-            }else{
-               //throw new Exception("invalid input"); 
-               break;
+        for(int i = start; i < str.length(); i++){
+            if(str.charAt(i) > '9' || str.charAt(i) < '0') return (int)res * sign;
+            res = res * 10 + (str.charAt(i) - '0');
+            if(sign == -1 && sign * res <= Integer.MIN_VALUE){
+                return Integer.MIN_VALUE;
+            }else if(sign == 1 && res >= Integer.MAX_VALUE){
+                return Integer.MAX_VALUE;
             }
         }
-        
-        return (int) (res * sign);
-    }
-    private boolean isValid(char c){
-        return c <= '9' && c >= '0';
+        return (int)res * sign;
     }
 }
