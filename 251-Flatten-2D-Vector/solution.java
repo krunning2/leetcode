bb2610d9a539b5029d109d2677542af2b215a5d2
krunning2 <zhidongke@yahoo.com>
2016-09-04 12:25:36 -0700

    [251. Flatten 2D Vector][Accepted]committed by Krunning

--- 251-Flatten-2D-Vector/solution.java
+++ 251-Flatten-2D-Vector/solution.java
@@ -1,28 +1,27 @@
 public class Vector2D implements Iterator<Integer> {
-    Iterator<List<Integer>> i2d;
-    Iterator<Integer> i1d;
+    Iterator<List<Integer>> lists;
+    Iterator<Integer> ints;
     public Vector2D(List<List<Integer>> vec2d) {
-        i2d = vec2d.iterator();
-        if(i2d.hasNext()){
-            i1d = i2d.next().iterator();
+        if(vec2d != null){
+            lists = vec2d.iterator();
+            if(lists.hasNext()){
+                ints = lists.next().iterator();
+            }
         }
     }
 
     @Override
     public Integer next() {
-        return i1d.next();
+        return ints.next();
     }
 
     @Override
     public boolean hasNext() {
-        while(i2d.hasNext() || i1d != null && i1d.hasNext()){
-            if(i1d.hasNext()){
-                return true;
-            }else{
-                i1d = i2d.next().iterator();
-            }
+        if(ints == null || !ints.hasNext() && !lists.hasNext()) return false;
+        while(!ints.hasNext() && lists.hasNext()){
+            ints = lists.next().iterator();
         }
-        return false;
+        return ints.hasNext();
     }
 }
 
