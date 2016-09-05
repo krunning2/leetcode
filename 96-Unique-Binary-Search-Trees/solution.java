4f13af09819988ef1cd0e7a5c20f13b42b1538e4
krunning2 <zhidongke@yahoo.com>
2016-09-04 19:19:49 -0700

    [96. Unique Binary Search Trees][Accepted]committed by Krunning

--- 96-Unique-Binary-Search-Trees/solution.java
+++ 96-Unique-Binary-Search-Trees/solution.java
@@ -4,8 +4,8 @@ public class Solution {
         dp[0] = 1;
         dp[1] = 1;
         for(int i = 2; i <= n; i++){
-            for(int j = 1; j <= i; j++){
-                dp[i] += dp[j - 1] * dp[i - j];
+            for(int j = 0; j < i; j++){
+                dp[i] += dp[j] * dp[i - j - 1]; 
             }
         }
         return dp[n];
