64a82f852ad97e24f817a6c29cc2f5570ea53769
krunning2 <zhidongke@yahoo.com>
2016-09-05 12:39:46 -0700

    [344. Reverse String][Accepted]committed by Krunning

--- 344-Reverse-String/solution.java
+++ 344-Reverse-String/solution.java
@@ -1,9 +1,8 @@
 public class Solution {
     public String reverseString(String s) {
-        if(s == null || s.length() == 0) return s;
-        char chars[] = s.toCharArray();
-        int start = 0;
-        int end = s.length() - 1;
+        if(s == null || s.length() <= 1) return s;
+        char[] chars = s.toCharArray();
+        int start = 0, end = chars.length - 1;
         while(start < end){
             char tmp = chars[start];
             chars[start] = chars[end];
