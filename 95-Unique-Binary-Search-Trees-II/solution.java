5700fa81853a91104d4b3fbbef599dcf44d8bd79
krunning2 <zhidongke@yahoo.com>
2016-09-04 19:26:12 -0700

    [95. Unique Binary Search Trees II][Accepted]committed by Krunning

--- 95-Unique-Binary-Search-Trees-II/solution.java
+++ 95-Unique-Binary-Search-Trees-II/solution.java
@@ -9,25 +9,23 @@
  */
 public class Solution {
     public List<TreeNode> generateTrees(int n) {
-        if(n <= 0){
-            return new ArrayList<TreeNode>();
-        }
-        return build(1, n);
+        if(n <= 0) return new ArrayList<>();
+        return get(1, n);
     }
-    private List<TreeNode> build(int start, int end){
-        List<TreeNode> res = new ArrayList<TreeNode> ();
+    private List<TreeNode> get(int start, int end){
+        List<TreeNode> res = new ArrayList<TreeNode>();
         if(start > end){
             res.add(null);
             return res;
         }
         for(int i = start; i <= end; i++){
-            List<TreeNode> left = build( start, i - 1);
-            List<TreeNode> right = build(i + 1, end);
+            List<TreeNode> left = get(start, i - 1);
+            List<TreeNode> right = get(i + 1, end);
             for(TreeNode l : left){
                 for(TreeNode r : right){
                     TreeNode root = new TreeNode(i);
-                    root.left = l;
                     root.right = r;
+                    root.left = l;
                     res.add(root);
                 }
             }
