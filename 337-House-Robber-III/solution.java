fff424ce43b0d45efa600993750d03e33d816b1f
krunning2 <zhidongke@yahoo.com>
2016-09-04 18:25:36 -0700

    [337. House Robber III][Accepted]committed by Krunning

--- 337-House-Robber-III/solution.java
+++ 337-House-Robber-III/solution.java
@@ -7,21 +7,19 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
- //cur[0] means don't rob current, cur[1] means rob current
 public class Solution {
     public int rob(TreeNode root) {
-        int[] cur = helper(root);
-        return Math.max(cur[0], cur[1]);
+        int[] res = robHelper(root);
+        return Math.max(res[0], res[1]);
     }
-    private int[] helper(TreeNode root){
-        if(root == null){
-            return new int[2];
-        }
-        int left[] = helper(root.left);
-        int right[] = helper(root.right);
-        int[] cur = new int[2];
-        cur[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
-        cur[1] = root.val + left[0] + right[0];
-        return cur;
+    //res[0] rob current, res[1] don't rob current
+    private int[] robHelper(TreeNode root){
+        if(root == null) return new int[2];
+        int[] left = robHelper(root.left);
+        int[] right = robHelper(root.right);
+        int[] res = new int[2];
+        res[0] = left[1] + right[1] + root.val;
+        res[1] = Math.max(left[1], left[0]) + Math.max(right[1], right[0]);
+        return res;
     }
 }
