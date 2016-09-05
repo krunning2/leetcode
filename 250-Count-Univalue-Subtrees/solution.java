544af6e2a7bd7fc2096a35fafaeaefce778dccb4
krunning2 <zhidongke@yahoo.com>
2016-09-04 20:10:11 -0700

    [250. Count Univalue Subtrees][Accepted]committed by Krunning

--- 250-Count-Univalue-Subtrees/solution.java
+++ 250-Count-Univalue-Subtrees/solution.java
@@ -19,8 +19,7 @@ public class Solution {
     private boolean isUnival(TreeNode root) {
         if (root == null) return true;
         if (isUnival(root.left) & isUnival(root.right)) {
-            if (root.left != null && root.left.val != root.val) return false;
-            if (root.right != null && root.right.val != root.val) return false;
+            if (root.right != null && root.right.val != root.val || root.left != null && root.left.val != root.val) return false;
             count++;
             return true;
         }
