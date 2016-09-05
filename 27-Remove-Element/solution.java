9af1f8030a3791c3085be4969eaea55f9fdaf37a
krunning2 <zhidongke@yahoo.com>
2016-09-05 12:37:59 -0700

    [27. Remove Element][Accepted]committed by Krunning

--- 27-Remove-Element/solution.java
+++ 27-Remove-Element/solution.java
@@ -1,12 +1,12 @@
 public class Solution {
     public int removeElement(int[] nums, int val) {
-        int start = 0;
-        int p = 0;
+        int start = 0, p = 0;
         while(p < nums.length){
-            if(nums[p] != val){
-                nums[start++] = nums[p];
+            if(nums[p] == val){
+                p++;
+            }else{
+                nums[start++] = nums[p++];
             }
-            p++;
         }
         return start;
     }
