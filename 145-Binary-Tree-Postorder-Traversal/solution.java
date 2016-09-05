be80b17ac8c8adf1d3404512242ec54c91ea312e
krunning2 <zhidongke@yahoo.com>
2016-09-04 18:39:38 -0700

    [145. Binary Tree Postorder Traversal][Accepted]committed by Krunning

--- 145-Binary-Tree-Postorder-Traversal/solution.java
+++ 145-Binary-Tree-Postorder-Traversal/solution.java
@@ -9,16 +9,17 @@
  */
 public class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-        LinkedList<Integer> res = new LinkedList<Integer>();
+        List<Integer> res = new ArrayList<Integer> ();
+        Stack<TreeNode> stack = new Stack<>();
         if(root == null) return res;
-        Stack<TreeNode> stack = new Stack<TreeNode>();
         stack.push(root);
         while(!stack.isEmpty()){
             TreeNode cur = stack.pop();
-            res.addFirst(cur.val);
+            res.add(cur.val);
             if(cur.left != null) stack.push(cur.left);
             if(cur.right != null) stack.push(cur.right);
         }
+        Collections.reverse(res);
         return res;
     }
 }
