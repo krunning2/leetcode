21306f271a46d51f90091c1eb4f5ccbffa4763a5
krunning2 <zhidongke@yahoo.com>
2016-09-05 22:52:11 -0700

    [123. Best Time to Buy and Sell Stock III][Accepted]committed by Krunning

--- 123-Best-Time-to-Buy-and-Sell-Stock-III/solution.java
+++ 123-Best-Time-to-Buy-and-Sell-Stock-III/solution.java
@@ -1,21 +1,22 @@
 public class Solution {
     public int maxProfit(int[] prices) {
-        int len = prices.length;
-        int[] left = new int[len];
-        int max = 0;
-        int lowest = Integer.MAX_VALUE;
-        for(int i = 0; i < len; i++){
-            max = Math.max(max, prices[i] - lowest);
-            left[i] = max;
-            lowest = Math.min(lowest, prices[i]);
+        if(prices == null || prices.length == 0) return 0;
+        int m = prices.length;
+        int[] leftMax = new int[m];
+        int min = Integer.MAX_VALUE;
+        int profit = 0;
+        for(int i = 0; i < prices.length; i++){
+            profit = Math.max(profit, prices[i] - min);
+            min = Math.min(min, prices[i]);
+            leftMax[i] = profit;
         }
-        max = 0;
-        int highest = 0;
+        int max = 0;
+        profit = 0;
         int res = 0;
-        for(int i = len - 1; i >= 0; i --){
-            max = highest - prices[i];
-            res = Math.max(res, max + left[i]);
-            highest = Math.max(highest, prices[i]);
+        for(int i = prices.length - 1; i >= 0; i--){
+            profit = Math.max(profit, max - prices[i]);
+            max = Math.max(max, prices[i]);
+            res = Math.max(res, profit + leftMax[i]);
         }
         return res;
     }
