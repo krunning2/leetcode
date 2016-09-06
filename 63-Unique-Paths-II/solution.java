6c72b51f5b7bcff4c61142f3e6497f8402cf8c8d
krunning2 <zhidongke@yahoo.com>
2016-09-05 21:26:55 -0700

    [63. Unique Paths II][Accepted]committed by Krunning

--- 63-Unique-Paths-II/solution.java
+++ 63-Unique-Paths-II/solution.java
@@ -1,6 +1,8 @@
 public class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        if(obstacleGrid == null || obstacleGrid.length == 0) return 0;
+        if(obstacleGrid == null || obstacleGrid.length == 0){
+            return 0;
+        }
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
         int[][] dp = new int[2][n];
@@ -11,15 +13,10 @@ public class Solution {
                 break;
             }
         }
-        
         for(int i = 1; i < m; i++){
-            dp[i % 2][0] = dp[(i - 1) % 2][0] != 0 && obstacleGrid[i][0] != 1 ? 1 : 0;
+            dp[i % 2][0] = obstacleGrid[i][0] != 1 && dp[(i - 1) % 2][0] == 1 ? 1 : 0;
             for(int j = 1; j < n; j++){
-                if(obstacleGrid[i][j] == 0){
-                    dp[i % 2][j] = dp[(i - 1) % 2][j] + dp[i % 2][j - 1];
-                }else{
-                    dp[i % 2][j] = 0;
-                }
+                dp[i % 2][j] = obstacleGrid[i][j] == 0 ? dp[(i - 1) % 2][j] + dp[i % 2][j - 1] : 0;
             }
         }
         return dp[(m - 1) % 2][n - 1];
