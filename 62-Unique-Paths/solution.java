4834ef4de0e2a3e85b37b57e1548f2a3b8b32e82
krunning2 <zhidongke@yahoo.com>
2016-09-05 21:18:50 -0700

    [62. Unique Paths][Accepted]committed by Krunning

--- 62-Unique-Paths/solution.java
+++ 62-Unique-Paths/solution.java
@@ -2,11 +2,11 @@ public class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[2][n];
         dp[0][0] = 1;
-        dp[1][0] = 1;
         for(int i = 0; i < n; i++){
             dp[0][i] = 1;
         }
         for(int i = 1; i < m; i++){
+            dp[i % 2][0] = 1;
             for(int j = 1; j < n; j++){
                 dp[i % 2][j] = dp[(i - 1) % 2][j] + dp[i % 2][j - 1];
             }
