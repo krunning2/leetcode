ca0b2580b85b154b54583a46855838a7fbcf8559
krunning2 <zhidongke@yahoo.com>
2016-09-05 22:28:01 -0700

    [120. Triangle][Accepted]committed by Krunning

--- 120-Triangle/solution.java
+++ 120-Triangle/solution.java
@@ -1,24 +1,23 @@
 public class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        if(triangle == null || triangle.size() == 0) return 0;
         int size = triangle.size();
         int[] dp = new int[size];
         dp[0] = triangle.get(0).get(0);
         for(int i = 1; i < size; i++){
             List<Integer> cur = triangle.get(i);
             for(int j = cur.size() - 1; j >= 0; j--){
-                if(j == cur.size() - 1){
-                    dp[j] = dp[j - 1] + cur.get(j);
-                }else if(j == 0){
+                if(j == 0){
                     dp[j] = dp[j] + cur.get(j);
+                }else if(j == cur.size() - 1){
+                    dp[j] = dp[j - 1] + cur.get(j);
                 }else{
                     dp[j] = Math.min(dp[j], dp[j - 1]) + cur.get(j);
                 }
             }
         }
         int res = Integer.MAX_VALUE;
-        for(int i = 0; i < size; i++){
-            res = Math.min(res, dp[i]);
+        for(int n : dp){
+            res = Math.min(n, res);
         }
         return res;
     }
