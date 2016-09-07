2c6dfddf547c247643f8c9870df72495d2e5751d
krunning2 <zhidongke@yahoo.com>
2016-09-06 21:29:15 -0700

    [295. Find Median from Data Stream][Accepted]committed by Krunning

--- 295-Find-Median-from-Data-Stream/solution.java
+++ 295-Find-Median-from-Data-Stream/solution.java
@@ -1,21 +1,38 @@
 public class MedianFinder {
     PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
     PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>((a, b) -> b - a);
+    Integer median = null;
     // Adds a number into the data structure.
     public void addNum(int num) {
-        maxHeap.offer(num);
-        minHeap.offer(maxHeap.poll());
-        if(minHeap.size() > maxHeap.size()){
-            maxHeap.offer(minHeap.poll());
+        if(median == null){
+            median = num;
+        } else if(median > num){
+            maxHeap.offer(num);
+        }else{
+            minHeap.offer(num);
         }
+        
+        if(maxHeap.size() < minHeap.size()){
+            maxHeap.offer(median);
+            median = minHeap.poll();
+        }else if(maxHeap.size() > minHeap.size() + 1){
+            minHeap.offer(median);
+            median = maxHeap.poll();
+        }
+        
+        // maxHeap.offer(num);
+        // minHeap.offer(maxHeap.poll());
+        // if(minHeap.size() > maxHeap.size()){
+        //     maxHeap.offer(minHeap.poll());
+        // }
     }
 
     // Returns the median of current data stream
     public double findMedian() {
         if(minHeap.size() == maxHeap.size()){
-            return (minHeap.peek() + maxHeap.peek()) / 2.0;
+            return median * 1.0;
         }else{
-            return maxHeap.peek() * 1.0;
+            return (maxHeap.peek() + median) / 2.0;
         }
     }
 };
