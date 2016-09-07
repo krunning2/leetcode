1d0aeb40a2a3406fffdc6fca3c91071c381be9ef
krunning2 <zhidongke@yahoo.com>
2016-09-06 22:14:36 -0700

    [280. Wiggle Sort][Accepted]committed by Krunning

--- 280-Wiggle-Sort/solution.java
+++ 280-Wiggle-Sort/solution.java
@@ -1,12 +1,18 @@
 public class Solution {
     public void wiggleSort(int[] nums) {
+        if(nums == null || nums.length <= 1){
+            return;
+        }
         for(int i = 1; i < nums.length; i++){
-            if(i % 2 == 1 && nums[i] < nums[i - 1] || i % 2 == 0 && nums[i] > nums[i - 1]){
+            if(i % 2 == 0 && nums[i] > nums[i - 1]){
                 int tmp = nums[i];
                 nums[i] = nums[i - 1];
-                nums[i - 1] = tmp;
+                nums[i -  1] = tmp;
+            }else if(i % 2 == 1 && nums[i] < nums[i - 1]){
+                int tmp = nums[i];
+                nums[i] = nums[i - 1];
+                nums[i -  1] = tmp;
             }
         }
     }
-    
 }
