34727483d1cc59b9d0d5167537de9de25f43db32
krunning2 <zhidongke@yahoo.com>
2016-09-07 11:02:57 -0700

    [356. Line Reflection][Accepted]committed by Krunning

--- 356-Line-Reflection/solution.java
+++ 356-Line-Reflection/solution.java
@@ -1,25 +1,17 @@
 public class Solution {
     public boolean isReflected(int[][] points) {
-        if(points == null || points.length == 0){
-            return true;
-        }
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
-        HashMap<Integer, Set<Integer>> map = new HashMap<Integer, Set<Integer>>();
-        for(int i = 0; i < points.length; i++){
-            max = Math.max(max, points[i][0]);
-            min = Math.min(min, points[i][0]);
-            map.putIfAbsent(points[i][0], new HashSet<Integer>());
-            map.get(points[i][0]).add(points[i][1]);
+        Set<Integer> set = new HashSet<>();
+        for(int[] p : points){
+            max = Math.max(max, p[0]);
+            min = Math.min(min, p[0]);
+            set.add(Arrays.hashCode(p));
         }
-        int sum =(max + min);
-        for(int i = 0; i < points.length; i++){
-            int curX = points[i][0];
-            int curY = points[i][1];
-            int targetX = sum - curX;
-            if(map.containsKey(targetX) && map.get(targetX).contains(curY)){
-                continue;
-            }else{
+        int sum = max + min;
+        for(int[] p : points){
+            int[] target = {sum - p[0], p[1]};
+            if(!set.contains(Arrays.hashCode(target))){
                 return false;
             }
         }
