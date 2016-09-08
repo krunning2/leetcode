bc3e42b0116451cd0c41084b8f95a8c448d78267
krunning2 <zhidongke@yahoo.com>
2016-09-07 22:05:03 -0700

    [332. Reconstruct Itinerary][Accepted]committed by Krunning

--- 332-Reconstruct-Itinerary/solution.java
+++ 332-Reconstruct-Itinerary/solution.java
@@ -6,7 +6,17 @@ public class Solution {
             map.putIfAbsent(ticket[0], new PriorityQueue<String>());
             map.get(ticket[0]).add(ticket[1]);
         }
-        DFS("JFK", res, map);
+        //DFS("JFK", res, map);
+        Stack<String> stack = new Stack<>();
+        stack.push("JFK");
+        while(!stack.isEmpty()){
+            while(map.get(stack.peek()) != null && !map.get(stack.peek()).isEmpty()){
+                stack.push(map.get(stack.peek()).poll());
+            }
+            res.addFirst(stack.pop());
+        }
+        
+        
         return res;
     }
     private void DFS(String departure, LinkedList<String> res, HashMap<String, PriorityQueue<String>> map){
