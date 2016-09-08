3cd482ee23386a84b8bef9ca69f13e9491711901
krunning2 <zhidongke@yahoo.com>
2016-09-07 20:46:10 -0700

    [310. Minimum Height Trees][Accepted]committed by Krunning

--- 310-Minimum-Height-Trees/solution.java
+++ 310-Minimum-Height-Trees/solution.java
@@ -1,22 +1,24 @@
 public class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        List<Integer> res = new ArrayList<>();
-        if(n <= 0) return res;
-        if (n == 1){
+        List<Integer> res = new ArrayList<Integer>();
+        if(n <= 0){
+            return res;
+        }
+        if(n == 1){
             res.add(0);
             return res;
         }
+        Map<Integer, List<Integer>> map = new HashMap<>();
         int[] indegree = new int[n];
-        Map<Integer, List<Integer>> graph = new HashMap<Integer, List<Integer>>();
         for(int i = 0; i < edges.length; i++){
-            indegree[edges[i][0]]++;
-            indegree[edges[i][1]]++;
-            graph.putIfAbsent(edges[i][0], new ArrayList<Integer>());
-            graph.get(edges[i][0]).add(edges[i][1]);
-            graph.putIfAbsent(edges[i][1], new ArrayList<Integer>());
-            graph.get(edges[i][1]).add(edges[i][0]);
+            indegree[edges[i][0]] ++;
+            indegree[edges[i][1]] ++;
+            map.putIfAbsent(edges[i][0], new ArrayList<>());
+            map.putIfAbsent(edges[i][1], new ArrayList<>());
+            map.get(edges[i][0]).add(edges[i][1]);
+            map.get(edges[i][1]).add(edges[i][0]);
         }
-        Queue<Integer> queue = new LinkedList<Integer>();
+        Queue<Integer> queue = new LinkedList<>();
         for(int i = 0; i < indegree.length; i++){
             if(indegree[i] == 1){
                 queue.offer(i);
@@ -26,16 +28,18 @@ public class Solution {
             int size = queue.size();
             for(int i = 0; i < size; i++){
                 int cur = queue.poll();
-                for(int x : graph.get(cur)){
-                    indegree[x] --;
-                    if(indegree[x] == 1) {
-                        queue.offer(x);
+                for(int node : map.get(cur)){
+                    indegree[node] --;
+                    if(indegree[node] == 1){
+                        queue.offer(node);
                     }
                 }
                 n--;
             }
         }
-        while(!queue.isEmpty()) res.add(queue.poll());
+        while(!queue.isEmpty()){
+            res.add(queue.poll());
+        }
         return res;
     }
 }
