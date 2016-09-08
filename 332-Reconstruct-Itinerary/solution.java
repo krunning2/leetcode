285a21330ca5e301115618e31841546968e2be99
krunning2 <zhidongke@yahoo.com>
2016-09-07 21:51:38 -0700

    [332. Reconstruct Itinerary][Accepted]committed by Krunning

--- 332-Reconstruct-Itinerary/solution.java
+++ 332-Reconstruct-Itinerary/solution.java
@@ -1,32 +1,19 @@
 public class Solution {
-    
     public List<String> findItinerary(String[][] tickets) {
-        Map<String, PriorityQueue<String>> flights = new HashMap();
-        LinkedList<String> path = new LinkedList();
-        for (String[] oneway: tickets) {
-            flights.putIfAbsent(oneway[0], new PriorityQueue());
-            flights.get(oneway[0]).add(oneway[1]);
+        LinkedList<String> res = new LinkedList<String>();
+        HashMap<String, PriorityQueue<String>> map = new HashMap<>();
+        for(String[] ticket : tickets){
+            map.putIfAbsent(ticket[0], new PriorityQueue<String>());
+            map.get(ticket[0]).add(ticket[1]);
         }
-        Stack<String> stack = new Stack<>();
-        stack.push("JFK");
-        
-        while(!stack.isEmpty()){
-            PriorityQueue<String> arrivals = flights.get(stack.peek());
-            while (arrivals != null && !arrivals.isEmpty()){
-                stack.push(arrivals.poll());
-                arrivals = flights.get(stack.peek());
-            }
-            path.addFirst(stack.pop());
-        }
-        
-        return path;
+        DFS("JFK", res, map);
+        return res;
     }
-    public void dfs(String departure, Map<String, PriorityQueue<String>>flights, LinkedList<String> path) {
-        Stack<String> stack = new Stack<>();
-        PriorityQueue<String> arrivals = flights.get(departure);
-        while (arrivals != null && !arrivals.isEmpty()){
-            dfs(arrivals.poll(), flights, path);
+    private void DFS(String departure, LinkedList<String> res, HashMap<String, PriorityQueue<String>> map){
+        PriorityQueue<String> arrivals = map.get(departure);
+        while(arrivals != null && !arrivals.isEmpty()){
+            DFS(arrivals.poll(), res, map);
         }
-        path.addFirst(departure);
+        res.addFirst(departure);
     }
 }
