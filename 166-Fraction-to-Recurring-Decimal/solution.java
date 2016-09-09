af305fe8f2fefa07fdd5fa258c1cbbc5fcb51656
krunning2 <zhidongke@yahoo.com>
2016-09-08 20:52:12 -0700

    [166. Fraction to Recurring Decimal][Accepted]committed by Krunning

--- 166-Fraction-to-Recurring-Decimal/solution.java
+++ 166-Fraction-to-Recurring-Decimal/solution.java
@@ -1,33 +1,35 @@
 public class Solution {
     public String fractionToDecimal(int numerator, int denominator) {
-        if(denominator == 0) return "NaN";
         if(numerator == 0) return "0";
+        if(denominator == 0) return "NaN";
+        Map<Long, Integer> map = new HashMap<Long, Integer>();
         StringBuilder sb = new StringBuilder();
-        Long n = new Long(numerator);
-        Long d = new Long(denominator);
-        if(n * d < 0){
+        int sign = 1;
+        if(numerator < 0) sign *= -1;
+        if(denominator < 0) sign *= -1;
+        if(sign < 0){
             sb.append("-");
         }
-        n = Math.abs(n);
+        Long n = new Long(numerator);
+        Long d = new Long(denominator);
         d = Math.abs(d);
+        n = Math.abs(n);
         sb.append(n / d);
         if(n % d == 0){
             return sb.toString();
-        }else{
-            sb.append(".");    
         }
-        HashMap<Long, Integer> map = new HashMap<Long, Integer>();
-        Long r = n % d;
-        while(r > 0){
-            if(map.containsKey(r)){
-                sb.insert(map.get(r), "(");
+        sb.append(".");
+        long remaining = n % d;
+        while(remaining > 0){
+            if(map.containsKey(remaining)){
+                sb.insert(map.get(remaining), "(");
                 sb.append(")");
-                break;
+                return sb.toString();
             }
-            map.put(r, sb.length());
-            r *= 10;
-            sb.append(r / d);
-            r %= d;
+            map.put(remaining, sb.length());
+            remaining *= 10;
+            sb.append(remaining / d);
+            remaining %= d;
         }
         return sb.toString();
     }
