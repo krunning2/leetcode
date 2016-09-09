ec1ebcb3a054fab6994ada8cf460984974e4528b
krunning2 <zhidongke@yahoo.com>
2016-09-08 22:31:37 -0700

    [212. Word Search II][Accepted]committed by Krunning

--- 212-Word-Search-II/solution.java
+++ 212-Word-Search-II/solution.java
@@ -40,53 +40,52 @@ public class Solution {
         
     }
     
-    class TrieNode {  
-        public TrieNode[] children = new TrieNode[26];  
-        public String item = "";  
-          
-        // Initialize your data structure here.  
-        public TrieNode() {  
-        }  
-    }  
-  
-    class Trie {  
-        private TrieNode root;  
-      
-        public Trie() {  
-            root = new TrieNode();  
-        }  
-      
-        // Inserts a word into the trie.  
-        public void insert(String word) {  
-            TrieNode node = root;  
-            for (char c : word.toCharArray()) {  
-                if (node.children[c - 'a'] == null) {  
-                    node.children[c - 'a'] = new TrieNode();  
-                }  
-                node = node.children[c - 'a'];  
-            }  
-            node.item = word;  
-        }  
-      
-        // Returns if the word is in the trie.  
-        public boolean search(String word) {  
-            TrieNode node = root;  
-            for (char c : word.toCharArray()) {  
-                if (node.children[c - 'a'] == null) return false;  
-                node = node.children[c - 'a'];  
-            }  
-            return node.item.equals(word);  
-        }  
-      
-        // Returns if there is any word in the trie  
-        // that starts with the given prefix.  
-        public boolean startsWith(String prefix) {  
-            TrieNode node = root;  
-            for (char c : prefix.toCharArray()) {  
-                if (node.children[c - 'a'] == null) return false;  
-                node = node.children[c - 'a'];  
-            }  
-            return true;  
-        }  
+    class Trie{
+        Node root;
+        Trie(){
+            root = new Node();
+        }
+        
+        public void insert(String word){
+            Node cur = root;
+            for(char c : word.toCharArray()){
+                if(!cur.map.containsKey(c)){
+                    cur.map.put(c, new Node());
+                }
+                cur = cur.map.get(c);
+            }
+            cur.end = true;
+        }
+        
+        public boolean search(String word){
+            Node cur = root;
+            for(char c : word.toCharArray()){
+                if(!cur.map.containsKey(c)){
+                    return false;
+                }
+                cur = cur.map.get(c);
+            }
+            return cur.end;
+        }
+        
+        public boolean startsWith(String word){
+            Node cur = root;
+            for(char c : word.toCharArray()){
+                if(!cur.map.containsKey(c)){
+                    return false;
+                }
+                cur = cur.map.get(c);
+            }
+            return true;
+        }
+        
+        class Node {
+            Map<Character, Node> map;
+            boolean end;
+            Node(){
+                map = new HashMap<>();
+                end = false;
+            }
+        }
     }
 }
