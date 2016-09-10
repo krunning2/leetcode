6900510657ef2d3e2581d4f9520c397c3826530b
krunning2 <zhidongke@yahoo.com>
2016-09-10 12:24:30 -0700

    [308. Range Sum Query 2D - Mutable][Accepted]committed by Krunning

--- 308-Range-Sum-Query-2D---Mutable/solution.java
+++ 308-Range-Sum-Query-2D---Mutable/solution.java
@@ -1,8 +1,7 @@
 public class NumMatrix {
+    int[][] BIT2D;
     int[][] matrix;
-    int[][] BIT;
-    int m;
-    int n;
+    int m, n;
     public NumMatrix(int[][] matrix) {
         if(matrix == null || matrix.length == 0){
             return;
@@ -10,7 +9,7 @@ public class NumMatrix {
         this.matrix = matrix;
         m = matrix.length;
         n = matrix[0].length;
-        BIT = new int[m + 1][n + 1];
+        BIT2D = new int[m + 1][n + 1];
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j ++){
                 updateBIT(i, j, matrix[i][j]);
@@ -18,24 +17,6 @@ public class NumMatrix {
         }
     }
     
-    private void updateBIT(int row, int col, int val){
-        for(int i = row + 1; i <= m; i += i & -i){
-            for(int j = col + 1; j <= n; j += j & -j){
-                BIT[i][j] += val;
-            }
-        }
-    }
-    
-    private int getSum(int row, int col){
-        int sum = 0;
-        for(int i = row + 1; i > 0; i -= i & -i){
-            for(int j = col + 1; j > 0; j -= j & -j){
-                sum += BIT[i][j];
-            }
-        }
-        return sum;
-    }
-
     public void update(int row, int col, int val) {
         int diff = val - matrix[row][col];
         matrix[row][col] = val;
@@ -44,6 +25,24 @@ public class NumMatrix {
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
         return getSum(row2, col2) - getSum(row2, col1 - 1) - getSum(row1 - 1, col2) + getSum(row1 -1 , col1 - 1); 
+    }
+    
+    private void updateBIT(int row, int col, int val){
+        for(int i = row + 1; i <= m; i += i & -i){
+            for(int j = col + 1; j <= n; j += j & -j){
+                BIT2D[i][j] += val;
+            }
+        }
+    }
+    
+    private int getSum(int row, int col){
+        int sum = 0;
+        for(int i = row + 1; i > 0; i -= i & -i){
+            for(int j = col + 1; j > 0; j -= j & -j){
+                sum += BIT2D[i][j];
+            }
+        }
+        return sum;
     }
     
 }
