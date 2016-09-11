5906280c1c02cef1e83c6606f56fbaad7a9b8da7
krunning2 <zhidongke@yahoo.com>
2016-09-10 22:36:46 -0700

    [14. Longest Common Prefix][Accepted]committed by Krunning

--- 14-Longest-Common-Prefix/solution.java
+++ 14-Longest-Common-Prefix/solution.java
@@ -1,9 +1,10 @@
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
-        int p = 0;
-        if(strs == null || strs.length == 0) return "";
-        if(strs.length == 1) return strs[0];
+        if(strs == null || strs.length == 0){
+            return "";
+        }
         String first = strs[0];
+        int p = 0;
         while(p < first.length()){
             char cur = first.charAt(p);
             for(int i = 1; i < strs.length; i++){
