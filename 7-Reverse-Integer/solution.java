03e110cc2e9d749835d31ca1497552c42e09e82c
krunning2 <zhidongke@yahoo.com>
2016-09-10 22:19:16 -0700

    [7. Reverse Integer][Accepted]committed by Krunning

--- 7-Reverse-Integer/solution.java
+++ 7-Reverse-Integer/solution.java
@@ -1,13 +1,19 @@
 public class Solution {
     public int reverse(int x) {
         int sign = x < 0 ? -1 : 1;
-        long abs = Math.abs(x);
+        long y = Math.abs((long)x);
         long res = 0;
-        while(abs > 0){
-            res = 10 * res + abs % 10;
-            abs /= 10;
-            if(res > Integer.MAX_VALUE || res < Integer.MIN_VALUE) return 0;
+        while(y > 0){
+            res = res * 10 + y % 10;
+            y /= 10;
         }
-        return (int) (res * sign);
+        res = res * sign;
+        if(res > Integer.MAX_VALUE){
+            return 0;
+        }
+        if(res < Integer.MIN_VALUE){
+            return 0;
+        }
+        return (int)res;
     }
 }
