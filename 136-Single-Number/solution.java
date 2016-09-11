e8e43bf36b5bfadcc02d0dfa9db07bab019680e7
krunning2 <zhidongke@yahoo.com>
2016-09-10 21:30:41 -0700

    [136. Single Number][Accepted]committed by Krunning

--- 136-Single-Number/solution.java
+++ 136-Single-Number/solution.java
@@ -1,9 +1,9 @@
 public class Solution {
     public int singleNumber(int[] nums) {
-        int res = 0;
-        for(int i = 0; i < nums.length; i++){
-            res ^= nums[i];
+        int x = 0;
+        for(int n : nums){
+            x = x ^ n;
         }
-        return res;
+        return x;
     }
 }
