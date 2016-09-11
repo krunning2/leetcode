3c32ec259e5d84c708c41584d6bf6701ba37c906
krunning2 <zhidongke@yahoo.com>
2016-09-10 22:22:50 -0700

    [191. Number of 1 Bits][Accepted]committed by Krunning

--- 191-Number-of-1-Bits/solution.java
+++ 191-Number-of-1-Bits/solution.java
@@ -3,8 +3,8 @@ public class Solution {
     public int hammingWeight(int n) {
         int count = 0;
         for(int i = 0; i < 32; i++){
-            if((n & 1) == 1) count++;
-            n >>= 1;
+            count += (n & 1) == 1 ? 1: 0;
+            n = n >> 1;
         }
         return count;
     }
