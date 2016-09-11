7569045c3822340e4011900e9ed0c1c351a83a24
krunning2 <zhidongke@yahoo.com>
2016-09-10 20:30:46 -0700

    [327. Count of Range Sum][Accepted]committed by Krunning

--- 327-Count-of-Range-Sum/solution.java
+++ 327-Count-of-Range-Sum/solution.java
@@ -15,52 +15,47 @@ public class Solution {
         long res = 0;
         Node root = build(0, B.length - 1, B);
         for(int i = nums.length - 1; i >= 0; i--){
-            modify(root, sum);
+            update(root, sum);
             sum -= nums[i];
             res += query(root, lower + sum, upper + sum);
         }
         return (int) res;
     }
     
-    private Node build(int start, int end, long[] A){
-        if(start > end){
-            return null;
-        }
-        if(start == end){
-            return new Node(A[start], A[end], 0L);
-        }
-        int mid = (start + end) >> 1;
-        Node root = new Node(A[start], A[end], 0L);
-        root.left = build(start, mid, A);
-        root.right = build(mid + 1, end, A);
+    private Node build(int start, int end, long[] nums){
+        if(start > end) return null;
+        if(start == end) return new Node(nums[start], nums[end], 0L);
+        int mid = (start + end) / 2;
+        Node root = new Node(nums[start], nums[end], 0L);
+        root.left = build(start, mid, nums);
+        root.right = build(mid + 1, end, nums);
         return root;
     }
-    private long query(Node root, long start, long end){
-        if(root == null || root.end < start || root.start > end){
-            return 0;
-        }
-        if(start <= root.start && end >= root.end){
-            return root.val;
-        }
-        return query(root.left, start, end) + query(root.right, start, end);
-    }
-    private void modify(Node root, long val){
+    
+    private void update(Node root, long val){
         if(root == null){
             return;
         }
-        if(root.start <= val && root.end >= val){
-            root.val ++;
-            modify(root.left, val);
-            modify(root.right, val);
+        if(val >= root.start && val <= root.end){
+            root.val++;
+            update(root.left, val);
+            update(root.right, val);
         }
     }
+    
+    private long query(Node root, long start, long end){
+        if(start > end || root == null || root.end < start || root.start > end){
+            return 0;
+        }
+        if(start <= root.start && end >= root.end) return root.val;
+        return query(root.left, start, end) + query(root.right, start, end);
+    }
+    
+    
     class Node {
-        Node left;
-        Node right;
-        long start;
-        long end;
+        Node left, right;
+        long start, end;
         long val;
-        
         Node(long start, long end, long val){
             this.start = start;
             this.end = end;
