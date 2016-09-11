624ce9a0b2acf93b377007e7198192f1805faea7
krunning2 <zhidongke@yahoo.com>
2016-09-10 21:36:27 -0700

    [137. Single Number II][Accepted]committed by Krunning

--- 137-Single-Number-II/solution.java
+++ 137-Single-Number-II/solution.java
@@ -1,15 +1,15 @@
 public class Solution {
     public int singleNumber(int[] nums) {
         int[] bit = new int[32];
-        for(int i = 0; i < nums.length; i++){
-            for(int j = 0; j < 32; j++){
-                bit[j] += nums[i] >> j & 1;
+        for(int n : nums){
+            for(int i = 0; i < 32; i++){
+                bit[i] += (n >> i) & 1;
             }
         }
         int res = 0;
         for(int i = 0; i < 32; i++){
             if(bit[i] % 3 != 0){
-                res += 1 << i;
+                res += 1<<i;
             }
         }
         return res;
