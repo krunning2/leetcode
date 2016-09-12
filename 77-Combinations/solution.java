d61d745e77f60c137a698f1167341fdad6475231
krunning2 <zhidongke@yahoo.com>
2016-09-12 00:01:20 -0700

    [77. Combinations][Accepted]committed by Krunning

--- 77-Combinations/solution.java
+++ 77-Combinations/solution.java
@@ -1,17 +1,17 @@
 public class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> res  = new ArrayList<List<Integer>> ();
-        helper(n, k, res, new ArrayList<>(), 1);
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        DFS(res, new ArrayList<>(), k, 1, n);
         return res;
     }
-    private void helper(int n, int k, List<List<Integer>> res, List<Integer> cur, int pos){
+    private void DFS(List<List<Integer>> res, List<Integer> cur, int k, int pos, int n){
         if(cur.size() == k){
             res.add(new ArrayList<>(cur));
             return;
         }
-        for(int i = pos; i <= n; i ++){
+        for(int i = pos; i <= n; i++){
             cur.add(i);
-            helper(n, k, res, cur, i + 1);
+            DFS(res, cur, k, i + 1, n);
             cur.remove(cur.size() - 1);
         }
     }
