20e077d63efdc219b696fcd913e6292133f6e229
krunning2 <zhidongke@yahoo.com>
2016-09-11 22:57:35 -0700

    [223. Rectangle Area][Accepted]committed by Krunning

--- 223-Rectangle-Area/solution.java
+++ 223-Rectangle-Area/solution.java
@@ -1,12 +1,12 @@
 public class Solution {
     public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
-        int recOne = (C - A) * (D - B);
-        int recTwo = (G - E) * (H - F);
-        if(A > G || E > C || B > H || F > D){
-            return recOne + recTwo;
+        int aera1 = (C - A) * (D - B);
+        int aera2 = (G - E) * (H - F);
+        if(D < F || B > H || C < E || G < A){
+            return aera1 + aera2;
         }
-        int length = Math.min(C, G) - Math.max(A, E);
-        int height = Math.min(D, H) - Math.max(B, F);
-        return recOne + recTwo - length * height;
+        int height = Math.min(C, G) - Math.max(A, E);
+        int weight = Math.min(D, H) - Math.max(B, F);
+        return aera1 + aera2 - height * weight;
     }
 }
