2899cfd6fb32582701603d2c88c3b951e1798fad
krunning2 <zhidongke@yahoo.com>
2016-09-11 21:16:27 -0700

    [147. Insertion Sort List][Accepted]committed by Krunning

--- 147-Insertion-Sort-List/solution.java
+++ 147-Insertion-Sort-List/solution.java
@@ -10,7 +10,6 @@ public class Solution {
     public ListNode insertionSortList(ListNode head) {
         ListNode dummy = new ListNode(Integer.MIN_VALUE);
         ListNode cur = head;
-        // dummy.next = head;
         while(cur != null){
             ListNode pre = findInsertLocation(dummy, cur.val);
             ListNode next = cur.next;
@@ -20,9 +19,8 @@ public class Solution {
         }
         return dummy.next;
     }
-    private ListNode findInsertLocation(ListNode node, int val){
-        ListNode pre = null;
-        ListNode cur = node;
+    private ListNode findInsertLocation(ListNode head, int val){
+        ListNode pre = null, cur = head;
         while(cur != null && cur.val <= val){
             pre = cur;
             cur = cur.next;
