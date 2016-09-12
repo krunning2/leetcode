c96d4a2d4bc68c3addc01422336702d671eeeb87
krunning2 <zhidongke@yahoo.com>
2016-09-11 22:06:41 -0700

    [190. Reverse Bits][Accepted]committed by Krunning

--- 190-Reverse-Bits/solution.java
+++ 190-Reverse-Bits/solution.java
@@ -2,10 +2,10 @@ public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
         int res = 0;
-        for(int i = 0; i < 32; i++){
+        for(int i = 0 ; i < 32; i++){
             res += n & 1;
-            n >>= 1;
-            if(i < 31) res <<= 1;
+            n = n >> 1;
+            if(i < 31) res = res << 1;
         }
         return res;
     }
