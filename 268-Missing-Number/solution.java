5cda8e821d0d5c953fac21c5f9e071e2c14b03e1
krunning2 <zhidongke@yahoo.com>
2016-09-11 23:31:54 -0700

    [268. Missing Number][Accepted]committed by Krunning

--- 268-Missing-Number/solution.java
+++ 268-Missing-Number/solution.java
@@ -1,27 +1,10 @@
 public class Solution {
     public int missingNumber(int[] nums) {
-        if(nums == null || nums.length == 0){
-            return 0;
-        }
-        int n = nums.length;
-        boolean seenZero = false;
+        int res = nums.length;
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] == 0){
-                seenZero = true;
-                nums[i] = n + 1;
-            }
+            res ^= nums[i];
+            res ^= i;
         }
-        if(!seenZero) return 0;
-        for(int i = 0; i < nums.length; i++){
-            int index = Math.abs(nums[i]) - 1;
-            if(index < n)
-                nums[index] = -Math.abs(nums[index]);
-        }
-        for(int i = 0; i < n; i++){
-            if(nums[i] > 0){
-                return i + 1;
-            }
-        }
-        return n;
+        return res;
     }
 }
