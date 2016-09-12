dd93fd3d7e3171c3d877e34c9d1cf3702ba943bb
krunning2 <zhidongke@yahoo.com>
2016-09-11 21:59:39 -0700

    [48. Rotate Image][Accepted]committed by Krunning

--- 48-Rotate-Image/solution.java
+++ 48-Rotate-Image/solution.java
@@ -1,38 +1,13 @@
 public class Solution {
     public void rotate(int[][] matrix) {
-        if(matrix == null || matrix.length == 0){
-            return;
-        }
-        // if(matrix.length < 4){
-            rotate1(matrix);
-        // }else{
-        //     rotate2(matrix);
-        // }
-    }
-    
-    private void rotate1(int[][] matrix){
-        int n = matrix.length;
-        for(int i = 0; i < n / 2; i++){
-            for(int j = 0; j < (n + 1) / 2; j++){
+        int m = matrix.length;
+        for(int i = 0; i < m / 2; i++){
+            for(int j = 0; j < (m + 1) / 2; j++){
                 int tmp = matrix[i][j];
-                matrix[i][j] = matrix[n - j - 1][i];
-                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
-                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
-                matrix[j][n - i - 1] = tmp;
-            }
-        }
-    }
-    
-    // anti-clockwise
-    private void rotate2(int[][] matrix){
-        int n = matrix.length;
-        for(int i = 0; i < n / 2; i++){
-            for(int j = 0; j < (n + 1) / 2; j++){
-                int tmp = matrix[i][j];
-                matrix[i][j] = matrix[j][n - i - 1];
-                matrix[j][n - i - 1] = matrix[n - i - 1][n - j - 1];
-                matrix[n - i - 1][n - j - 1] = matrix[n - j - 1][i];
-                matrix[n - j - 1][i] = tmp;
+                matrix[i][j] = matrix[m - j - 1][i];
+                matrix[m - j - 1][i] = matrix[m - i - 1][m - j - 1];
+                matrix[m - i - 1][m - j - 1] = matrix[j][m - i - 1];
+                matrix[j][m - i - 1] = tmp;
             }
         }
     }
