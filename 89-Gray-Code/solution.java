3af0ad4e15e4f69919888eac6a488964c4e9e675
krunning2 <zhidongke@yahoo.com>
2016-09-11 23:42:46 -0700

    [89. Gray Code][Accepted]committed by Krunning

--- 89-Gray-Code/solution.java
+++ 89-Gray-Code/solution.java
@@ -1,13 +1,12 @@
 public class Solution {
     public List<Integer> grayCode(int n) {
-        List<Integer> res = new ArrayList();
+        List<Integer> res = new ArrayList<Integer>();
         res.add(0);
-        if(n == 0) return res;
         for(int i = 1; i <= n; i++){
             int size = res.size();
-            int x = 1 << (i - 1);
+            int digit = 1 << i - 1;
             for(int j = size - 1; j >= 0; j--){
-                res.add(res.get(j) + x);
+                res.add(res.get(j) + digit);
             }
         }
         return res;
