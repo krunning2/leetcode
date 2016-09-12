825d5476958d3920f4dacc08f46fb5586ea0b563
krunning2 <zhidongke@yahoo.com>
2016-09-11 20:51:53 -0700

    [29. Divide Two Integers][Accepted]committed by Krunning

--- 29-Divide-Two-Integers/solution.java
+++ 29-Divide-Two-Integers/solution.java
@@ -1,21 +1,23 @@
 public class Solution {
     public int divide(int dividend, int divisor) {
         if(divisor == 0) return Integer.MAX_VALUE;
-        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+        if(dividend == Integer.MIN_VALUE && divisor == -1){
             return Integer.MAX_VALUE;
         }
         int sign = 1;
         if(dividend < 0) sign *= -1;
         if(divisor < 0) sign *= -1;
-        long dvd = (long) Math.abs((long) dividend);
-        long dis = (long) Math.abs((long) divisor);
+        long dis = Math.abs((long)divisor);
+        long did = Math.abs((long)dividend);
         int res = 0;
-        while(dis <= dvd){
+        while(did >= dis){
             int shift = 0;
-            while(dvd >= (dis << shift)) shift++;
+            while(did >= (dis << shift) ){
+                shift ++;
+            }
             shift --;
             res += 1 << shift;
-            dvd -= dis << shift;
+            did -= dis << shift;
         }
         return sign * res;
     }
