ccec04201cca577c2c1e20b9ffbccd8656181d2b
krunning2 <zhidongke@yahoo.com>
2016-09-11 20:39:31 -0700

    [172. Factorial Trailing Zeroes][Accepted]committed by Krunning

--- 172-Factorial-Trailing-Zeroes/solution.java
+++ 172-Factorial-Trailing-Zeroes/solution.java
@@ -1,9 +1,10 @@
 public class Solution {
     public int trailingZeroes(int n) {
         int count = 0;
-        while(n / 5 >= 1){
-            count += n / 5;
-            n /= 5;
+        long fac = 5;
+        while(n / fac >= 1){
+            count += n / fac;
+            fac *= 5;
         }
         return count;
     }
