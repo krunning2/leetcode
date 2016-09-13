f3508e345304f28a72d65980f75a699f81d6181b
krunning2 <zhidongke@yahoo.com>
2016-09-13 00:24:32 -0700

    [119. Pascal's Triangle II][Accepted]committed by Krunning

--- 119-Pascal's-Triangle-II/solution.java
+++ 119-Pascal's-Triangle-II/solution.java
@@ -1,26 +1,20 @@
 public class Solution {
-    public List<Integer> getRow(int k) {
-        
-        if(k < 0){
-            List<Integer> res = new ArrayList<Integer>();
-            return res;
-        } 
-        if(k == 0){
-            List<Integer> res = new ArrayList<Integer>();
-            res.add(1);
-            return res;
-        }
-        Integer[] res = new Integer[k + 1];
-        res[0] = 1;
-        for(int i = 1; i <= k; i++){
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> res = new ArrayList<Integer>();
+        if(rowIndex < 0) return res;
+        res.add(1);
+        if(rowIndex == 0) return res;
+        Integer[] tmp = new Integer[rowIndex + 1];
+        tmp[0] = 1;
+        for(int i = 1; i <= rowIndex; i++){
             for(int j = i; j >= 0; j--){
-                if(j == 0 || j == i){
-                    res[j] = 1;
+                if(j == i || j == 0){
+                    tmp[j] = 1;
                 }else{
-                    res[j] = res[j] + res[j - 1];
+                    tmp[j] = tmp[j] + tmp[j - 1];
                 }
             }
         }
-        return new ArrayList<Integer>(Arrays.asList(res));
+        return new ArrayList<Integer>(Arrays.asList(tmp));
     }
 }
