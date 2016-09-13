1eb559058963876fc88f709b03c97c62f6edc7b6
krunning2 <zhidongke@yahoo.com>
2016-09-13 00:53:52 -0700

    [92. Reverse Linked List II][Accepted]committed by Krunning

--- 92-Reverse-Linked-List-II/solution.java
+++ 92-Reverse-Linked-List-II/solution.java
@@ -8,26 +8,23 @@
  */
 public class Solution {
     public ListNode reverseBetween(ListNode head, int m, int n) {
-        if(head == null || head.next == null) return head;
-        ListNode dummy = new ListNode(-1);
+        ListNode dummy = new ListNode(0);
         dummy.next = head;
-        ListNode p = dummy;
+        ListNode pre = dummy;
+        
         for(int i = 1; i < m; i++){
-            p = p.next;
+            pre = pre.next;
         }
-        ListNode pre = p;
-        ListNode cur = p.next;
-        ListNode next = cur == null ? null : cur.next;
-        for(int i = 0; i < n - m; i++){
-            ListNode tmp = next.next;
-            next.next = cur;
+        ListNode p = pre;
+        ListNode cur = pre.next;
+        for(int i = 0; i <= n - m; i++){
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre =cur;
             cur = next;
-            next = tmp;
         }
-        if(p != null && p.next != null){
-            p.next.next = next;
-            p.next = cur;
-        }
+        p.next.next = cur;
+        p.next = pre;
         return dummy.next;
     }
 }
