d4e63622f87d39fc444868599cff2ee0a9fecaf1
krunning2 <zhidongke@yahoo.com>
2016-09-13 00:48:25 -0700

    [216. Combination Sum III][Accepted]committed by Krunning

--- 216-Combination-Sum-III/solution.java
+++ 216-Combination-Sum-III/solution.java
@@ -1,21 +1,21 @@
 public class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
-        List<List<Integer>> res  = new ArrayList<List<Integer>> ();
-        helper(n, k, res, new ArrayList(), 1, 0);
+        List<List<Integer>> res = new ArrayList<List<Integer>> ();
+        helper(res, new ArrayList<>(), k, 0, n, 1);
         return res;
     }
-    private void helper(int n, int k, List<List<Integer>> res, List<Integer> cur, int pos, int sum){
-        if(cur.size() == k && sum == n){
+    private void helper(List<List<Integer>> res, List<Integer> cur, int k, int sum, int target, int pos){
+        if(k == cur.size() && target == sum){
             res.add(new ArrayList<>(cur));
             return;
         }
         if(cur.size() >= k) return;
-        for(int i = pos; i <= 9; i ++){
-            cur.add(i);
+        for(int i = pos; i <= 9; i++){
             sum += i;
-            helper(n, k, res, cur, i + 1, sum);
+            cur.add(i);
+            helper(res, cur, k, sum, target, i + 1);
+            sum-=i;
             cur.remove(cur.size() - 1);
-            sum -= i;
         }
     }
 }
