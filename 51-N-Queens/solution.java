66486e889ca15e62c3b5505526902cfb9d861f77
krunning2 <zhidongke@yahoo.com>
2016-09-14 23:03:45 -0700

    [51. N-Queens][Accepted]committed by Krunning

--- 51-N-Queens/solution.java
+++ 51-N-Queens/solution.java
@@ -1,45 +1,39 @@
 public class Solution {
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> res = new ArrayList<List<String>>();
-        search(0, new int[n], n, res);
+        List<List<String>> res = new ArrayList<>();
+        DFS(0, n, res, new int[n]);
         return res;
     }
     
-    private void search(int row, int[] queen, int n, List<List<String>> res){
-        if(row == n) {
-            res.add(build(queen));
+    private void DFS(int curRow, int n, List<List<String>> res, int[] queenCol){
+        if(curRow == n){
+            List<String> tmp = new ArrayList<>();
+            for(int i = 0; i < n; i++){
+                StringBuilder sb = new StringBuilder();
+                for(int j = 0; j < n; j++){
+                    if(queenCol[i] == j){
+                        sb.append("Q");
+                    }else{
+                        sb.append(".");
+                    }
+                }
+                tmp.add(sb.toString());
+            }
+            res.add(tmp);
             return;
         }
-        for(int i = 0; i < n; i++){
-            if(isValid(row, i, queen)){
-                queen[row] = i;
-                search(row + 1, queen, n, res);
-                queen[row] = 0;
+        for(int j = 0; j < n; j++){
+            if(isValid(queenCol, curRow, j)){
+                queenCol[curRow] = j;
+                DFS(curRow + 1, n, res, queenCol);
             }
         }
     }
     
-    private List<String> build(int[] queen){
-        List<String> res = new ArrayList<String>();
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < queen.length; i++){
-            for(int j = 0; j < queen.length; j++){
-                if(queen[i] == j){
-                    sb.append("Q");
-                }else{
-                    sb.append(".");
-                }
-            }
-            res.add(sb.toString());
-            sb.setLength(0);
-        }
-        return res;
-    }
-    
-    private boolean isValid(int row, int col, int[] queen){
-        for(int preR = 0; preR < row; preR++){
-            int preC = queen[preR];
-            if(preC == col || Math.abs(preR - row) == Math.abs(preC - col)){
+    private boolean isValid(int[] queenCol, int row, int col){
+        for(int preRow = 0; preRow < row; preRow++){
+            int preCol = queenCol[preRow];
+            if(col == preCol || Math.abs(preRow - row) == Math.abs(preCol - col)){
                 return false;
             }
         }
