b03a845856c6174ddaac1d76f81b78c92899586a
krunning2 <zhidongke@yahoo.com>
2016-09-14 23:06:46 -0700

    [52. N-Queens II][Accepted]committed by Krunning

--- 52-N-Queens-II/solution.java
+++ 52-N-Queens-II/solution.java
@@ -1,27 +1,28 @@
 public class Solution {
-    int sum = 0;
     public int totalNQueens(int n) {
-        search(0, n, new int[n]);
-        return sum;
+        int[] res = {0};        
+        search(0, new int[n], n, res);
+        return res[0];
     }
     
-    private void search(int row, int n, int[] queen){
-        if(row == n){
-            sum ++;
+    private void search(int row, int[] queen, int n, int[] res){
+        if(row == n) {
+            res[0] ++;
             return;
         }
         for(int i = 0; i < n; i++){
             if(isValid(row, i, queen)){
                 queen[row] = i;
-                search(row + 1, n, queen);
+                search(row + 1, queen, n, res);
             }
         }
     }
-    
     private boolean isValid(int row, int col, int[] queen){
-        for(int i = 0; i < row; i++){
-            int preC = queen[i];
-            if(preC == col || Math.abs(preC - col) == Math.abs(i - row)) return false;
+        for(int preR = 0; preR < row; preR++){
+            int preC = queen[preR];
+            if(preC == col || Math.abs(preR - row) == Math.abs(preC - col)){
+                return false;
+            }
         }
         return true;
     }
