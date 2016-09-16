da6328e105fe9eb083cf4d458577e39dc2498ec3
krunning2 <zhidongke@yahoo.com>
2016-09-15 22:09:35 -0700

    [342. Power of Four][Accepted]committed by Krunning

--- 342-Power-of-Four/solution.java
+++ 342-Power-of-Four/solution.java
@@ -1,6 +1,5 @@
 public class Solution {
     public boolean isPowerOfFour(int num) {
-        if(num <= 0) return false;
-        return ((num &(num - 1)) == 0) && ((num & 0x55555555) == num);
+        return num > 0 && (num&(num - 1)) == 0 && (num - 1) % 3 == 0;
     }
 }
