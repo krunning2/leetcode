0cadca03149f57047ccb252cdceddd786370e665
krunning2 <zhidongke@yahoo.com>
2016-09-24 09:53:23 -0700

    [50. Pow(x, n)][Accepted]committed by Krunning

--- 50-Pow(x,-n)/solution.java
+++ 50-Pow(x,-n)/solution.java
@@ -1,17 +1,18 @@
 public class Solution {
-    public double myPow(double x, int n) {
-        long m = n;
-        if(m < 0){
-            m = -m;
-            x = 1/x;
+    public double myPow(double x, int m) {
+        long n = m;
+        if(n < 0){
+            x = 1 / x;
+            n *= -1;
         }
-        double res = 1;
         double f = x;
-        for(; m > 0; m = m >> 1){
-            if((m & 1) == 1){
+        double res = 1.0;
+        while(n > 0){
+            if((n & 1) == 1){
                 res *= f;
             }
             f *= f;
+            n = n >> 1;
         }
         return res;
     }
