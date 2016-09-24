c7f00c463fb044b4a029a22bde3b160bdb8f6771
krunning2 <zhidongke@yahoo.com>
2016-09-24 10:32:52 -0700

    [244. Shortest Word Distance II][Accepted]committed by Krunning

--- 244-Shortest-Word-Distance-II/solution.java
+++ 244-Shortest-Word-Distance-II/solution.java
@@ -1,31 +1,33 @@
 public class WordDistance {
-    Map<String, List<Integer>> map;
+    HashMap<String, List<Integer>> map;
     public WordDistance(String[] words) {
         map = new HashMap<>();
         for(int i = 0; i < words.length; i++){
-            if(!map.containsKey(words[i])){
-                map.put(words[i], new ArrayList<>());
-            }
+            map.putIfAbsent(words[i], new ArrayList<>());
             map.get(words[i]).add(i);
         }
     }
 
     public int shortest(String word1, String word2) {
-        List<Integer> l1 = map.get(word1);
-        List<Integer> l2 = map.get(word2);
-        int res = Integer.MAX_VALUE;
-        int p1 = 0, p2 = 0;
-        while(p1 < l1.size() && p2 < l2.size()){
-            if(l1.get(p1) == l2.get(p2)){
+        List<Integer> pos1 = map.get(word1);
+        List<Integer> pos2 = map.get(word2);
+        int p1 = 0, p2 = 0, res = Integer.MAX_VALUE;
+        while(p1 < pos1.size() && p2 < pos2.size()){
+            if(pos1.get(p1) == pos2.get(p2)){
                 return 0;
-            }else if(l1.get(p1) > l2.get(p2)){
-                res = Math.min(res, l1.get(p1) - l2.get(p2));
+            }else if(pos1.get(p1) > pos2.get(p2)){
+                res = Math.min(res, pos1.get(p1) - pos2.get(p2));
                 p2++;
             }else{
-                res = Math.min(res, l2.get(p2) - l1.get(p1));
+                res = Math.min(res, pos2.get(p2) - pos1.get(p1));
                 p1++;
             }
         }
         return res;
     }
 }
+
+// Your WordDistance object will be instantiated and called as such:
+// WordDistance wordDistance = new WordDistance(words);
+// wordDistance.shortest("word1", "word2");
+// wordDistance.shortest("anotherWord1", "anotherWord2");
