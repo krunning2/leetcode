eb9b7a44c1ec9d022a06d6693c956a43dee82ea5
krunning2 <zhidongke@yahoo.com>
2016-09-25 10:02:04 -0700

    [238. Product of Array Except Self][Accepted]committed by Krunning

--- 238-Product-of-Array-Except-Self/solution.java
+++ 238-Product-of-Array-Except-Self/solution.java
@@ -1,15 +1,16 @@
 public class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] res = new int[nums.length];
-        res[0] = 1;
+        if(nums == null || nums.length == 0) return nums;
+        int[] result = new int[nums.length];
+        result[0] = 1;
         for(int i = 1; i < nums.length; i++){
-            res[i] = res[i - 1] * nums[i - 1];
+            result[i] = result[i - 1] * nums[i - 1];
         }
-        int right = nums[nums.length - 1] ;
+        int fac = nums[nums.length - 1];
         for(int i = nums.length - 2; i >= 0; i--){
-            res[i] *= right;
-            right *= nums[i];
+            result[i] *= fac;
+            fac *= nums[i];
         }
-        return res;
+        return result;
     }
 }
