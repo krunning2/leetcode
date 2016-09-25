8347f294229a5fc8c8eef1e78e83ac945f7b8a03
krunning2 <zhidongke@yahoo.com>
2016-09-25 11:39:36 -0700

    [367. Valid Perfect Square][Accepted]committed by Krunning

--- 367-Valid-Perfect-Square/solution.java
+++ 367-Valid-Perfect-Square/solution.java
@@ -1,15 +1,15 @@
 public class Solution {
     public boolean isPerfectSquare(int num) {
-        long start = 1;
-        long end = num / 2 + 1;
+        if(num <= 0) return false;
+        long start = 1, end = num / 2 + 1;
         while(start + 1 < end){
             long mid = start - (start - end) / 2;
             if(mid * mid == num){
                 return true;
-            }else if(mid * mid < num){
-                start = mid;
-            }else{
+            }else if(mid * mid > num){
                 end = mid;
+            }else{
+                start = mid;
             }
         }
         return start * start == num || end * end == num;
