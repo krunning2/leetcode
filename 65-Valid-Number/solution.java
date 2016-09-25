24db0a3d83919df7710620bd6887413c830035ea
krunning2 <zhidongke@yahoo.com>
2016-09-25 16:09:25 -0700

    [65. Valid Number][Accepted]committed by Krunning

--- 65-Valid-Number/solution.java
+++ 65-Valid-Number/solution.java
@@ -1,30 +1,25 @@
 public class Solution {
     public boolean isNumber(String s) {
+        if(s == null || s.length() == 0) return false;
         s = s.trim();
         boolean seenNum = false;
-        boolean seenDot = false;
         boolean seenE = false;
+        boolean seenDot = false;
         boolean numAfterE = false;
         for(int i = 0; i < s.length(); i++){
             char cur = s.charAt(i);
-            if(cur <= '9' && cur >= '0'){
+            if(cur >= '0' && cur <= '9'){
                 seenNum = true;
                 numAfterE = true;
             }else if(cur == 'e'){
-                if(seenE || !seenNum){
-                    return false;
-                }
-                seenE = true;
+                if(seenE || !seenNum) return false;
                 numAfterE = false;
+                seenE = true;
             }else if(cur == '.'){
-                if(seenDot || seenE){
-                    return false;
-                }
+                if(seenE || seenDot) return false;
                 seenDot = true;
             }else if(cur == '+' || cur == '-'){
-                if(i != 0 && s.charAt(i - 1) != 'e'){
-                    return false;
-                }
+                if(i != 0 && s.charAt(i - 1) != 'e') return false;
             }else{
                 return false;
             }
