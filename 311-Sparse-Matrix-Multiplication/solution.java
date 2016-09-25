2ed2e2195482aaa22fa7376cad8f845c3b201d48
krunning2 <zhidongke@yahoo.com>
2016-09-25 11:08:36 -0700

    [311. Sparse Matrix Multiplication][Accepted]committed by Krunning

--- 311-Sparse-Matrix-Multiplication/solution.java
+++ 311-Sparse-Matrix-Multiplication/solution.java
@@ -1,18 +1,16 @@
 public class Solution {
     public int[][] multiply(int[][] A, int[][] B) {
-        int m = A.length;
-        int n = A[0].length;
-        int nb = B[0].length;
-        int[][] res = new int[m][nb];
+        int m = A.length, n = A[0].length;
+        int l = B[0].length;
+        int[][] result = new int[m][l];
         for(int i = 0; i < m; i++){
             for(int k = 0; k < n; k++){
-                if(A[i][k] != 0){
-                    for(int j = 0; j < nb; j++){
-                        res[i][j] += A[i][k] * B[k][j];
+                if(A[i][k] != 0)
+                    for(int j = 0; j < l; j++){
+                        result[i][j] += A[i][k] * B [k][j];
                     }
-                }
             }
         }
-        return res;
+        return result;
     }
 }
