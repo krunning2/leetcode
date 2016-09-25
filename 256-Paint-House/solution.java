c4db333f4761757c249d218fefe5ef7078d5f322
krunning2 <zhidongke@yahoo.com>
2016-09-25 15:59:07 -0700

    [256. Paint House][Accepted]committed by Krunning

--- 256-Paint-House/solution.java
+++ 256-Paint-House/solution.java
@@ -1,16 +1,19 @@
 public class Solution {
     public int minCost(int[][] costs) {
-        int lastGreen = 0;
-        int lastRed = 0;
-        int lastBlue = 0;
-        for(int i = 0; i < costs.length; i++){
-            int curGreen = lastGreen;
-            int curRed = lastRed;
-            int curBlue = lastBlue;
-            lastGreen = Math.min(curRed, curBlue) + costs[i][2];
-            lastBlue = Math.min(curRed, curGreen) + costs[i][1];
-            lastRed = Math.min(curGreen, curBlue) + costs[i][0]; 
+        if(costs == null || costs.length == 0){
+            return 0;
         }
-        return Math.min(Math.min(lastGreen, lastBlue), lastRed);
+        int red = costs[0][0];
+        int blue = costs[0][1];
+        int green = costs[0][2];
+        for(int i = 1; i < costs.length; i++){
+            int curRed = Math.min(blue, green) + costs[i][0];
+            int curBlue = Math.min(red, green) + costs[i][1];
+            int curGreen = Math.min(blue, red) + costs[i][2];
+            red = curRed;
+            blue = curBlue;
+            green = curGreen;
+        }
+        return Math.min(Math.min(red, green), blue);
     }
 }
