d3a49322f2b297492026440f6dd8ec08130c9218
krunning2 <zhidongke@yahoo.com>
2016-09-25 11:16:53 -0700

    [198. House Robber][Accepted]committed by Krunning

--- 198-House-Robber/solution.java
+++ 198-House-Robber/solution.java
@@ -1,13 +1,13 @@
 public class Solution {
     public int rob(int[] nums) {
         if(nums == null || nums.length == 0) return 0;
-        int[] dp = new int[nums.length];
+        int[] dp = new int[2];
         dp[0] = nums[0];
         if(nums.length == 1) return dp[0];
         dp[1] = Math.max(nums[0], nums[1]);
         for(int i = 2; i < nums.length; i++){
-            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+            dp[i % 2] = Math.max(nums[i] + dp[(i - 2) % 2], dp[(i - 1) % 2]);
         }
-        return dp[nums.length - 1];
+        return dp[(nums.length - 1) % 2];
     }
 }
