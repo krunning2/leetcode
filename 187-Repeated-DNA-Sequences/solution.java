bdb01b361e7812612086e52ccf3424ae8baf1c14
krunning2 <zhidongke@yahoo.com>
2016-09-24 22:25:46 -0700

    [187. Repeated DNA Sequences][Accepted]committed by Krunning

--- 187-Repeated-DNA-Sequences/solution.java
+++ 187-Repeated-DNA-Sequences/solution.java
@@ -1,24 +1,27 @@
 public class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        int[] map = new int[26];
-        map['C' - 'A'] = 1;
-        map['G' - 'A'] = 2;
-        map['T' - 'A'] = 3;
+        List<String> res = new ArrayList<String>();
+        if(s == null) return res;
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('A', 0);
+        map.put('C', 1);
+        map.put('G', 2);
+        map.put('T', 3);
         Map<Integer, Integer> counter = new HashMap<>();
-        List<String> res = new ArrayList<>();
         for(int i = 0; i < s.length() - 9; i++){
             int v = 0;
-            for(int j = i; j < i + 10; j++){
+            for(int j = i; j < 10 + i; j++){
+                v |= map.get(s.charAt(j));
                 v = v << 2;
-                v |= map[s.charAt(j) - 'A'];
             }
             if(counter.containsKey(v) && counter.get(v) == 1){
-                counter.put(v, 2);
                 res.add(s.substring(i, i + 10));
+                counter.put(v, 2);
             }else if(!counter.containsKey(v)){
                 counter.put(v, 1);
             }
         }
         return res;
     }
+    
 }
