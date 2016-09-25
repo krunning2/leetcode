784d703b36ed35af9bd687862aa6082e741b89e6
krunning2 <zhidongke@yahoo.com>
2016-09-25 10:04:57 -0700

    [101. Symmetric Tree][Accepted]committed by Krunning

--- 101-Symmetric-Tree/solution.java
+++ 101-Symmetric-Tree/solution.java
@@ -10,11 +10,11 @@
 public class Solution {
     public boolean isSymmetric(TreeNode root) {
         if(root == null) return true;
-        return isValid(root.left, root.right);
+        return helper(root.left, root.right);
     }
-    private boolean isValid(TreeNode left, TreeNode right){
-        if(left == null || right == null) return left == null && right == null;
+    private boolean helper(TreeNode left, TreeNode right){
+        if(left == null || right == null) return right == null && left == null;
         if(left.val != right.val) return false;
-        return isValid(left.right, right.left) && isValid(left.left, right.right);
+        return helper(left.left, right.right) && helper(left.right, right.left);
     }
 }
