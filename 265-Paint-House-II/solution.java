c671c03d48185b4ceeb9e7d31af5eefa7d1f0ede
krunning2 <zhidongke@yahoo.com>
2016-09-25 17:48:43 -0700

    [265. Paint House II][Accepted]committed by Krunning

--- 265-Paint-House-II/solution.java
+++ 265-Paint-House-II/solution.java
@@ -1,20 +1,19 @@
 public class Solution {
     public int minCostII(int[][] costs) {
-        if(costs == null || costs.length == 0) return 0;
-        int n = costs.length;
-        int k = costs[0].length;
-        int[][] minCosts = new int[2][k];
-        int res = Integer.MAX_VALUE;
-        
-        for(int i = 0; i < k; i++){
-            minCosts[0][i] = costs[0][i];
+        if(costs == null || costs.length == 0){
+            return 0;
         }
-        
+        int k = costs[0].length;
+        int[][] minCost = new int[2][k];
+        int res = Integer.MAX_VALUE;
+        for(int i = 0; i < k; i++){
+            minCost[0][i] = costs[0][i];
+        }
         for(int i = 1; i < costs.length; i++){
             int min1 = Integer.MAX_VALUE;
             int min2 = Integer.MAX_VALUE;
             for(int j = 0; j < k; j++){
-                int cur = minCosts[(i - 1) % 2][j];
+                int cur = minCost[(i - 1) % 2][j];
                 if(min1 > cur){
                     min2 = min1;
                     min1 = cur;
@@ -23,11 +22,11 @@ public class Solution {
                 }
             }
             for(int j = 0; j < k; j++){
-                minCosts[i % 2][j] = min1 == minCosts[(i - 1) % 2][j] ? min2 + costs[i][j] : min1 + costs[i][j];
+                minCost[i % 2][j] = min1 == minCost[(i - 1) % 2][j] ? costs[i][j] + min2 : costs[i][j] + min1;
             }
         }
-        for(int i = 0; i < k; i++){
-            res = Math.min(res, minCosts[(n - 1) % 2][i]);
+        for(int j = 0; j < k; j++){
+            res = Math.min(res, minCost[(costs.length - 1) % 2][j]);
         }
         return res;
     }
