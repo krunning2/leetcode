f28cc2380c46345edcbeea42ad03592dc75c3372
krunning2 <zhidongke@yahoo.com>
2016-10-02 10:24:39 -0700

    [177. Nth Highest Salary][Accepted]committed by Krunning

--- 177-Nth-Highest-Salary/solution.sql
+++ 177-Nth-Highest-Salary/solution.sql
@@ -3,6 +3,6 @@ BEGIN
     set N=N-1;
   RETURN (
       # Write your MySQL query statement below.
-      select distinct Salary from Employee order by Salary desc limit 1 offset N
+      select distinct Salary from Employee order by Salary desc limit N,1
   );
 END
