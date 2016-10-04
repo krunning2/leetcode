55b2ce71f7d3598a90c637d251fcf38c1f547d38
krunning2 <zhidongke@yahoo.com>
2016-10-04 08:26:45 -0700

    [229. Majority Element II][Accepted]committed by Krunning

--- 229-Majority-Element-II/solution.java
+++ 229-Majority-Element-II/solution.java
@@ -5,6 +5,7 @@ public class Solution {
         int count1 = 0;
         int count2 = 0;
         for(int n : nums){
+            // we need to first check the "candidate1 == n" then "check count2 == 0", since it can be "88777", so can1 == 8, but count == 0, so can2 = 8 
             if(candidate1 == n){
                 count1++;
             }else if(candidate2 == n){
