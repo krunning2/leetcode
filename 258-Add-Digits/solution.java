ae1c7339ae9aad9b1b211b0682e1dad9312c5475
krunning2 <zhidongke@yahoo.com>
2016-10-04 09:22:37 -0700

    [258. Add Digits][Accepted]committed by Krunning

--- 258-Add-Digits/solution.java
+++ 258-Add-Digits/solution.java
@@ -1,13 +1,7 @@
 public class Solution {
     public int addDigits(int num) {
-        while(num >= 10){
-            int sum = 0;
-            while(num > 0){
-                sum += num % 10;
-                num /= 10;
-            }
-            num = sum;
-        }
-        return num;
+        if(num == 0) return 0;
+        if(num % 9 == 0) return 9;
+        return (num % 9);
     }
 }
