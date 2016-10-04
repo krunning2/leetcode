0246f889d17ea86d86307e529fd0b821e6707922
krunning2 <zhidongke@yahoo.com>
2016-10-04 08:21:19 -0700

    [229. Majority Element II][Accepted]committed by Krunning

--- 229-Majority-Element-II/solution.java
+++ 229-Majority-Element-II/solution.java
@@ -1,37 +1,39 @@
 public class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> result = new ArrayList<>();
         int candidate1 = -1;
         int candidate2 = -1;
         int count1 = 0;
         int count2 = 0;
         for(int n : nums){
-            if(n == candidate1){
+            if(candidate1 == n){
                 count1++;
-            }else if(n == candidate2){
+            }else if(candidate2 == n){
                 count2++;
             }else if(count1 == 0){
                 candidate1 = n;
-                count1 = 1;
+                count1 ++;
             }else if(count2 == 0){
                 candidate2 = n;
-                count2 = 1;
+                count2++;
             }else{
                 count1--;
                 count2--;
             }
         }
-        count2 = 0;
         count1 = 0;
+        count2 = 0;
         for(int n : nums){
-            if(n == candidate1){
+            if(candidate1 == n){
                 count1++;
-            }else if(n == candidate2){
+            }else if(candidate2 == n){
                 count2++;
             }
         }
-        if(count1 > nums.length / 3) result.add(candidate1);
-        if(count2 > nums.length / 3) result.add(candidate2);
-        return result;
+        List<Integer> res = new ArrayList<>();
+        if(count1 > nums.length / 3)
+            res.add(candidate1);
+        if(count2 > nums.length / 3)
+            res.add(candidate2);
+        return res;
     }
 }
