06f210ef69bff8c6634ee6049a631ee85be73e68
krunning2 <zhidongke@yahoo.com>
2016-10-04 08:09:13 -0700

    [169. Majority Element][Accepted]committed by Krunning

--- 169-Majority-Element/solution.java
+++ 169-Majority-Element/solution.java
@@ -1,15 +1,12 @@
 public class Solution {
     public int majorityElement(int[] nums) {
-        if(nums == null || nums.length == 0){
-            return -1;
-        }
-        int candidate = nums[0];
         int count = 1;
-        for(int i = 1; i< nums.length; i++){
-            if(candidate == nums[i]){
-                count ++;
-            }else{
+        int candidate = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            if(candidate != nums[i]){
                 count--;
+            }else{
+                count++;
             }
             if(count == 0){
                 candidate = nums[i];
