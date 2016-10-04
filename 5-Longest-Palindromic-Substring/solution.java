9677745af439200217aa47a72ac8e77e84c3a48b
krunning2 <zhidongke@yahoo.com>
2016-10-04 08:03:25 -0700

    [5. Longest Palindromic Substring][Accepted]committed by Krunning

--- 5-Longest-Palindromic-Substring/solution.java
+++ 5-Longest-Palindromic-Substring/solution.java
@@ -1,18 +1,16 @@
 public class Solution {
     public String longestPalindrome(String s) {
         if(s == null || s.length() <= 1) return s;
-        int size = s.length();
-        boolean[][] map = new boolean[size][size];
-        for(int i = 0; i < size; i++){
-            map[i][i] = true;
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        for(int i = 0; i < s.length(); i++){
+            dp[i][i] = true;
         }
-        int max = 1;
         String res = "";
-        // len 表示加上的长度，并非总长度
-        for(int len = 1; len < size; len ++){
-            for(int start = 0; start + len < size; start++){
-                if(s.charAt(start) == s.charAt(start + len) && (map[start + 1][start + len - 1] || len == 1)){
-                    map[start][start + len] = true;
+        int max = 0;
+        for(int len = 1; len < s.length(); len++){
+            for(int start = 0; start + len < s.length(); start++){
+                if(s.charAt(start) == s.charAt(start + len) &&( len == 1 || dp[start + 1][start + len - 1])){
+                    dp[start][start + len] = true;
                     if(max < len + 1){
                         max = len + 1;
                         res = s.substring(start, start + len + 1);
