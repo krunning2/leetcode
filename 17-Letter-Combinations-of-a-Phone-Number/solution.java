f2d7899ad10c6ed4412811209ee79609430df404
krunning2 <zhidongke@yahoo.com>
2016-10-08 10:12:52 -0700

    [17. Letter Combinations of a Phone Number][Accepted]committed by Krunning

--- 17-Letter-Combinations-of-a-Phone-Number/solution.java
+++ 17-Letter-Combinations-of-a-Phone-Number/solution.java
@@ -1,23 +1,21 @@
 public class Solution {
     public List<String> letterCombinations(String digits) {
         String[] letters = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        List<String> res = new ArrayList<String>();
-        if(digits == null || digits.length() == 0){
-            return res;
-        }
-        DFS(digits, 0, new StringBuilder(), res, letters);
+        List<String> res = new ArrayList<>();
+        if(digits == null || digits.length() == 0) return res;
+        DFS(0, digits, letters, new StringBuilder(), res);
         return res;
     }
-    private void DFS(String digits, int pos, StringBuilder sb, List<String> res, String[] letters){
-        if(pos == digits.length()){
+    private void DFS(int pos, String digits, String[] letters, StringBuilder sb, List<String> res){
+        if(sb.length() == digits.length()){
             res.add(sb.toString());
             return;
         }
-        String s = letters[digits.charAt(pos) - '0'];
-        for(int i = 0; i < s.length(); i++){
+        int num = digits.charAt(pos) - '0';
+        for(int i = 0; i < letters[num].length(); i++){
             int len = sb.length();
-            sb.append(s.charAt(i));
-            DFS(digits, pos + 1, sb, res, letters);
+            sb.append(letters[num].charAt(i));
+            DFS(pos + 1, digits, letters, sb, res);
             sb.setLength(len);
         }
     }
