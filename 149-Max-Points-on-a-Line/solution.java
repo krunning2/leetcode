d384d9e4359470ad758af56855c3a2378dd28e21
krunning2 <zhidongke@yahoo.com>
2016-10-08 08:57:50 -0700

    [149. Max Points on a Line][Accepted]committed by Krunning

--- 149-Max-Points-on-a-Line/solution.java
+++ 149-Max-Points-on-a-Line/solution.java
@@ -9,37 +9,33 @@
  */
 public class Solution {
     public int maxPoints(Point[] points) {
-        if(points == null || points.length == 0){
-            return 0;
-        }
         Map<Long, Integer> map = new HashMap<>();
-        int res = 1;
+        int max = 0;
         for(int i = 0; i < points.length; i++){
             int same = 0;
             int localMax = 1;
             map.clear();
             for(int j = i + 1; j < points.length; j++){
-                int x = points[i].x - points[j].x;
-                int y = points[i].y - points[j].y;
-                if(x == 0 && y == 0){
+                Point cur = points[i];
+                Point next = points[j];
+                if(cur.x == next.x && cur.y == next.y){
                     same++;
-                    continue;
-                }
-                int gcd = getGCD(x, y);
-                x /= gcd;
-                y /= gcd;
-                long slope =( (long)x << 32 | y );
-                
-                if(map.containsKey(slope)){
-                    map.put(slope, map.get(slope) + 1);
                 }else{
-                    map.put(slope, 2);
+                    int gcd = getGCD(cur.y - next.y, cur.x - next.x);
+                    int x = (cur.x - next.x) / gcd;
+                    int y = (cur.y - next.y) / gcd;
+                    long key = ((long)x) << 32 | y;
+                    if(map.containsKey(key)){
+                        map.put(key, map.get(key) + 1);
+                    }else{
+                        map.put(key, 2);
+                    }
+                    localMax = Math.max(localMax, map.get(key));
                 }
-                localMax = Math.max(localMax, map.get(slope));
             }
-            res = Math.max(res, localMax + same);
+            max = Math.max(max, same + localMax);
         }
-        return res;
+        return max;
     }
     private int getGCD(int a, int b){
         return b == 0 ? a : getGCD(b, a % b);
