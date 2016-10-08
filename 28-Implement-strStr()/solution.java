c6cf4a12353b0862e3cf6aa8d7a1325a26ef24d3
krunning2 <zhidongke@yahoo.com>
2016-10-08 10:55:46 -0700

    [28. Implement strStr()][Accepted]committed by Krunning

--- 28-Implement-strStr()/solution.java
+++ 28-Implement-strStr()/solution.java
@@ -1,35 +1,19 @@
 public class Solution {
-    public int strStr(String s, String p) {
-        if(s == null && p != null || s.length() == 0 && p.length() != 0) return -1;
-        if(p == null || p.length() == 0) return 0;
-        int[] next = getNext(p);
-        int i = 0, j = 0;
-        while(i < s.length()){
-            if(j== -1 || p.charAt(j) == s.charAt(i)){
-                j++;
-                i++;
-                if(j == p.length()){
-                    return i - j;
+    public int strStr(String haystack, String needle) {
+        if((haystack == null || haystack.length() == 0) && needle != null && needle.length() != 0) return -1;
+        if(needle == null || needle.length() == 0) return 0;
+        int len1 = haystack.length();
+        int len2 = needle.length();
+        for(int i = 0; i <= len1 - len2 ; i++){
+            int j = 0;
+            while(j < len2){
+                if(haystack.charAt(i + j) != needle.charAt(j)){
+                    break;
                 }
-            }else{
-                j = next[j];
+                j++;
+                if(j == len2) return i;
             }
         }
         return -1;
-    }
-    private int[] getNext(String p){
-        int[] next = new int[p.length() + 1];
-        int i = 0, j = -1;
-        next[0] = -1;
-        while(i < p.length()){
-            if(j == -1 || p.charAt(i) == p.charAt(j)){
-                j++;
-                i++;
-                next[i] = j;
-            }else{
-                j = next[j];
-            }
-        }
-        return next;
     }
 }
