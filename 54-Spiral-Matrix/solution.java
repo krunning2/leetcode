70d1cfd3f6f1b931b988d4152eb7e11c2b6b781e
krunning2 <zhidongke@yahoo.com>
2016-10-10 00:16:21 -0700

    [54. Spiral Matrix][Accepted]committed by Krunning

--- 54-Spiral-Matrix/solution.java
+++ 54-Spiral-Matrix/solution.java
@@ -1,14 +1,15 @@
 public class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        if(matrix== null || matrix.length == 0) return res;
         int start = 0;
-        List<Integer> res = new ArrayList<Integer>();
-        while(2 * start < matrix.length && 2 * start < matrix[0].length){
-            helper(matrix, start, res);
-            start++;
+        while(start * 2 < matrix.length && start * 2 < matrix[0].length){
+            get(matrix, start, res);
+            start ++;
         }
         return res;
     }
-    private void helper(int[][] matrix, int start, List<Integer> res){
+    private void get(int[][] matrix, int start, List<Integer> res){
         int endRow = matrix.length - start - 1;
         int endCol = matrix[0].length - start - 1;
         for(int i = start; i <= endCol; i++){
@@ -17,14 +18,15 @@ public class Solution {
         for(int i = start + 1; i <= endRow; i++){
             res.add(matrix[i][endCol]);
         }
-        if(endRow > start)
-            for(int i = endCol - 1; i >= start; i --){
+        if(endRow > start){
+            for(int i = endCol - 1; i >= start; i--){
                 res.add(matrix[endRow][i]);
             }
-        if(endCol > start)
+        }
+        if(endCol > start){
             for(int i = endRow - 1; i > start; i--){
                 res.add(matrix[i][start]);
             }
-            
+        }
     }
 }
