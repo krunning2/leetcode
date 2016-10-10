95d8bf16cc7ecaad3adf3ecd05c6d6c2536c27f0
krunning2 <zhidongke@yahoo.com>
2016-10-09 22:50:00 -0700

    [79. Word Search][Accepted]committed by Krunning

--- 79-Word-Search/solution.java
+++ 79-Word-Search/solution.java
@@ -1,30 +1,31 @@
 public class Solution {
     public boolean exist(char[][] board, String word) {
-        if(word == null || word.length() == 0) return false;
-        int m = board.length;
-        int n = board[0].length;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(board[i][j] == word.charAt(0) && DFS(board, word, new boolean[m][n], i, j, 0)){
-                    return true;
+        if(board == null || word == null || word.length() == 0) return true;
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(board[i][j] == word.charAt(0)){
+                    if(DFS(board, word, new boolean[board.length][board[0].length], 0, i, j)){
+                        return true;
+                    }
                 }
             }
         }
         return false;
     }
-    
-    private boolean DFS(char[][] board, String word, boolean[][] visited, int x, int y, int p){
-        if(p == word.length()) return true;
-        if(x < 0 || y < 0 || x >= board.length || y >= board[0].length || visited[x][y] || word.charAt(p) != board[x][y]){
+    private boolean DFS(char[][] board, String word, boolean[][] visited, int pos, int x, int y){
+        if(pos == word.length()) return true;
+        if(x < 0 ||  y < 0 || x >= board.length || y >= board[0].length || visited[x][y] ||board[x][y] != word.charAt(pos)){
             return false;
         }
         visited[x][y] = true;
-        boolean res = 
-           DFS(board, word, visited, x + 1, y, p + 1)
-        || DFS(board, word, visited, x - 1, y, p + 1)
-        || DFS(board, word, visited, x, y + 1, p + 1)
-        || DFS(board, word, visited, x, y - 1, p + 1);
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
+        for(int i = 0; i < 4; i++){
+            if(DFS(board, word, visited, pos + 1, x + dx[i], y + dy[i])){
+                return true;
+            }
+        }
         visited[x][y] = false;
-        return res;
+        return false;
     }
 }
