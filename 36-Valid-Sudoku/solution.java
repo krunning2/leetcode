cb5fb0ad332267c46cd4c0138f7c28a57b30fad0
krunning2 <zhidongke@yahoo.com>
2016-10-10 22:45:57 -0700

    [36. Valid Sudoku][Accepted]committed by Krunning

--- 36-Valid-Sudoku/solution.java
+++ 36-Valid-Sudoku/solution.java
@@ -1,25 +1,28 @@
 public class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean[] visited = new boolean[9];
-        for(int i = 0; i < board.length; i++){
+        if(board == null || board.length != 9 || board[0].length != 9) return false;
+        // for each row
+        boolean[] visited = new boolean[10];
+        for(int i = 0; i < 9; i++){
             Arrays.fill(visited, false);
-            for(int j = 0; j < board[0].length; j++){
+            for(int j = 0; j < 9; j++){
                 if(!validate(visited, board[i][j])) return false;
             }
         }
-        for(int i = 0; i < board.length; i++){
+        // for each col
+        
+        for(int i = 0; i < 9; i++){
             Arrays.fill(visited, false);
-            for(int j = 0; j < board[0].length; j++){
+            for(int j = 0; j < 9; j++){
                 if(!validate(visited, board[j][i])) return false;
             }
         }
-        for(int i = 0; i < board.length; i += 3){
-            for(int j = 0; j < board[0].length; j += 3){
+        
+        for(int i = 0; i < 9; i += 3){
+            for(int j = 0; j < 9; j += 3){
                 Arrays.fill(visited, false);
                 for(int k = 0; k < 9; k++){
-                    if(!validate(visited, board[i + k / 3][j + k % 3])){
-                        return false;
-                    }
+                    if(!validate(visited, board[i + k / 3][j + k % 3])) return false;
                 }
             }
         }
