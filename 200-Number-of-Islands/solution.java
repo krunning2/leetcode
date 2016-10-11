a131fee12e85629e8d8d362193dac4acc13c4f8f
krunning2 <zhidongke@yahoo.com>
2016-10-10 22:30:59 -0700

    [200. Number of Islands][Accepted]committed by Krunning

--- 200-Number-of-Islands/solution.java
+++ 200-Number-of-Islands/solution.java
@@ -1,31 +1,27 @@
 public class Solution {
     public int numIslands(char[][] grid) {
-        if(grid == null || grid.length == 0 || grid[0].length == 0){
-            return 0;
-        }
-        int m = grid.length;
-        int n = grid[0].length;
+        if(grid == null || grid.length == 0 || grid[0].length == 0) return 0;
         int count = 0;
-        boolean[][] visited = new boolean[m][n];
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
                 if(grid[i][j] == '1' && !visited[i][j]){
                     count++;
-                    DFS(grid, visited, i, j);
+                    DFS(grid, i, j, visited);
                 }
             }
         }
         return count;
     }
-    private void DFS(char[][] grid, boolean[][] visited, int row, int col){
-        if(row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || visited[row][col] ||grid[row][col] == '0'){
+    private void DFS(char[][] grid, int x, int y, boolean[][] visited){
+        if(x < 0 || y < 0 || y >= grid[0].length || x >= grid.length || visited[x][y] || grid[x][y] != '1'){
             return;
         }
-        visited[row][col] = true;
-        int[] dx = {0, 0, -1, 1};
-        int[] dy = {1, -1, 0, 0};
+        visited[x][y] = true;
+        int[] dx = {1,-1,0,0};
+        int[] dy = {0,0,1,-1};
         for(int i = 0; i < 4; i++){
-            DFS(grid, visited, row + dx[i], col + dy[i]);
+            DFS(grid, x + dx[i], y + dy[i], visited);
         }
     }
 }
