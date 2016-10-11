6f263b119342e38543e83382365794382eb13530
krunning2 <zhidongke@yahoo.com>
2016-10-10 20:42:02 -0700

    [217. Contains Duplicate][Accepted]committed by Krunning

--- 217-Contains-Duplicate/solution.java
+++ 217-Contains-Duplicate/solution.java
@@ -1,10 +1,8 @@
 public class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<Integer>();
+        Set<Integer> set = new HashSet<>();
         for(int n : nums){
-            if(set.contains(n)){
-                return true;
-            }
+            if(set.contains(n)) return true;
             set.add(n);
         }
         return false;
