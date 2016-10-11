eb8204769a5dd1d8b465e7886ea6efd66e3ced82
krunning2 <zhidongke@yahoo.com>
2016-10-10 21:14:55 -0700

    [220. Contains Duplicate III][Accepted]committed by Krunning

--- 220-Contains-Duplicate-III/solution.java
+++ 220-Contains-Duplicate-III/solution.java
@@ -3,13 +3,16 @@ public class Solution {
         if(nums == null || nums.length < 2 || t < 0 || k < 1) return false;
         TreeSet<Integer> set = new TreeSet<>();
         for(int i = 0; i < nums.length; i++){
-            Integer ceiling = set.ceiling(nums[i]);
-            Integer floor = set.floor(nums[i]);
-            if(floor != null && nums[i] <= t + floor || ceiling != null && ceiling - nums[i] <= t){
+            int n = nums[i];
+            Integer ceiling = set.ceiling(n);
+            Integer floor = set.floor(n);
+            if(ceiling != null && ceiling - n <= t || floor != null && n <= t + floor){
                 return true;
             }
-            set.add(nums[i]);
-            if(i >= k) set.remove(nums[i - k]);
+            set.add(n);
+            if(i >= k){
+                set.remove(nums[i - k]);
+            }
         }
         return false;
     }
