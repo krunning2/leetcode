a35413d0e2f8d03bed19c748a27d350d0d6d5286
krunning2 <zhidongke@yahoo.com>
2016-10-11 22:43:26 -0700

    [72. Edit Distance][Accepted]committed by Krunning

--- 72-Edit-Distance/solution.java
+++ 72-Edit-Distance/solution.java
@@ -2,22 +2,20 @@ public class Solution {
     public int minDistance(String word1, String word2) {
         int m = word1.length();
         int n = word2.length();
-        int[][] dp = new int[m + 1][n + 1];
-        for(int i = 0; i <= m; i++){
-            dp[i][0] = i;
-        }
+        int[][] dp = new int[2][n + 1];
         for(int i = 0; i <= n; i++){
             dp[0][i] = i;
         }
         for(int i = 1; i <= m; i++){
+            dp[i % 2][0] = i;
             for(int j = 1; j <= n; j++){
                 if(word1.charAt(i - 1) == word2.charAt(j - 1)){
-                    dp[i][j] = dp[i - 1][j - 1];
+                    dp[i % 2][j] = dp[(i - 1) % 2][j - 1];
                 }else{
-                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+                    dp[i % 2][j] = Math.min(Math.min(dp[i % 2][j - 1], dp[(i - 1) % 2][j]), dp[(i - 1) % 2][j - 1]) + 1;
                 }
             }
         }
-        return dp[m][n];
+        return dp[m % 2][n];
     }
 }
