cc0ebb36de64b45c7ad2139ddb8064fc5afb6f52
krunning2 <zhidongke@yahoo.com>
2016-10-11 23:24:59 -0700

    [46. Permutations][Accepted]committed by Krunning

--- 46-Permutations/solution.java
+++ 46-Permutations/solution.java
@@ -1,23 +1,25 @@
 public class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        if(nums == null) return res;
-        DFS(nums, new ArrayList<>(), res, new boolean[nums.length]);
+        if(nums == null || nums.length == 0) return res;
+        DFS(nums, new boolean[nums.length], res, new ArrayList<>());
         return res;
     }
-    private void DFS(int[] nums, List<Integer> cur, List<List<Integer>> res, boolean[] visited){
+    
+    private void DFS(int[] nums, boolean[] visited, List<List<Integer>> res, List<Integer> cur){
         if(cur.size() == nums.length){
             res.add(new ArrayList<>(cur));
             return;
         }
         for(int i = 0; i < nums.length; i++){
-            if(!visited[i]){
-                visited[i] = true;
-                cur.add(nums[i]);
-                DFS(nums, cur, res, visited);
-                visited[i] = false;
-                cur.remove(cur.size() - 1);
+            if(visited[i]){
+               continue; 
             }
+            visited[i] = true;
+            cur.add(nums[i]);
+            DFS(nums, visited, res, cur);
+            visited[i] = false;
+            cur.remove(cur.size() - 1);
         }
     }
 }
