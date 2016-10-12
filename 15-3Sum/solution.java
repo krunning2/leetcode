596029652e3fa8aaa1d1d06533601966cead2cbb
krunning2 <zhidongke@yahoo.com>
2016-10-11 23:11:09 -0700

    [15. 3Sum][Accepted]committed by Krunning

--- 15-3Sum/solution.java
+++ 15-3Sum/solution.java
@@ -1,25 +1,23 @@
 public class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(nums == null || nums.length == 0){
+            return res;
+        }
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
         for(int i = 0; i < nums.length - 2; i++){
             int cur = nums[i];
-            int start = i + 1;
-            int end = nums.length - 1;
+            int start = i + 1, end = nums.length - 1;
             while(start < end){
                 if(nums[start] + nums[end] + cur == 0){
-                    List<Integer> tmp = new ArrayList<>();
-                    tmp.add(cur);
-                    tmp.add(nums[start]);
-                    tmp.add(nums[end]);
-                    res.add(tmp);
-                    start++;
-                    end--;
+                    res.add(Arrays.asList(new Integer[]{cur, nums[start], nums[end]}));
+                    start ++;
+                    end --;
                     while(start < end && nums[start] == nums[start - 1]){
                         start++;
                     }
                     while(start < end && nums[end] == nums[end + 1]){
-                        end--;
+                        end --;
                     }
                 }else if(nums[start] + nums[end] + cur > 0){
                     end--;
