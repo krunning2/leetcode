668a273044267102d77b76259d2ccc73255ee4f7
krunning2 <zhidongke@yahoo.com>
2016-10-12 23:57:58 -0700

    [112. Path Sum][Accepted]committed by Krunning

--- 112-Path-Sum/solution.java
+++ 112-Path-Sum/solution.java
@@ -10,9 +10,7 @@
 public class Solution {
     public boolean hasPathSum(TreeNode root, int sum) {
         if(root == null) return false;
-        if(root.left == null && root.right == null && sum == root.val){
-            return true;
-        }
-        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum -root.val);
+        if(root.val == sum && root.left == null && root.right == null) return true;
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
     }
 }
