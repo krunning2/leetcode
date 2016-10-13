d35bf273f37ee6a3fdf0e755fb8e8557628d6293
krunning2 <zhidongke@yahoo.com>
2016-10-13 13:06:49 -0700

    [58. Length of Last Word][Accepted]committed by Krunning

--- 58-Length-of-Last-Word/solution.java
+++ 58-Length-of-Last-Word/solution.java
@@ -1,18 +1,15 @@
 public class Solution {
     public int lengthOfLastWord(String s) {
         if(s == null || s.length() == 0) return 0;
-        int len = 0;
-        s = s.trim();
-        int p = s.length() - 1;
-        while(p >= 0){
-            if(s.charAt(p) == ' '){
-                return len;
-            }else{
-                len++;
-            }
-            p--;
+        int len = s.length() - 1;
+        while(len >= 0 && s.charAt(len) == ' '){
+            len--;
         }
-        if(p == -1) return s.length();
-        return len;
+        int lastWordLen = 0;
+        while(len >= 0 && s.charAt(len) != ' '){
+            len --;
+            lastWordLen ++;
+        }
+        return lastWordLen;
     }
 }
