d6a70e9dd5b2126adf71b8a5ed791892aa4be479
krunning2 <zhidongke@yahoo.com>
2016-10-13 11:46:45 -0700

    [43. Multiply Strings][Accepted]committed by Krunning

--- 43-Multiply-Strings/solution.java
+++ 43-Multiply-Strings/solution.java
@@ -1,33 +1,37 @@
 public class Solution {
     public String multiply(String num1, String num2) {
-        if(num1 == null || num2 == null || num1.length() == 0 || num2.length() == 0) return "";
+        if(num1 == null || num2 == null || num1.length() == 0 || num2.length() == 0){
+            return "";
+        }
         int sign = 1;
-        
-        if(num1.charAt(0) == '-'){
+        if(num1.startsWith("-")){
             sign *= -1;
             num1 = num1.substring(1);
         }
-        if(num2.charAt(0) == '-'){
+        if(num2.startsWith("-")){
             sign *= -1;
-            num2 = num2.substring(1);
+            num2.substring(1);
         }
-        int [] res = new int[num1.length() + num2.length()];
-        for(int i = num1.length() - 1; i >= 0; i--){
-            for(int j = num2.length() - 1; j >= 0; j--){
+        int len1 = num1.length();
+        int len2 = num2.length();
+        int[] res = new int[len1 + len2];
+        for(int i = len1 - 1 ; i >= 0; i--){
+            for(int j = len2 - 1; j >= 0; j--){
                 int p1 = i + j;
                 int p2 = i + j + 1;
-                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
-                int sum = mul + res[p2];
-                res[p2] = sum % 10;
-                res[p1] += sum / 10;
+                res[p2] += (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                res[p1] += res[p2] / 10;
+                res[p2] %= 10;
             }
         }
         StringBuilder sb = new StringBuilder();
-        for(int r : res){
-            if(r == 0 && sb.length() == 0) continue;
-            sb.append(r);
+        for(int i = 0; i < len1 + len2; i++){
+            if(sb.length() == 0 && res[i] == 0) continue;
+            sb.append(res[i]);
         }
-        if(sign == -1) sb.insert(0, "-");
+        if(sign == -1) {
+            sb.insert(0, "-");
+        }
         return sb.length() == 0 ? "0" : sb.toString();
     }
 }
