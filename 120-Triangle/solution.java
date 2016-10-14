1aa9270fabf41f74698fdd37a0ff3a379b32b1cb
krunning2 <zhidongke@yahoo.com>
2016-10-13 22:34:49 -0700

    [120. Triangle][Accepted]committed by Krunning

--- 120-Triangle/solution.java
+++ 120-Triangle/solution.java
@@ -1,24 +1,25 @@
 public class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int size = triangle.size();
-        int[] dp = new int[size];
+        if(triangle == null || triangle.size() == 0){
+            return 0;
+        }
+        int[] dp = new int[triangle.size()];
         dp[0] = triangle.get(0).get(0);
-        for(int i = 1; i < size; i++){
-            List<Integer> cur = triangle.get(i);
-            for(int j = cur.size() - 1; j >= 0; j--){
+        for(int i = 1; i < triangle.size(); i++){
+            for(int j = triangle.get(i).size() - 1; j >= 0 ; j--){
                 if(j == 0){
-                    dp[j] = dp[j] + cur.get(j);
-                }else if(j == cur.size() - 1){
-                    dp[j] = dp[j - 1] + cur.get(j);
+                    dp[j] = triangle.get(i).get(j) + dp[j];
+                }else if(j == triangle.get(i).size() - 1){
+                    dp[j] = triangle.get(i).get(j) + dp[j - 1];
                 }else{
-                    dp[j] = Math.min(dp[j], dp[j - 1]) + cur.get(j);
+                    dp[j] = triangle.get(i).get(j) + Math.min(dp[j - 1], dp[j]);
                 }
             }
         }
-        int res = Integer.MAX_VALUE;
-        for(int n : dp){
-            res = Math.min(n, res);
+        int min = dp[0];
+        for(int i = 1; i < dp.length; i++){
+            min = Math.min(min, dp[i]);
         }
-        return res;
+        return min;
     }
 }
