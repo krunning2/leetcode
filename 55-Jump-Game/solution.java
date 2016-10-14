66158bee375e88cb3ac112fedd49eaa153598c7f
krunning2 <zhidongke@yahoo.com>
2016-10-13 22:20:13 -0700

    [55. Jump Game][Accepted]committed by Krunning

--- 55-Jump-Game/solution.java
+++ 55-Jump-Game/solution.java
@@ -1,13 +1,11 @@
 public class Solution {
     public boolean canJump(int[] nums) {
         int next = 0;
-        int dis = 0;
+        int step = 0;
         for(int i = 0; i < nums.length; i++){
-            if(dis < i){
-                dis = next;
-                if(dis < i){
-                    return false;
-                }
+            if(step < i){
+                step = next;
+                if(step < i) return false;
             }
             next = Math.max(next, nums[i] + i);
         }
