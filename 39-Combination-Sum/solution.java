448155a2d3ba3f74411370f732af7c19c5c4bff7
krunning2 <zhidongke@yahoo.com>
2016-10-13 23:18:57 -0700

    [39. Combination Sum][Accepted]committed by Krunning

--- 39-Combination-Sum/solution.java
+++ 39-Combination-Sum/solution.java
@@ -1,23 +1,21 @@
 public class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Arrays.sort(candidates);
         List<List<Integer>> res = new ArrayList<>();
-        helper(candidates, target, 0, 0, new ArrayList<>(), res);
+        if(candidates == null || candidates.length == 0) return res;
+        DFS(candidates, target, 0, 0, res, new ArrayList<>());
         return res;
     }
-    private void helper(int[] candidates, int target, int pos, int sum, List<Integer> cur, List<List<Integer>> res){
-        if(target == sum){
-            res.add(new ArrayList<>(cur));
+    private void DFS(int[] candidates, int target, int pos, int sum, List<List<Integer>> res, List<Integer> cur){
+        if(sum > target){
             return;
         }
-        if(target < sum){
-            return;
+        if(sum == target){
+            res.add(new ArrayList<>(cur));
+            return ;
         }
         for(int i = pos; i < candidates.length; i++){
-            sum += candidates[i];
             cur.add(candidates[i]);
-            helper(candidates, target, i, sum, cur, res);
-            sum -= candidates[i];
+            DFS(candidates, target, i, sum + candidates[i], res, cur);
             cur.remove(cur.size() - 1);
         }
     }
